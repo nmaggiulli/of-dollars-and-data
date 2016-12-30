bf06131afe65ae5eaa92c283fd18135a7713302a
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2016-12-30 07:01:00 -0500

    Create header.R file to point to the relevant directories on my local machine

---
+++ header.R
@@ -0,0 +1,17 @@
+# Set dataroot location
+dataroot <- "C:/Users/Nick/data/of-dollars-and-data/"
+
+# Set dataset libraries
+raw <- paste0(dataroot, "datasets/raw/")
+mst <- paste0(dataroot, "datasets/mst/")
+
+# Set the import/export directories
+importdir <- paste0(root, "import/")
+exportdir <- paste0(root, "export/")
+
+# Set the programroot location
+programroot <- "~/git/of-dollars-and-data/"
+
+# Set options  
+# This option is used to prevent strings from being imported as factors
+options(StringsAsFactors=FALSE)
