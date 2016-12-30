df8b1a0f8699d539811933eb1652bc929c4f99d1
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2016-12-30 07:02:21 -0500

    Create skeleton program which will serve as a base template for all future programs

---
+++ skeleton.R
@@ -0,0 +1,14 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+
+########################## Start Program Here ######################### #
+
+
+
+# ############################  End  ################################## #
