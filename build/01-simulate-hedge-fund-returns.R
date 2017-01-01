acfb6fe2c48e6ac5ebefd7c24d6f0b852a2cedf8
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-01 10:55:57 -0500

    Update hedge fund model to simulate returns
    Still need to add in value paths and save down data

--- build/01-simulate-hedge-fund-returns.R
+++ build/01-simulate-hedge-fund-returns.R
@@ -13,8 +13,8 @@ library(dplyr)
 
 # Set the initial values for the simulation
 n_simulations <- 10000
-intial_investor_value <- 1 * 10^9                            
-initial_hedge_fund_value <- 0
+initial_client_capital <- 1 * 10^9                            
+initial_hedge_fund_capital <- 0
 n_periods <- 50
 
 # This seed allows us to have reproducible random sampling
@@ -26,17 +26,32 @@ hegde_fund_performance_fee <- 0.2
 sample_mean_return <- 0.1
 sample_sd_return <- 0.2
 
-# Initialze matrix for the returns 
+# Initialze matrix for the returns and value paths
 # I use matrices because they are much, much faster than data frames in this regard
 asset_return_matrix <- matrix(NA,nrow=n_simulations,ncol=n_periods)
+client_value_matrix <- matrix(NA,nrow=n_simulations,ncol=n_periods)
+hedge_fund_value_matrix <- matrix(NA,nrow=n_simulations,ncol=n_periods)
 
-# Sample from a normal distribution n_simulations for each of the periods in our model
-# Put the simulated returns into each column (period) of the data
+# Run the simulations for each period by getting asset returns and calculating asset values
+# This assumes the returns are annualized
 for (i in 1:n_periods){
+  # Sample from a normal distribution n_simulations for each of the periods in our model
+  # Put the simulated returns into each column (period) of the data
   asset_return_matrix[,i] <- rnorm(n_simulations, sample_mean_return, sample_sd_return)
+  
+  # At this point each row is a different simulation and the column is the i-th period
+  # Calculate the final asset values (use some if logic for 1st period)
+  if (i == 1){
+    client_value_matrix[,i] <- initial_client_capital * (1+asset_return_matrix)
+  } else if (i > 1){
+    
+  } else {
+    print("Error:  This should never happen")
+  }
 }
 
-# At this point each row is a simulation and each column is a period
+
+
 
 
 # ############################  End  ################################## #
