984e14ad6a45f0e4c7c5fbd11f85a9720840dcfc
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-03 07:44:38 -0500

    Add dynamic titles to analysis 03
    Use the SCF data dictionary to define the titles of the charts.
    Also added proper x and y axis labels.
    Still need to make legend look better (or in a different location)

--- analysis/03-scf-networth-charts.R
+++ analysis/03-scf-networth-charts.R
@@ -9,6 +9,7 @@ source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(scales)
 
 ########################## Start Program Here ######################### #
 
@@ -18,13 +19,43 @@ windowsFonts(LibreBaskerville=windowsFont("Libre Baskerville"))
 # Load data fom local library
 scf_stack <- readRDS(paste0(localdir, "03-scf-stack.Rds"))
 
+# Define first_year and last_year dynamically for the charts
+first_year <- min(scf_stack$year)
+last_year <- max(scf_stack$year)
+
 # Create lists of education class and age class to loop over
 agecl_list <- unique(scf_stack$agecl)
 edcl_list <- unique(scf_stack$edcl)
 
+# As a reminder here are what the codes for agecl and edcl represent:
+# agecl = age class, 1:<35, 2:35-44, 3:45-54, 4:55-64, 5:65-74, 6:>=75
+# edcl = education class, 1 = no high school diploma/GED, 2 = high school diploma or GED,
+#   3 = some college, 4 = college degree
+
 # Loop through the lists in order to create plots
 for (i in agecl_list){
   for (j in edcl_list){
+    
+    # Define strings for each age class based on the definitions above
+    if (i == 1){
+      agecl_string <- paste0("Less Than 35")
+    } else if (i > 1 & i < 6){
+      agecl_string <- paste0((35 + (i-2)*10), "-", (44 + (i-2)*10))
+    } else if (i == 6){
+      agecl_string <- paste0("75 or Older")
+    }
+    
+    # Do the same for the education class
+    if (j == 1){
+      edcl_string <- "No High School Diploma/GED"
+    } else if (j == 2){
+      edcl_string <- "High School Diploma/GED"
+    } else if (j == 3){
+      edcl_string <- "Some College"
+    } else if (j == 4){
+      edcl_string <- "College Degree"
+    }
+    
     # Filter the data to the correct age and education 
     # Then group by year and calculate networth percentiles
     to_plot <- filter(scf_stack, agecl == i, edcl == j) %>%
@@ -33,7 +64,7 @@ for (i in agecl_list){
                           pct_25 = quantile(networth, probs=0.25),
                           pct_50 = quantile(networth, probs=0.5),
                           pct_75 = quantile(networth, probs=0.75),
-                          pct_90 = quantile(networth, probs=0.90),
+                          #pct_90 = quantile(networth, probs=0.90),
                           n_obs = n()) %>%
               gather(networth_percentile, value, -year)
     
@@ -46,8 +77,14 @@ for (i in agecl_list){
     # Set the file_path based on the function input 
     file_path = paste0(exportdir, "03-scf-networth-charts/age_", i, "_edc_", j, ".jpeg")
     
+    # Create a dynamic title based upon the agecl and edcl
+    top_title <- paste0("Age Cohort:  ", agecl_string, "\n", "Education Cohort:\n", edcl_string)
+    
     plot <- ggplot(to_plot, aes(x = year, y = value, col = networth_percentile)) +
       geom_line() +
+      ggtitle(top_title)  +
+      scale_x_continuous(breaks = seq(first_year, last_year, 3)) +
+      scale_y_continuous(labels = dollar) +
       # Make a theme that matches the OfDollarsAndData.com blog
       theme(plot.title = element_text(family="LibreBaskerville", size = 15, face="bold", margin = margin(0, 0, 10, 0)),
             axis.title.y = element_text(face = "bold", size = 11, family = "LibreBaskerville", margin = margin(0, 10, 0, 0)),
