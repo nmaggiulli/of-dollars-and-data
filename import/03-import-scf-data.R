f1db5b1dade86462a7ebb1118ec4aec1a0d69e25
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-03 19:44:18 -0500

    Remove the my_font creation from the top of programs
    I mistakenly put it there because I was referring to it in correctly
    in another program.  my_font is not stored as an object but rather a
    font object which can be referred to as such family='my_font'.
    This is the same as loading the LibreBaskerville font in this case.

--- import/03-import-scf-data.R
+++ import/03-import-scf-data.R
@@ -4,9 +4,6 @@ rm(list = ls()) #clear your environment
 ########################## Load in header file ######################## #
 source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
 
-# Use the LibreBaskerville font
-windowsFonts(my_font=windowsFont("Libre Baskerville"))
-
 ########################## Load in Libraries ########################## #
 
 
