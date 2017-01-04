a2e7a153573b2eab8d45720be1e9c1cba21b62b3
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-04 08:17:28 -0500

    Refactor 03 charts for clarity
    Redoing charts to show networth percentiles by age.
    This will produce fewer charts which will be easier to comprehend.

--- analysis/03-scf-networth-charts.R
+++ analysis/03-scf-networth-charts.R
@@ -13,6 +13,8 @@ library(scales)
 library(grid)
 library(gridExtra)
 library(gtable)
+library(devtools)
+library(plotly)
 
 ########################## Start Program Here ######################### #
 
@@ -23,8 +25,10 @@ scf_stack <- readRDS(paste0(localdir, "03-scf-stack.Rds"))
 first_year <- min(scf_stack$year)
 last_year  <- max(scf_stack$year)
 
+# Plotly credentials
+py <- plotly("ofdollarsanddata", "..........")
+
 # Create lists of education class and age class to loop over
-agecl_list <- unique(scf_stack$agecl)
 edcl_list  <- unique(scf_stack$edcl)
 
 # As a reminder here are what the codes for agecl and edcl represent:
@@ -33,78 +37,79 @@ edcl_list  <- unique(scf_stack$edcl)
 #   3 = some college, 4 = college degree
 
 # Loop through the lists in order to create plots
-for (i in agecl_list){
-  for (j in edcl_list){
-    
-    # Define strings for each age class based on the definitions above
-    if (i == 1){
-      agecl_string <- paste0("Less Than 35")
-    } else if (i > 1 & i < 6){
-      agecl_string <- paste0((35 + (i-2)*10), "-", (44 + (i-2)*10))
-    } else if (i == 6){
-      agecl_string <- paste0("75 or Older")
-    }
-    
-    # Do the same for the education class
-    if (j == 1){
-      edcl_string <- "No High School Diploma/GED"
-    } else if (j == 2){
-      edcl_string <- "High School Diploma/GED"
-    } else if (j == 3){
-      edcl_string <- "Some College"
-    } else if (j == 4){
-      edcl_string <- "College Degree"
-    }
-    
-    # Filter the data to the correct age and education 
-    # Then group by year and calculate networth percentiles
-    to_plot <- filter(scf_stack, agecl == i, edcl == j) %>%
-                    group_by(year) %>%
-                summarise(`10th` = quantile(networth, probs=0.1),
-                          `25th` = quantile(networth, probs=0.25),
-                          `50th` = quantile(networth, probs=0.5),
-                          `75th` = quantile(networth, probs=0.75),
-                          n_obs = n()) %>%
-              gather(`Net Worth Percentile`, value, -year)
-    
-    plot_n <- filter(to_plot, `Net Worth Percentile` == "n_obs")
-    to_plot <- filter(to_plot, `Net Worth Percentile` != "n_obs")
+for (j in edcl_list){
   
-    to_plot$`Net Worth Percentile` <- factor(to_plot$`Net Worth Percentile`,
-                                             levels=c("75th", "50th", "25th", "10th"))
-    
-    
-    # Assign the data frame to another name to exmaine after plotting 
-    assign(paste0("to_plot_", i, "_", j), to_plot) 
-                
-    # Set the file_path based on the function input 
-    file_path = paste0(exportdir, "03-scf-networth-charts/age_", i, "_edc_", j, ".jpeg")
-    
-    # Create a dynamic title based upon the agecl and edcl
-    top_title <- paste0("Age Group:  ", agecl_string, "\n", "Education Level:  ", edcl_string)
-    
-    plot <- ggplot(to_plot, aes(x = year, y = value, col = `Net Worth Percentile`)) +
-      geom_line() +
-      ggtitle(top_title)  +
-      scale_x_continuous(breaks = seq(first_year, last_year, 3)) +
-      scale_y_continuous(labels = dollar) +
-      of_dollars_and_data_theme +
-      labs(x = "Year" , y = "Net Worth ($)")
-    
-    source_string <- "Source:  Federal Reserve Board, Survey of Consumer Finances"
-    note_string   <- "Note:  Net worth percentiles are shown at the household level." 
-    
-    my_gtable   <- ggplot_gtable(ggplot_build(plot))
-    
-    source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
-                            gp =gpar(fontfamily = "my_font", fontsize = 8))
-    note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
-                            gp =gpar(fontfamily = "my_font", fontsize = 8))
-    my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
-    my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
-    
-    ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
+  # Define strings for each age class based on the definitions above
+#   if (i == 1){
+#     agecl_string <- paste0("Less Than 35")
+#   } else if (i > 1 & i < 6){
+#     agecl_string <- paste0((35 + (i-2)*10), "-", (44 + (i-2)*10))
+#   } else if (i == 6){
+#     agecl_string <- paste0("75 or Older")
+#   }
+  
+  # Define strings for education class
+  if (j == 1){
+    edcl_string <- "No High School Diploma/GED"
+  } else if (j == 2){
+    edcl_string <- "High School Diploma/GED"
+  } else if (j == 3){
+    edcl_string <- "Some College"
+  } else if (j == 4){
+    edcl_string <- "College Degree"
   }
+  
+  # Filter the data to the correct age and education 
+  # Then group by year and calculate networth percentiles
+  to_plot <- filter(scf_stack, edcl == j, year == 2013) %>%
+                  group_by(year, agecl) %>%
+              summarise(`10th` = quantile(networth, probs=0.1),
+                        `25th` = quantile(networth, probs=0.25),
+                        `50th` = quantile(networth, probs=0.5),
+                        `75th` = quantile(networth, probs=0.75),
+                        n_obs = n()) %>%
+            gather(`Net Worth Percentile`, value, -year, -agecl)
+  
+  plot_n <- filter(to_plot, `Net Worth Percentile` == "n_obs")
+  to_plot <- filter(to_plot, `Net Worth Percentile` != "n_obs")
+
+  to_plot$`Net Worth Percentile` <- factor(to_plot$`Net Worth Percentile`,
+                                           levels=c("75th", "50th", "25th", "10th"))
+  
+  #Get agecl min and max for x-axis on plots
+  first_agecl <- min(to_plot$agecl)
+  last_agecl  <- max(to_plot$agecl)
+  
+  # Assign the data frame to another name to exmaine after plotting 
+  assign(paste0("to_plot_", j), to_plot) 
+              
+  # Set the file_path based on the function input 
+  file_path = paste0(exportdir, "03-scf-networth-charts/_edc_", j, ".jpeg")
+  
+  # Create a dynamic title based upon the agecl and edcl
+  top_title <- paste0("Education Level:  ", edcl_string)
+  
+  plot <- ggplot(to_plot, aes(x = agecl, y = value, col = `Net Worth Percentile`)) +
+    geom_line() +
+    ggtitle(top_title)  +
+    scale_x_continuous(breaks = seq(first_agecl, last_agecl, 1)) +
+    scale_y_continuous(labels = dollar) +
+    of_dollars_and_data_theme +
+    labs(x = "Age Group" , y = "Net Worth ($)")
+  
+  source_string <- "Source:  Federal Reserve Board, Survey of Consumer Finances"
+  note_string   <- "Note:  Net worth percentiles are shown at the household level." 
+  
+  my_gtable   <- ggplot_gtable(ggplot_build(plot))
+  
+  source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
+  my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
+  
+  ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
 }
 
 
