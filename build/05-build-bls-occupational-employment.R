fcc39559887adc5777f8ba790fabd04d09873a7c
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-12 07:46:23 -0500

    Add BLS OE area data to the master BLS OE dataset
    Area will be needed if I look into other datatypes.

--- build/05-build-bls-occupational-employment.R
+++ build/05-build-bls-occupational-employment.R
@@ -7,7 +7,7 @@ source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
 ########################## Load in Libraries ########################## #
 
 library(dplyr)
-library(zoo)
+library(stringr)
 
 ########################## Start Program Here ######################### #
 
@@ -28,24 +28,31 @@ create_index <- function(string){
 }
 
 areatype    <- create_index(areatype)
-datatype    <- create_index(datatype)
+area        <- create_index(area)
 industry    <- create_index(industry)
 occupation  <- create_index(occupation)
+datatype    <- create_index(datatype)
 
 # Parse the series ID based on the "pr.txt" file here:  https://download.bls.gov/pub/time.series/oe/
-bls_oe <- mutate(bls_oe, areatype_code = substr(series_id, 4, 4),
-                           industry_code = substr(series_id, 12, 17),
-                           occupation_code = substr(series_id, 18, 23),
-                           datatype_code = substr(series_id, 24, 25))
+bls_oe <- mutate(bls_oe, 
+                 areatype_code    = substr(series_id, 4, 4),
+                 area_code        = substr(series_id, 5, 11),
+                 industry_code    = substr(series_id, 12, 17),
+                 occupation_code  = substr(series_id, 18, 23),
+                 datatype_code    = substr(series_id, 24, 25))
+
+# Pad the area data with leading zeroes on area_code
+area$area_code <- str_pad(area$area_code, 7, pad = "0")
 
 # Merge on the sector, measure, and class information
-bls_oe <- bls_oe %>%
-                  left_join(areatype) %>%
-                    left_join(industry) %>%
-                      left_join(occupation) %>%
-                        left_join(datatype) %>%
-                    select(series_id, year, value, footnote_codes, 
-                          areatype_name, industry_name, occupation_name, datatype_name)
+bls_oe <- bls_oe                %>%
+          left_join(areatype)   %>%
+          left_join(area)       %>%
+          left_join(industry)   %>%
+          left_join(occupation) %>%
+          left_join(datatype)   %>%
+            select(series_id, year, value, footnote_codes, area_name,
+                  areatype_name, industry_name, occupation_name, datatype_name)
 
 # Save down final build before doing analysis
 saveRDS(bls_oe, paste0(localdir, "05-bls-oe.Rds"))
--- import/05-import-bls-occupational-employment.R
+++ import/05-import-bls-occupational-employment.R
@@ -6,6 +6,7 @@ source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
 
 ########################## Load in Libraries ########################## #
 
+library(dplyr)
 
 ########################## Start Program Here ######################### #
 
@@ -26,6 +27,18 @@ read_in_bls(industry)
 read_in_bls(occupation)
 read_in_bls(data.1.AllData)
 
+# Process area separetly because the column names that match areatype
+area <- read.csv(paste0("https://download.bls.gov/pub/time.series/oe/oe.area"), 
+                      header = TRUE, 
+                      sep = "\t",
+                      row.names = 2)
+
+# Drop those columns that we do not need
+area <- area[ , !(names(area) %in% c("area_code","area_name"))]
+
+area <- rename(area, area_code = areatype_code, state_code = row.names)
+
+saveRDS(area, paste0(importdir, "05-bls-occupational-employment/bls_oe_area.Rds"))
 
 
 
