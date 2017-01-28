b4389258951e2b91b0fa39f5567bfc2b27abc0e5
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-28 12:00:46 -0500

    Create an initial BLS CX import

---
+++ import/07-import-bls-consumer-expenditures.R
@@ -0,0 +1,33 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(dplyr)
+
+########################## Start Program Here ######################### #
+
+# Read in productivity data from tab-delimited text file on the web
+# Use function to make this dynamic
+read_in_bls <- function(string){
+  name      <- deparse(substitute(string))
+  temp_name <- read.csv(paste0("https://download.bls.gov/pub/time.series/cx/cx.", name), 
+                             header = TRUE, 
+                             sep = "\t")
+  
+  saveRDS(temp_name, paste0(importdir, "07-bls-consumer-expenditures/bls_cx_", name, ".Rds"))
+}
+
+
+read_in_bls(data.1.AllData)
+read_in_bls(category)
+read_in_bls(demographics)
+read_in_bls(subcategory)
+
+read_in_bls(characteristics)
+read_in_bls(item)
+
+# ############################  End  ################################## #
