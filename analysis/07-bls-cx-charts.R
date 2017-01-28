6c0b1da95a91c20f2285efd5ebf7cf7ae734e180
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-28 15:05:55 -0500

    Initial commit for BLS-CX EDA

---
+++ analysis/07-bls-cx-charts.R
@@ -0,0 +1,27 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(scales)
+library(grid)
+library(gridExtra)
+library(gtable)
+library(RColorBrewer)
+library(stringr)
+library(ggrepel)
+
+########################## Start Program Here ######################### #
+
+# Load data fom local library
+bls_cx <- readRDS(paste0(localdir, "07-bls-cx.Rds"))
+
+
+
+# ############################  End  ################################## #
