507521d0dfe6a87d3e980cc238f64d3671fbe403
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-01-29 10:06:11 -0500

    Create time charts for every item-demographic pairing
    Broken out by characteristics.
    This is how I do EDA (exploratory data analysis).  Mass charts.
    Look at the data and find something.

--- analysis/07-bls-cx-charts.R
+++ analysis/07-bls-cx-charts.R
@@ -29,12 +29,65 @@ read_in <- function(string){
 
 names <- c("item", "demographics", "characteristics", "subcategory", "category")
 
+# Loop through datasets to read in
 for (i in names){
   tmpname <- paste0(i)
   df      <- read_in(i)
   assign(tmpname, df, envir = .GlobalEnv)
   rm(df)
   rm(tmpname)
+}
+
+# Filter main cx data
+bls_cx_filtered <- filter(bls_cx, 
+                          category_name == "Expenditures")
+
+# Create a list for looping
+loop_list <- unique(select(bls_cx_filtered, item_name, demographics_name))
+
+for (i in 1:nrow(loop_list)){
+  to_plot              <- filter(bls_cx_filtered, item_name == loop_list[i, 1], 
+                                 demographics_name == loop_list[i, 2])
+  last_year            <- max(to_plot$year)
+  item_code            <- unique(to_plot$item_code)
+  demographics_code    <- unique(to_plot$demographics_code)
+  
+  # Set the file_path 
+  file_path = paste0(exportdir, "07-bls-consumer-expenditures/", item_code, "-", demographics_code, ".jpeg")
+  
+  # Plot the time trends
+  plot <- ggplot(to_plot, aes(x = year, y = value, col = characteristics_name))  +
+    geom_line() +
+    geom_text_repel(data = filter(to_plot, year == last_year),
+                    aes(year, 
+                        value, 
+                        label = characteristics_name, 
+                        family = "my_font"), 
+                    size = 3)+
+    scale_color_discrete(guide=FALSE) +
+    scale_y_continuous(label = dollar) +
+    ggtitle(paste0(loop_list[i, 1], "\n", loop_list[i, 2]))  +
+    of_dollars_and_data_theme +
+    labs(x = "Year" , y = "Annual Expenditure")
+  
+  # Turn plot into a gtable for adding text grobs
+  my_gtable   <- ggplot_gtable(ggplot_build(plot))
+  
+  source_string <- "Source:  Bureau of Labor Statistics, Consumer Expenditures (OfDollarsAndData.com)"
+  note_string   <- "Note:  " 
+  
+  # Make the source and note text grobs
+  source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  
+  # Add the text grobs to the bototm of the gtable
+  my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
+  my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
+  
+  # Save the plot  
+  ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm") 
 }
 
 
