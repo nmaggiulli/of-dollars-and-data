8b95a4b94186ea616598585c27e3a9309c874bd1
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-02-09 18:32:53 -0500

    Build the 1 month returns and price_plus_div in the import program
    Instead of relying on Excel calculations everything is done in R for greater
    transparency.

--- import/09-import-sp500-returns-pe.R
+++ import/09-import-sp500-returns-pe.R
@@ -12,9 +12,23 @@ library(dplyr)
 ########################## Start Program Here ######################### #
 
 # Bring in the data Shiller uses on his website
+# Build the data in this step
+# Also drop the 2017 data as the dividends are missing
 sp500_ret_pe <- read_excel(paste0(importdir, "09-sp500-returns-pe/ie_data.xls"),
                           sheet = "cleaned") %>%
-                select(Date, cape, price_plus_div)
+                  filter(!is.na(real_div)) %>%
+                select(Date, real_price, real_div, cape)
+
+for (i in 1:nrow(sp500_ret_pe)){
+  if (i == 1){
+    sp500_ret_pe[i, "n_shares"] <- 1 + sp500_ret_pe[i, "real_div"]/ 12 / sp500_ret_pe[i, "real_price"]
+    sp500_ret_pe[i, "price_plus_div"] <- sp500_ret_pe[i, "n_shares"] * sp500_ret_pe[i, "real_price"]
+  } else{
+    sp500_ret_pe[i, "n_shares"] <- sp500_ret_pe[(i - 1), "n_shares"] + sp500_ret_pe[i, "real_div"]/ 12 / sp500_ret_pe[i, "real_price"]
+    sp500_ret_pe[i, "price_plus_div"] <- sp500_ret_pe[i, "n_shares"] * sp500_ret_pe[i, "real_price"]
+    sp500_ret_pe[i, "ret_1_month"] <- sp500_ret_pe[i, "price_plus_div"]/sp500_ret_pe[(i - 1), "price_plus_div"] - 1
+  }
+}
 
 # Save down the data
 saveRDS(sp500_ret_pe, paste0(localdir, "09-sp500-ret-pe.Rds"))
