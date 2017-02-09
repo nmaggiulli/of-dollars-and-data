83ca63a706da2faca208df9fa508538724cef243
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-02-08 22:59:52 -0500

    Add returns series by date to 09

--- analysis/09-sp500-returns-vs-pe.R
+++ analysis/09-sp500-returns-vs-pe.R
@@ -16,6 +16,7 @@ library(gtable)
 library(RColorBrewer)
 library(stringr)
 library(ggrepel)
+library(lubridate)
 
 ########################## Start Program Here ######################### #
 
@@ -26,6 +27,15 @@ sp500_ret_pe   <- readRDS(paste0(localdir, "09-sp500-ret-pe.Rds"))
 # Subset S&P 500 returns
 sp500_ret_pe <- filter(sp500_ret_pe, cape != "NA", Date < 2017.01)
 
+sp500_ret_pe$year_month <- as.Date(paste0(substring(sp500_ret_pe$Date,
+                                                    1,
+                                                    4),
+                                                    "-",
+                                          substring(sp500_ret_pe$Date,
+                                                    6,
+                                                    7),
+                                          "-01"))
+
 first_year <- floor(min(sp500_ret_pe$Date))
 last_year <- floor(max(sp500_ret_pe$Date))
 
@@ -34,7 +44,7 @@ sp500_ret_pe$cape <- as.numeric(sp500_ret_pe$cape)
 
 # Setup a vector of different returns to calculate
 # This will be for 5 year, 10 year, 20 year, and 30 year returns
-returns_to_calc <- c(5, 10, 20, 30)
+returns_to_calc <- c(5, 10, 20, 30, 40)
 
 # Calculate returns over different time frames
 for (i in 1:nrow(sp500_ret_pe)){
@@ -43,7 +53,8 @@ for (i in 1:nrow(sp500_ret_pe)){
       break
     } else{
       name <- paste0("ret_", x)
-      sp500_ret_pe[i, name] <- (sp500_ret_pe[(i + (x * 12)), "price_plus_div"]/sp500_ret_pe[(i), "price_plus_div"])^(1/(x)) - 1
+      sp500_ret_pe[i, name] <- ((sp500_ret_pe[(i + (x * 12)), "price_plus_div"] / 
+                                  sp500_ret_pe[(i), "price_plus_div"]) ^ (1/(x))) - 1
     }
   }
 }
@@ -57,7 +68,7 @@ plot_ret_pe <- function(var){
   ymin <- floor(min(to_plot[, yvar]) * 10) / 10
   
   # Set the file_path for the next output
-  file_path = paste0(exportdir, "09-sp500-returns-pe/returns-", var,"-year.jpeg")
+  file_path = paste0(exportdir, "09-sp500-returns-pe/pe-returns-", var,"-year.jpeg")
   
   plot <- ggplot(data = to_plot, aes_string(x = "cape", y = yvar)) +
     geom_point() +
@@ -86,9 +97,37 @@ plot_ret_pe <- function(var){
   
   # Save the gtable
   ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
+
+  # Set the file_path for the next output
+  file_path = paste0(exportdir, "09-sp500-returns-pe/returns-", var,"-year.jpeg")
   
+  plot <- ggplot(data = to_plot, aes_string(x = "year_month", y = yvar)) +
+    geom_line() +
+    geom_smooth(method = "lm", se = FALSE) +
+    scale_y_continuous(label = percent, limits = c(ymin, ymax)) +
+    ggtitle(paste0("S&P 500 Annualized Real Return\n(Over ", var, " Years)")) +
+    of_dollars_and_data_theme +
+    labs(x = "Year" , y = "S&P 500 Annualized Real Return (%)")
   
+  # Add a source and note string for the plots
+  source_string <- paste0("Source:  http://www.econ.yale.edu/~shiller/data.htm, ", first_year, " - ", last_year," (OfDollarsAndData.com)")
+  note_string   <- paste0("Note:  Annualized real returns include reinvested dividends.") 
   
+  # Turn plot into a gtable for adding text grobs
+  my_gtable   <- ggplot_gtable(ggplot_build(plot))
+  
+  # Make the source and note text grobs
+  source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  
+  # Add the text grobs to the bototm of the gtable
+  my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
+  my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
+  
+  # Save the gtable
+  ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
 }
 
 for (x in returns_to_calc){
