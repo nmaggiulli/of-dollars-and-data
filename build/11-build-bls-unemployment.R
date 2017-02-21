8dfbf74e6b9ab049b88f9941e7e841ff54c9b978
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-02-20 22:02:40 -0500

    Build the BLS unemployment data

---
+++ build/11-build-bls-unemployment.R
@@ -0,0 +1,49 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(dplyr)
+library(stringr)
+
+########################## Start Program Here ######################### #
+
+# Create list of datasets to loop through
+loop_list <- c("data.0.CurrentU90-94",
+               "data.0.CurrentU95-99", 
+               "data.0.CurrentU00-04",
+               "data.0.CurrentU05-09",
+               "data.0.CurrentU10-14",
+               "data.0.CurrentU15-19")
+
+# Load in raw BLS UE data and stack it
+for (i in loop_list){
+  if (i == "data.0.CurrentU90-94"){
+    ue_stack <- readRDS(paste0(importdir, "11-bls-unemployment/bls_unemployment_", i, ".Rds"))
+  } else{
+    temp <- readRDS(paste0(importdir, "11-bls-unemployment/bls_unemployment_", i, ".Rds"))
+    ue_stack <- rbind(ue_stack, temp)
+  }
+}
+
+# Load in the area and area_type
+area <- readRDS(paste0(importdir, "11-bls-unemployment/bls_unemployment_area.Rds"))
+
+area <- select(area, area_type_code, area_code) %>%
+          mutate(area_text = area_code, area_code = area_type_code) %>%
+            select(area_code, area_text)
+
+# Define the area on the ue stack
+ue_stack <- mutate(ue_stack, area_code = substr(series_id, 4, 18))
+
+# Join the area data to the stack
+ue_stack <- ue_stack %>%
+              left_join(area)
+
+# Save down RDS
+saveRDS(ue_stack, paste0(localdir, "11-bls-ue.Rds"))
+
+# ############################  End  ################################## #
