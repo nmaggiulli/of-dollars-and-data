0b3bdb0d1c9f0e99c41d90fe514e4bf8960a4f69
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-03-01 22:22:05 -0500

    Add additional sensitivities to hedge fund analysis 13
    Now it can do return above alpha, a performance and management or
    a performance or management fee model, and I can also vary the correlation
    and the outperformance of the fund over the market benchmark.

--- analysis/13-simulate-correlated-hedge-fund-returns.R
+++ analysis/13-simulate-correlated-hedge-fund-returns.R
@@ -39,13 +39,11 @@ sd_market         <- 0.2
 run_sim <- function(hf_outperformance, 
                     hf_management_fee,
                     hf_performance_fee,
+                    hf_performance_above_alpha,
+                    management_and_performance_fee,
                     hf_deduct_fees,
                     market_management_fee,
-                    hf_corr_to_market,
-                    file_name,
-                    top_title,
-                    source_string,
-                    note_string
+                    hf_corr_to_market
                     ){
   
   # Set the mean and standard deviation for the hedge fund
@@ -81,13 +79,22 @@ run_sim <- function(hf_outperformance,
                    empirical = TRUE)
     
     # Determine if there is any alpha for each simulation
-    alpha <- pmax(0, ret[, 2] - ret[, 1])
+    if (hf_performance_above_alpha == 1){
+      alpha <- pmax(0, ret[, 2] - ret[, 1])
+    } else {
+      alpha <- pmax(0, ret[, 2])
+    }
     
     if (i == 1){
       client_market_matrix[, i] <- initial_client_capital * (1 + ret[, 1]) * (1 - market_management_fee)
       management_fee            <- initial_client_capital * hf_management_fee
-      performance_fee           <- initial_client_capital * alpha * hf_performance_fee
-      hf_final_fee              <- pmax(management_fee, performance_fee)
+      if (management_and_performance_fee == 0){
+        performance_fee           <- initial_client_capital * alpha * hf_performance_fee  
+        hf_final_fee              <- pmax(management_fee, performance_fee)
+      } else {
+        performance_fee           <- pmax(0, ((initial_client_capital * alpha - management_fee) * hf_performance_fee))
+        hf_final_fee              <- management_fee + performance_fee
+      }
       client_hf_matrix[, i]     <- (initial_client_capital * (1 + ret[, 2])) - hf_final_fee
       if (hf_deduct_fees == 1){
         fee_deduction             <- sapply(performance_fee, function(x){ ifelse(x == 0, management_fee, 0)})
@@ -97,8 +104,13 @@ run_sim <- function(hf_outperformance,
     } else {
       client_market_matrix[, i] <- client_market_matrix[, (i - 1)] * (1 + ret[, 1]) * (1 - market_management_fee)
       management_fee            <- client_hf_matrix[, (i - 1)] * hf_management_fee
-      performance_fee           <- client_hf_matrix[, (i - 1)] * alpha * hf_performance_fee  
-      hf_final_fee              <- pmax(0, pmax(management_fee, performance_fee) - fee_deduction)
+      if (management_and_performance_fee == 0){
+        performance_fee           <- client_hf_matrix[, (i - 1)] * alpha * hf_performance_fee  
+        hf_final_fee              <- pmax(0, pmax(management_fee, performance_fee) - fee_deduction)
+      } else {
+        performance_fee           <- pmax(0, ((client_hf_matrix[, (i - 1)] * alpha - management_fee) * hf_performance_fee))
+        hf_final_fee              <- management_fee + performance_fee
+      }
       client_hf_matrix[, i]     <- (client_hf_matrix[, (i - 1)] * (1 + ret[, 2])) - hf_final_fee
       if (hf_deduct_fees == 1){
         fee_deduction             <- sapply(performance_fee, function(x){ ifelse(x == 0, management_fee, 0)})
@@ -107,26 +119,86 @@ run_sim <- function(hf_outperformance,
       }
     }
   }
-  assign("client_hf_matrix", client_hf_matrix, envir = .GlobalEnv)
-  assign("client_market_matrix", client_hf_matrix, envir = .GlobalEnv)
   print(paste0("The hedge fund outperformed the market (net of fees) in ", sum(client_hf_matrix[, n_years] > client_market_matrix[, n_years])/n_simulations * 100, "% of simulations"))
+  return(sum(client_hf_matrix[, n_years] > client_market_matrix[, n_years])/n_simulations)
 }
 
+# run_sim(
+#   hf_outperformance              = 0,
+#   hf_management_fee              = 0.02,
+#   hf_performance_fee             = 0.2,
+#   hf_performance_above_alpha     = 1,
+#   management_and_performance_fee = 0,
+#   hf_deduct_fees                 = 1,
+#   market_management_fee          = 0.0005,
+#   hf_corr_to_market              = 0.7
+# )
 
+# Create a matrix to store the results
+results <- data.frame(hf_outperformance              = numeric(),
+                      hf_management_fee              = numeric(),
+                      hf_performance_fee             = numeric(),
+                      hf_performance_above_alpha     = integer(),
+                      management_and_performance_fee = integer(),
+                      hf_deduct_fees                 = integer(),
+                      market_management_fee          = numeric(),
+                      hf_corr_to_market              = numeric(),
+                      hf_outperform_pct              = numeric(),
+                      scenario                       = integer()
+                      )
 
-# Set simulation parameters
-run_sim(
-  hf_outperformance = 0.01,
-  hf_management_fee = 0.01, 
-  hf_performance_fee = 0.3, 
-  hf_deduct_fees = 1,
-  market_management_fee = 0.0005,
-  hf_corr_to_market = 0.9,
-  file_name = "hf_over_client_2_and_20_sp500_return.jpeg",
-  top_title = "On Average, the Hedge Fund is Richer\nThan Its Clients In Less Than 20 Years",
-  source_string = "Source:  Simulated returns (OfDollarsAndData.com)",
-  note_string = "Note:  Assumes a 2% management fee and a 20% performance fee."
-)
+# Loop through outperformance, correlation, and other sensitivities
+i <- 1
+for (o in seq(0, 0.04, by = 0.01)){
+  for (c in seq(0, 0.9, by = 0.1)){
+    for (scenario in seq(1, 3, by = 1)){
+      if (scenario == 1){
+        mf  <- 0.01
+        pf  <- 0.3
+        paa <- 1
+        map <- 0
+        df  <- 1
+      } else if (scenario == 2){
+        mf  <- 0.02
+        pf  <- 0.2
+        paa <- 0
+        map <- 1
+        df  <- 0
+      } else if (scenario == 3){
+        mf  <- 0.01
+        pf  <- 0.17
+        paa <- 0
+        map <- 1
+        df  <- 0
+      }
+      
+      results[i, "hf_outperformance"]              <- o
+      results[i, "hf_management_fee"]              <- mf
+      results[i, "hf_performance_fee"]             <- pf
+      results[i, "hf_performance_above_alpha"]     <- paa
+      results[i, "management_and_performance_fee"] <- map
+      results[i, "hf_deduct_fees"]                 <- df
+      results[i, "market_management_fee"]          <- 0.0005
+      results[i, "hf_corr_to_market"]              <- c
+      results[i, "scenario"]                       <- scenario
+      
+      results[i, "hf_outperform_pct"] <- run_sim(
+        hf_outperformance              = o,
+        hf_management_fee              = mf,
+        hf_performance_fee             = pf,
+        hf_performance_above_alpha     = paa,
+        management_and_performance_fee = map,
+        hf_deduct_fees                 = df,
+        market_management_fee          = 0.0005,
+        hf_corr_to_market              = c
+      )
+      i <- i + 1
+      print(i)
+    }
+  }
+}
 
+# Save down RDS given the loops above take a few minutes to run
+saveRDS(results, paste0(localdir, "13-hf-correlation-results.Rds"))
 
 # ############################  End  ################################## #
