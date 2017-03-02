777356b69f13d5b6ac516235e36c98bd6a97d8a1
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-03-02 08:57:23 -0500

    Change titles to make analysis 13 plots clearer

--- analysis/13-plot-correlated-hedge-fund-returns.R
+++ analysis/13-plot-correlated-hedge-fund-returns.R
@@ -15,6 +15,7 @@ library(grid)
 library(gridExtra)
 library(gtable)
 library(ggrepel)
+library(stringr)
 
 ########################## Start Program Here ######################### #
 
@@ -24,7 +25,7 @@ hf_results <- readRDS(paste0(localdir, "13-hf-correlation-results.Rds"))
 # Loop through rows to add additional info to the data frame
 for (i in 1:nrow(hf_results)){
   if (hf_results[i, "scenario"] == 1){
-    hf_results[i, "scenario_name"] <- "1% management or 30% performance"
+    hf_results[i, "scenario_name"] <- "1% management or 30% of alpha"
   } else if (hf_results[i, "scenario"] == 2){
     hf_results[i, "scenario_name"] <- "2% management and 20% performance"
   } else if (hf_results[i, "scenario"] == 3){
@@ -43,14 +44,14 @@ for (j in unique_outperformance){
             geom_text_repel(data = filter(to_plot, hf_corr_to_market == 0.5), 
                     aes(hf_corr_to_market, 
                         col = scenario_name,
-                        label = scenario_name,
-                        family = "my_font"),
-                    max.iter =  3000) +
+                        label = str_wrap(scenario_name, width = 18),
+                        family = "my_font")) +
           scale_color_brewer(palette = "Set1", guide = FALSE) +
-          ggtitle(paste0("Test ", j)) +
+          ggtitle(paste0("Hedge Fund Performance Relative to Market\n(Assumes ", 100*j, "% Annual Outperformance)")) +
           of_dollars_and_data_theme +
           scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.1), label = percent) +
-          labs(x = "Hedge Fund Correlation to Market" , y = "Percentage of Simulations Where Hedge Fund Outperforms")
+          scale_x_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.1)) +
+          labs(x = "Hedge Fund Correlation to Market" , y = "Percentage of Simulations Where Fund\nOutperforms The Market (Net of Fees)")
   
   # Add a source and note string for the plots
   source_string <- "Source:  Simulated returns (OfDollarsAndData.com)"
