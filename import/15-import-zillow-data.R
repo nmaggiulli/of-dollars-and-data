858159bba106f81e99b839e9dba733a5b33a49a0
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-03-21 22:00:31 -0400

    Fix import to properly bring in zipped data
    Uses a temp file downloaded locally.

--- import/15-import-zillow-data.R
+++ import/15-import-zillow-data.R
@@ -16,11 +16,17 @@ download.file("http://files.zillowstatic.com/research/public/State.zip",temp)
 # List the files before selecting which ones to keep
 unzip(temp, list = TRUE)
 
+import_file <- function(path, name){
+  test <- read.table(unz(temp, paste0("State/", path)), sep = ",", header = TRUE)
+  saveRDS(test, paste0(importdir, "15-zillow-home-price/", name, ".Rds"))
+}
+
 # Keep median sold price per square foot and median rental price per square foot
-state_mediansold_sqft_all <- read.table(unz(temp, "State/State_MedianSoldPricePerSqft_AllHomes.csv"))
-state_mediansold_all      <- read.table(unz(temp, "State/State_MedianSoldPrice_AllHomes.csv"))
-state_rentalprice_all     <- read.table(unz(temp, "State/State_MedianRentalPrice_AllHomes.csv"))
-state_rentalprice_1bed    <- read.table(unz(temp, "State/State_MedianRentalPrice_1Bedroom.csv"))
+import_file("State_MedianSoldPricePerSqft_AllHomes.csv", "state_mediansold_sqft_all")
+import_file("State_MedianSoldPrice_AllHomes.csv", "state_mediansold_all")
+import_file("State_Zhvi_TopTier.csv", "state_zhvi_toptier")
+import_file("State_Zhvi_BottomTier.csv", "state_zhvi_bottomtier")
+import_file("State_Zhvi_MiddleTier.csv", "state_zhvi_middletier")
 
 unlink(temp)
 
