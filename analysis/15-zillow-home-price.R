b961c0c6e788679098442e072693a8d265082423
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-03-22 22:00:38 -0400

    Plot the top, middle, and bottom tiers for all metro areas
    Will use these to refine a story about the housing market.

--- analysis/15-zillow-home-price.R
+++ analysis/15-zillow-home-price.R
@@ -15,41 +15,73 @@ library(gridExtra)
 library(gtable)
 library(RColorBrewer)
 library(stringr)
+library(tools)
 
 ########################## Start Program Here ######################### #
 
-# Create a function to read in data and select the start year for maps
-plot_data <- function(file_name, yr, top_title){
-  to_plot <- readRDS(paste0(localdir, "15_", file_name, ".Rds"))
-  to_plot <- filter_(to_plot, ~year >= yr) %>%
-              arrange_(~RegionID, ~year) 
+start_year <- as.Date("2000-01-01")
+
+
+# Read in data frames
+metro_mediansold_all <- readRDS(paste0(localdir, "15_metro_mediansold_all.Rds"))
+
+read_in_tier <- function(name){
+  string <- deparse(substitute(name))
+  temp <- readRDS(paste0(localdir, "15_metro_zhvi_", string,"tier.Rds"))
+  temp <- filter(temp, year >= start_year)
+  temp$type <- toTitleCase(string)
+  assign(paste0("metro_zhvi_", string), temp, envir = .GlobalEnv)
+}
+
+read_in_tier(top)
+read_in_tier(middle)
+read_in_tier(bottom)
+
+all_tiers <- bind_rows(metro_zhvi_top, metro_zhvi_bottom, metro_zhvi_middle)
+
+unique_metros <- unique(all_tiers$RegionName)
+
+for (n in unique_metros){
+
+  # Subset the data to plot by the region name
+  to_plot <- filter(all_tiers, RegionName == n) %>%
+              arrange(RegionID, type, year)
   
-  print(head(to_plot))
+  # Get the region_id for file naming
+  region_id <- unique(to_plot$RegionID)
   
   # Get the years list
   first_year  <- min(to_plot$year)
   last_year   <- max(to_plot$year)
   
-  # Create a diff variable for % changes over time
-  to_plot <- to_plot %>%
-              filter(year== first_year | year == last_year) %>%
-              arrange(RegionID, year) %>%
-              mutate(diff = price/lag(price) - 1) %>%
-              filter(year == last_year)
-  
-  # Find the range of y-values for mapping
-  y_max <- max(to_plot$diff, na.rm = TRUE)
-  y_min <- min(to_plot$diff, na.rm = TRUE)
+  # Create the index for the price series
+  for (i in 1:nrow(to_plot)){
+    if (to_plot[i, "year"] == first_year){
+      last_value <- to_plot[i, "price"]
+      to_plot[i, "index"] <- 100
+    } else {
+      pct_change <- (to_plot[i, "price"] - last_value) / last_value
+      last_value <- to_plot[i, "price"]
+      to_plot[i, "index"] <- (1 + pct_change) * to_plot[i - 1, "index"]
+    }
+  }
   
   # Set the file_path based on the function input 
-  file_path = paste0(exportdir, "15-zillow-home-price/", file_name, "-", yr, ".jpg")
-  
-  print(head(to_plot))
+  file_path = paste0(exportdir, "15-zillow-home-price/zhvi-", region_id ,".jpeg")
   
   # Create the plot
-  plot <- ggplot() +
-  of_dollars_and_data_theme +
-  ggtitle(top_title) 
+  plot <- ggplot(to_plot, aes(x = year, y = index, col = type)) +
+            geom_line() +
+            geom_text_repel(data = filter(to_plot, year == last_year),
+                            aes(year,
+                                index, 
+                                label = type,
+                                family = "my_font")) +
+            scale_color_discrete(guide = FALSE) +
+            of_dollars_and_data_theme +
+            scale_y_continuous(limits = c(50, 300)) +
+            ggtitle(paste0("Home Sale Values By Tier\n", n)) +
+            labs(x = "Year", y = "Index (2000 = 100)")
   
   # Add a source and note string for the plots
   source_string <- "Source:  Zillow Research (OfDollarsAndData.com)"
@@ -73,17 +105,7 @@ plot_data <- function(file_name, yr, top_title){
   ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
 }
 
-plot_data("state_mediansold_all", 
-          2000,
-          "Median Sold Price All Homes\n2000 - 2017")
 
-plot_data("state_zhvi_toptier", 
-          2000,
-          "Median Sold Price Top Tier Homes\n2000 - 2017")
-
-plot_data("state_zhvi_bottomtier", 
-          2000,
-          "Median Sold Price Bottom Tier Homes\n2000 - 2017")
 
     
 
