97734632d0667e72331e2a1d9cd61b1d78ea54f0
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-03-26 20:45:54 -0400

    Add area plot for percentage of total assets that are investment gains

--- analysis/16-investing-vs-saving-plots.R
+++ analysis/16-investing-vs-saving-plots.R
@@ -66,7 +66,6 @@ convert_to_df <- function(matrix, type){
 
 savings_df <- convert_to_df(savings_matrix, "savings")
 returns_df <- convert_to_df(returns_matrix, "returns")
-assets_df  <- convert_to_df(asset_matrix, "total_assets")
 
 # Bind the two data frames
 to_plot <- bind_rows(savings_df, returns_df)
@@ -91,78 +90,112 @@ while (ceiling(abs(y_max - y_min))/y_unit > 10){
 # Define a new y_max if the y_unit has changed
 y_max <- create_max_min(y_max, y_unit, ceiling)
 
-# Set the file path
-file_path = paste0(exportdir, "16-investing-vs-saving/saving-vs-investing.jpeg")
-
-# Create plot 
-plot <- ggplot(data = to_plot, aes(x = year, fill = type, weight = value)) +
-            geom_bar(position = "stack") +
-            geom_line(data = assets_df, aes(x = year, y = value), col = "black") + 
-            geom_hline(yintercept = 0) +
-            geom_text_repel(data = 
-                              filter(to_plot, 
-                                     year == 1, 
-                                     type == "savings"),
-                            aes(x = year, 
-                                y = value,
-                                col = type,
-                                label = str_wrap("Savings Matter Early in Life", width = 10),
-                                family = "my_font"),
-                            nudge_y = 120000,
-                            nudge_x = 2) +
-            geom_text_repel(data = 
-                              filter(assets_df, 
-                                     year == 20, 
-                                     type == "total_assets"),
-                            aes(x = year, 
-                                y = value,
-                                col = type,
-                                label = str_wrap("Total Assets", width = 18),
-                                family = "my_font"),
-                            nudge_y = 20000,
-                            nudge_x = -2,
-                            max.iter = 3000) +
-            geom_text_repel(data = 
-                              filter(to_plot, 
-                                     year == 29, 
-                                     type == "returns"),
-                            aes(x = year, 
-                                y = value,
-                                col = type,
-                                label = str_wrap("Investments Dominate Later in Life", width = 10),
-                                family = "my_font"),
-                            nudge_y = 130000,
-                            nudge_x = -2) +
-            scale_color_manual(values = my_palette, guide = FALSE) +
-            scale_fill_manual(values = my_palette, guide = FALSE) +
-            scale_y_continuous(labels = dollar, limits = c(y_min, y_max), breaks = seq(y_min, y_max, y_unit)) +
-            scale_x_continuous(breaks = seq(0, n_years_working, 5)) +
-            of_dollars_and_data_theme +
-            labs(x = "Years" , y = "Value") +
-            ggtitle(paste0("Savings and Investment Returns Have\nVarying Impact Over Time"))
-
-# Add a source and note string for the plots
-source_string <- "Source:  Simulated data (OfDollarsAndData.com)"
-note_string   <- paste0("Note:  Assumes an annual mean return of ", 
-                        sample_mean*100,
-                        "% with a ",
-                        sample_sd*100,
-                        "% standard deviation.") 
-
-# Turn plot into a gtable for adding text grobs
-my_gtable   <- ggplot_gtable(ggplot_build(plot))
-
-# Make the source and note text grobs
-source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
-                        gp =gpar(fontfamily = "my_font", fontsize = 8))
-note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
-                        gp =gpar(fontfamily = "my_font", fontsize = 8))
-
-# Add the text grobs to the bototm of the gtable
-my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
-my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
-
-# Save the gtable
-ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
+## Create 1st plot
+  # Set the file path
+  file_path = paste0(exportdir, "16-investing-vs-saving/saving-vs-investing.jpeg")
+  
+  # Create plot 
+  plot <- ggplot(data = to_plot, aes(x = year, fill = type, weight = value)) +
+              geom_bar(position = "stack") +
+              geom_hline(yintercept = 0) +
+              geom_text_repel(data = 
+                                filter(to_plot, 
+                                       year == 1, 
+                                       type == "savings"),
+                              aes(x = year, 
+                                  y = value,
+                                  col = type,
+                                  label = str_wrap("Savings Matter Early in Life", width = 10),
+                                  family = "my_font"),
+                              nudge_y = 120000,
+                              nudge_x = 2) +
+              geom_text_repel(data = 
+                                filter(to_plot, 
+                                       year == 29, 
+                                       type == "returns"),
+                              aes(x = year, 
+                                  y = value,
+                                  col = type,
+                                  label = str_wrap("Investments Dominate Later in Life", width = 10),
+                                  family = "my_font"),
+                              nudge_y = 130000,
+                              nudge_x = -2) +
+              scale_color_manual(values = my_palette, guide = FALSE) +
+              scale_fill_manual(values = my_palette, guide = FALSE) +
+              scale_y_continuous(labels = dollar, limits = c(y_min, y_max), breaks = seq(y_min, y_max, y_unit)) +
+              scale_x_continuous(breaks = seq(0, n_years_working, 5)) +
+              of_dollars_and_data_theme +
+              labs(x = "Years" , y = "Change in Value") +
+              ggtitle(paste0("Savings and Investment Returns Have\nVarying Impact Over Time"))
+  
+  # Add a source and note string for the plots
+  source_string <- "Source:  Simulated data (OfDollarsAndData.com)"
+  note_string   <- paste0("Note:  Assumes an annual mean return of ", 
+                          sample_mean*100,
+                          "% with a ",
+                          sample_sd*100,
+                          "% standard deviation.") 
+  
+  # Turn plot into a gtable for adding text grobs
+  my_gtable   <- ggplot_gtable(ggplot_build(plot))
+  
+  # Make the source and note text grobs
+  source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  
+  # Add the text grobs to the bototm of the gtable
+  my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
+  my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
+  
+  # Save the gtable
+  ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
  
+## Create additional plot to show percentage of total assets attributable to savings vs investment
+assets_df  <- convert_to_df(asset_matrix, "total_assets")
+
+assets_df$pct   <-  1 - ((seq(1, n_years_working + 1) * (income * savings_rate)) / assets_df$value)
+assets_df$type  <- "investment_pct"
+
+## Create 2ndt plot
+  
+  # Set the file path
+  file_path = paste0(exportdir, "16-investing-vs-saving/pct-of-total-assets.jpeg")
+  
+  # Create plot 
+  plot <- ggplot(data = assets_df, aes(x = year, y = pct, fill = type)) +
+    geom_area() +
+    scale_color_manual(values = my_palette, guide = FALSE) +
+    scale_fill_manual(values = my_palette, guide = FALSE) +
+    scale_y_continuous(labels = percent, limits = c(0, 1)) +
+    scale_x_continuous(breaks = seq(0, n_years_working, 5)) +
+    of_dollars_and_data_theme +
+    labs(x = "Years" , y = "Percentage of Total Assets") +
+    ggtitle(paste0("Percentage of Total Assets Attributable To\nInvestment Returns"))
+  
+  # Add a source and note string for the plots
+  source_string <- "Source:  Simulated data (OfDollarsAndData.com)"
+  note_string   <- paste0("Note:  Assumes an annual mean return of ", 
+                          sample_mean*100,
+                          "% with a ",
+                          sample_sd*100,
+                          "% standard deviation.") 
+  
+  # Turn plot into a gtable for adding text grobs
+  my_gtable   <- ggplot_gtable(ggplot_build(plot))
+  
+  # Make the source and note text grobs
+  source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  
+  # Add the text grobs to the bototm of the gtable
+  my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
+  my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
+
+  # Save the gtable
+  ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
+
 # ############################  End  ################################## #
