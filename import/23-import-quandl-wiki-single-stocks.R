9207e2a8f334eb9fd3cd113d69f85fea862a59b5
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-05-02 21:56:53 -0400

    Import Wiki EOD stock prices for select companies
    Use Quandl's API with R.

---
+++ import/23-import-quandl-wiki-single-stocks.R
@@ -0,0 +1,28 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(Quandl)
+library(dplyr)
+
+########################## Start Program Here ######################### #
+
+tickers <- c("AAPL", "AMZN", "TSLA", "XOM")
+
+for (t in 1:length(tickers)){
+  string     <- paste0("WIKI/", tickers[t])
+  temp       <- Quandl(string)
+  out        <- select(temp, Date, `Adj. Close`) %>%
+                  mutate(ticker = tickers[t],
+                         price_plus_div = `Adj. Close`)
+  assign(tickers[t], out, envir = .GlobalEnv)
+}
+
+all_wiki_stocks <- bind_rows(AAPL, AMZN, TSLA, XOM)
+saveRDS(all_wiki_stocks, paste0(localdir, "23-wiki-single-stocks.Rds"))
+
+# ############################  End  ################################## #
