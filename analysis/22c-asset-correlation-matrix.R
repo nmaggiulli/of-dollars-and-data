8f3f00736504db30eba34cf3c71d58347ce2836c
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-05-17 09:19:44 -0400

    Add additional correlation matrix based on requests from readers

---
+++ analysis/22c-asset-correlation-matrix.R
@@ -0,0 +1,57 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(corrplot)
+library(Hmisc)
+
+########################## Start Program Here ######################### #
+
+# Read in BV data
+bv <- readRDS(paste0(localdir, "06-bv-returns.Rds"))
+
+# Create years_list for looping
+years_list <- seq(as.numeric(substr(min(unique(bv$year)), 1, 4)) + 4,
+                  as.numeric(substr(max(unique(bv$year)), 1, 4)),
+                  1)
+
+
+create_corr_matrix <- function(y){
+
+  bv_subset <- filter(bv, 
+                            year > as.POSIXct(paste0(y - 5, "-12-31"), format = "%Y-%m-%d"),
+                            year < as.POSIXct(paste0(y, "-12-31"), format = "%Y-%m-%d")) %>%
+                select(-year)
+  
+  res <- cor(bv_subset)
+  
+  t <- paste0("\nCorrelations From ", y - 4, "-", y)
+  corrplot(res, type = "upper", order = "alphabet",
+           tl.col = "black", tl.srt = 45,
+           title = t)
+}
+
+for (y in years_list){
+  # Create Jpegs
+  jpeg(paste0(exportdir, "22c-corr-matrix/corr-",y, ".jpeg"))
+  create_corr_matrix(y)
+  dev.off()
+}
+
+# Instead of creating these images as a GIF in R, do it in Bash
+# I use Git Bash + magick because this is way faster than creating the GIF in R
+# After navigating to the correct folder, use this command:
+#
+# magick convert -delay 120 loop -0 *.jpeg all_corr_matrix.gif
+#
+# 
+
+
+# ############################  End  ################################## #
