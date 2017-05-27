094f8d125a7f169b0541168c0a740f046f36b37e
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-05-27 14:00:10 -0400

    Update sequence of return risk plots for animation

--- analysis/24-sequence-of-return-plots.R
+++ analysis/24-sequence-of-return-plots.R
@@ -36,11 +36,11 @@ value_path <- data.frame(matrix(NA, nrow = n_years, ncol = 0))
 for (i in 1:n_years){
   value_path[i, "year"] <- i
   if(i == 1){
-    value_path[i, "Bad Returns Early"] <- annual_savings
-    value_path[i, "Bad Returns Later"] <- annual_savings
+    value_path[i, "Negative Returns Early"] <- annual_savings
+    value_path[i, "Negative Returns Later"] <- annual_savings
   } else {
-    value_path[i, "Bad Returns Early"] <- annual_savings + (value_path[(i - 1), "Bad Returns Early"] * (1 + scenario_1[i]))
-    value_path[i, "Bad Returns Later"] <- annual_savings + (value_path[(i - 1), "Bad Returns Later"] * (1 + scenario_2[i]))
+    value_path[i, "Negative Returns Early"] <- annual_savings + (value_path[(i - 1), "Negative Returns Early"] * (1 + scenario_1[i]))
+    value_path[i, "Negative Returns Later"] <- annual_savings + (value_path[(i - 1), "Negative Returns Later"] * (1 + scenario_2[i]))
   }
 }
 
@@ -48,29 +48,81 @@ value_path <- gather(value_path, scenario, value, -year)
   
 for (i in 2:n_years){      
   to_plot <- filter(value_path, year <= i)
+  
   # Set the file_path based on the function input 
-  file_path = paste0(exportdir, "24-sequence-of-return-plots/plot-",i ,".jpeg")
+  if (i < 10){
+    file_path = paste0(exportdir, "24-sequence-of-return-plots/plot-0",i ,".jpeg")
+  } else{
+    file_path = paste0(exportdir, "24-sequence-of-return-plots/plot-",i ,".jpeg")
+  }
   
   # Create title with ticker in subtitle
-  top_title <- paste0("Negative Returns Later in Life\nHave A Larger Impact")
+  top_title <- paste0("Negative Returns Have A Larger Impact\nLater in Life")
   
+  if (i < (n_bad_years + 1)){
   # Create the plot object
   plot <- ggplot(to_plot, aes(x = year, y = value)) +
     geom_line() +
-    geom_point(data=filter(to_plot, year == i), col = "red") +
+    geom_point(data=filter(to_plot, year == i, scenario == "Negative Returns Early"), col = "red") +
+    geom_point(data=filter(to_plot, year == i, scenario == "Negative Returns Later"), col = "green") +
+    geom_text_repel(data=filter(to_plot, year == i, scenario == "Negative Returns Early"),
+                    aes(x = year,
+                        y = value,
+                        label = paste0("-", abs(scenario_1[i])*100, "%"),
+                        family = "my_font"),
+                    col = "red"
+                    ) +
+    geom_text_repel(data=filter(to_plot, year == i, scenario == "Negative Returns Later"),
+                    aes(x = year,
+                        y = value,
+                        label = paste0("+", abs(scenario_2[i])*100, "%"),
+                        family = "my_font"),
+                    col = "green"
+                    ) +
+    geom_vline(xintercept = 10, linetype =  "dashed", col = "black") +
     facet_grid(~scenario) +
     ggtitle(top_title) +
     guides(fill = FALSE) +
     of_dollars_and_data_theme +
     scale_y_continuous(limits = c(0, 200000), label = dollar) +
     scale_x_continuous(limits = c(1, n_years)) +
-    labs(x = "Year", y = "Total Portfolio Value")
+    labs(x = paste0("Year ", i), y = "Total Portfolio Value")
+  } else {
+    plot <- ggplot(to_plot, aes(x = year, y = value)) +
+      geom_line() +
+      geom_point(data=filter(to_plot, year == i, scenario == "Negative Returns Early"), col = "green") +
+      geom_point(data=filter(to_plot, year == i, scenario == "Negative Returns Later"), col = "red") +
+      geom_text_repel(data=filter(to_plot, year == i, scenario == "Negative Returns Early"),
+                      aes(x = year,
+                          y = value,
+                          label = paste0("+", abs(scenario_1[i])*100, "%"),
+                          family = "my_font"),
+                      col = "green"
+      ) +
+      geom_text_repel(data=filter(to_plot, year == i, scenario == "Negative Returns Later"),
+                      aes(x = year,
+                          y = value,
+                          label = paste0("-", abs(scenario_2[i])*100, "%"),
+                          family = "my_font"),
+                      col = "red"
+      ) +
+      geom_vline(xintercept = 10, linetype =  "dashed", col = "black") +
+      facet_grid(~scenario) +
+      ggtitle(top_title) +
+      guides(fill = FALSE) +
+      of_dollars_and_data_theme +
+      scale_y_continuous(limits = c(0, 200000), label = dollar) +
+      scale_x_continuous(limits = c(1, n_years)) +
+      labs(x = paste0("Year ", i), y = "Total Portfolio Value")
+  }
   
   # Turn plot into a gtable for adding text grobs
   my_gtable   <- ggplot_gtable(ggplot_build(plot))
   
   source_string <- "Source: Simulated returns (OfDollarsAndData.com)"
-  note_string   <- paste0("Note:  Assumes a $", annual_savings," annual investment.") 
+  note_string   <- paste0("Note:  Assumes $",  
+                          formatC(as.numeric(annual_savings), format="f", digits=0, big.mark=",")
+                          ," of annual savings over a ", n_years, " year period.") 
   
   # Make the source and note text grobs
   source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
@@ -85,6 +137,12 @@ for (i in 2:n_years){
   # Save the plot  
   ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm") 
 }
+
+# Instead of creating these images as a GIF in R, do it in Bash
+# I use Git Bash + magick because this is way faster than creating the GIF in R
+# After navigating to the correct folder, use this command:
+#
+# magick convert -delay 220 loop -0 *.jpeg all_sequence_plots.gif
 
 # ############################  End  ################################## #
 
