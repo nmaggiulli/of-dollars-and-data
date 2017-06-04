eea1826b6ec08176aeee5948171e394c7ce36b97
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-06-04 16:12:17 -0400

    More changes to the sp500 global maxima plots

--- analysis/26b-sp500-global-max.R
+++ analysis/26b-sp500-global-max.R
@@ -16,6 +16,7 @@ library(gtable)
 library(RColorBrewer)
 library(stringr)
 library(ggrepel)
+library(magrittr)
 
 ########################## Start Program Here ######################### #
 
@@ -57,16 +58,21 @@ base_breaks <- function(n = 10){
   }
 }
 
+# Get the original price for the first date
 orig_price <- as.numeric(sp500_ret_pe[1, "price_plus_div"])
 
-years <- seq(first_year, last_year, 1)
+# Use the original price to rescale to $1 for the first month
+sp500_ret_pe %<>%
+  mutate(price_final = price_plus_div / orig_price)
 
-years <- c(years, rep(last_year, 8))
+# Get the years sequence and add the last year a few extra times for the GIF
+years <- seq(first_year, last_year, 1)
+years <- c(years, rep(last_year, 15))
+
 
 for (i in 2:length(years)){
   to_plot <- sp500_ret_pe %>%
-                filter(Date <= years[i]) %>%
-                mutate(price_final = price_plus_div / orig_price)
+                filter(Date <= years[i])
 
   if(i < 10){
     i_string <- paste0("00", i)
@@ -93,7 +99,7 @@ for (i in 2:length(years)){
   my_gtable   <- ggplot_gtable(ggplot_build(plot))
 
   source_string <- paste0("Source:  http://www.econ.yale.edu/~shiller/data.htm, ", first_year, " - ", last_year," (OfDollarsAndData.com)")
-  note_string   <- paste0("Note:  Red points represent the highest value of the S&P 500 as of that point in time.")
+  note_string   <- paste0("Note:  A red point represents the highest value of the S&P 500 as of that point in time.")
 
   # Make the source and note text grobs
   source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
@@ -117,10 +123,10 @@ for (i in 2:length(years)){
 
 # Section to plot the number of months between global maxima
 global_max_vec <- sp500_ret_pe$global_max
-max_dist_vec    <- c()
-counter <- 1
+max_dist_vec   <- c()
+counter        <- 1
 
-for (j in 2:length(global_max_vec)){
+for (j in 1:length(global_max_vec)){
   if (global_max_vec[j] == 1){
     counter      <- 1
     max_dist_vec <- c(max_dist_vec, counter)
@@ -134,13 +140,15 @@ print(paste0("Max between peaks:", max(max_dist_vec)))
 print(paste0("Avg between peaks:", mean(max_dist_vec)))
 print(paste0("Median between peaks:", median(max_dist_vec)))
 
+# Create a data frame for the max_dist_vec
+df_months <- data.frame(x = sp500_ret_pe$Date, y = max_dist_vec) 
+
 # File path to save plot
 file_path = paste0(exportdir, "26b-sp500-global-max/sp500-peaks-global-maxima.jpeg")
 
-plot <- ggplot(as.data.frame(max_dist_vec), aes(x = sp500_ret_pe[2:nrow(sp500_ret_pe),"Date"], y = max_dist_vec)) +
-  geom_area(fill = "red") +
+plot <- ggplot(to_plot, aes(x = x)) +
+  geom_area(data = df_months, aes(y = y), fill = "blue", stat = "identity") +
   of_dollars_and_data_theme +
-  scale_y_continuous(breaks = seq(0, 150, 25)) + 
   scale_x_continuous(limits = c(1880, 2020), breaks = seq(1880, 2020, 20)) +
   labs(x = "Date", y = "Number of Months From Previous Peak") +
   ggtitle("Most Market Peaks Occur\nIn Consecutive Months")
