04b6dfb0e295eb1bcaff7694081a0fc79ad99f2b
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-06-08 08:57:00 -0400

    Japanese market data import and initial analysis

---
+++ analysis/28-drawdown-plots-wiki-japan.R
@@ -0,0 +1,30 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(grid)
+library(gridExtra)
+library(gtable)
+library(RColorBrewer)
+library(stringr)
+library(ggrepel)
+
+########################## Start Program Here ######################### #
+
+# Read in data for individual stocks and sp500 Shiller data
+jpy_wiki <- readRDS(paste0(localdir, "28-wiki-japan.Rds"))
+
+
+
+
+# ############################  End  ################################## #
+
+  
---
+++ import/28-import-quandl-wiki-japan.R
@@ -0,0 +1,23 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(Quandl)
+library(dplyr)
+
+########################## Start Program Here ######################### #
+
+tickers <- c("NIKKEI/INDEX")
+
+for (t in 1:length(tickers)){
+  string     <- paste0(tickers[t])
+  temp       <- Quandl(string)
+}
+
+saveRDS(temp, paste0(localdir, "28-wiki-japan.Rds"))
+
+# ############################  End  ################################## #
