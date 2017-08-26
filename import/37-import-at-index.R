3d119d62e99bc5fe0b684b79b694aa79c7c11705
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-08-26 17:23:40 -0400

    Import program for at-index

---
+++ import/37-import-at-index.R
@@ -0,0 +1,28 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/nmaggiulli/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(dplyr)
+
+########################## Start Program Here ######################### #
+
+import_and_index <- function(filename, savename){
+  temp <- read.csv(paste0(importdir, "37-at-index/", filename)) %>%
+            arrange(year)
+  first_price <- temp[1, "price"]
+  temp2 <- temp %>%
+            mutate(index = price/first_price)
+  saveRDS(temp2, paste0(localdir, savename))
+}
+
+import_and_index("wheat-prices-farmdoc.csv", "37-wheat-prices-1966-2016.Rds")
+import_and_index("avocado-prices-usda-ycharts.csv", "37-avocado-prices-1966-2016.Rds")
+
+
+
+
+# ############################  End  ################################## #
