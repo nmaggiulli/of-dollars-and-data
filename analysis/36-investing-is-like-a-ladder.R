71061c4b4e0ab817d948e3d534ed9818bec50aad
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-08-26 15:00:34 -0400

    Add results output for post

--- analysis/36-investing-is-like-a-ladder.R
+++ analysis/36-investing-is-like-a-ladder.R
@@ -22,7 +22,7 @@ library(dplyr)
 
 # Read in data for individual stocks and sp500 Shiller data
 sp500_ret_pe    <- readRDS(paste0(localdir, "09-sp500-ret-pe.Rds")) %>%
-                    filter(Date >= "1880-01-01")
+                    filter(Date >= "1920-01-01")
 
 # Calculate returns for the S&P data
 for (i in 1:nrow(sp500_ret_pe)){
@@ -70,11 +70,12 @@ dd        <- drawdown_path(sp500_ret_pe)
 # Create vector of drawdown percentages to loop over
 
 drops <- c(-0.05, -0.1, -0.2, -0.3, -0.4, -0.5)
-#drops <- c(-0.05)
+
+results_df <- data.frame(matrix(NA, nrow=length(drops), ncol=0))
 
 ## These next functions will be used for summary stats
 # Find the amount of time from bottom to peak
-bottom_to_peak <- function(drop){
+bottom_to_peak <- function(drop, j, name){
   limit                <- 0
   n_months_below_limit <- 0
   n_months_recovery    <- c()
@@ -90,12 +91,14 @@ bottom_to_peak <- function(drop){
   }
   
   n_months_recovery_ex_0 <- n_months_recovery[!n_months_recovery %in% c(0)]
-  print(paste0("Average months for a ", drop ," recovery: ", mean(n_months_recovery_ex_0)))
-  print(paste0("Median months for a ", drop ," recovery: ", median(n_months_recovery_ex_0)))
+  results_df[j, "name"]    <- name
+  results_df[j, "b_p_avg"] <- mean(n_months_recovery_ex_0)
+  results_df[j, "b_p_med"] <- median(n_months_recovery_ex_0)
+  assign("results_df", results_df, envir = .GlobalEnv)
 }
 
 # Find the amount of time from peak to bottom
-peak_to_bottom <- function(drop){
+peak_to_bottom <- function(drop, j, name){
   n_months_above_limit <- 0
   n_months_to_drop    <- c()
   limit <- 0
@@ -112,13 +115,15 @@ peak_to_bottom <- function(drop){
   }
   
   n_months_to_drop_ex_0 <- n_months_to_drop[!n_months_to_drop %in% c(0)]
-  print(paste0("Average months for a ", drop ," drop: ", mean(n_months_to_drop_ex_0)))
-  print(paste0("Median months for a ", drop ," drop: ", median(n_months_to_drop_ex_0)))
+  results_df[j, "name"]    <- name
+  results_df[j, "p_b_avg"] <- mean(n_months_to_drop_ex_0)
+  results_df[j, "p_b_med"] <- median(n_months_to_drop_ex_0)
+  assign("results_df", results_df, envir = .GlobalEnv)
 }
 
 # Find the amount of time from peak to known bottom
 # A known bottom is biased in that we know that we will hit a certain drawdown
-peak_to_known_bottom <- function(drop){
+peak_to_known_bottom <- function(drop, j, name){
   n_months_above_limit <- 0
   n_months_to_drop    <- c()
   limit <- 0
@@ -134,8 +139,10 @@ peak_to_known_bottom <- function(drop){
   }
   
   n_months_to_drop_ex_0 <- n_months_to_drop[!n_months_to_drop %in% c(0)]
-  print(paste0("Average months for a known ", drop ," drop: ", mean(n_months_to_drop_ex_0)))
-  print(paste0("Median months for a known ", drop ," drop: ", median(n_months_to_drop_ex_0)))
+  results_df[j, "name"]    <- name
+  results_df[j, "p_kb_avg"] <- mean(n_months_to_drop_ex_0)
+  results_df[j, "p_kb_med"] <- median(n_months_to_drop_ex_0)
+  assign("results_df", results_df, envir = .GlobalEnv)
 }
 
 # Loop through each drop amount
@@ -143,10 +150,6 @@ for (j in 1:length(drops)){
   # Create dataset with drawdown info and S&P 500 prices
   sp500_ret_pe_dd <- sp500_ret_pe %>%
     left_join(dd)
-  
-  bottom_to_peak(drops[j])
-  peak_to_bottom(drops[j])
-  peak_to_known_bottom(drops[j])
   
   if (j == 1){
     name <- "5 Percent"
@@ -161,6 +164,10 @@ for (j in 1:length(drops)){
   } else if (j == 6){
     name <- "50 Percent"
   }
+  
+  bottom_to_peak(drops[j], j, name)
+  peak_to_bottom(drops[j], j, name)
+  peak_to_known_bottom(drops[j], j, name)
   
   # First tag the recoveries
   sp500_ret_pe_dd <- arrange(sp500_ret_pe_dd, Date)
@@ -205,7 +212,7 @@ for (j in 1:length(drops)){
   recoveries_df <- filter(to_plot, recovery == 1, recovery_date != Date, lead(peak) == 1)
   
   # Set the file_path based on the function input 
-  file_path = paste0(exportdir, "36-stocks-ladder/sp500-bottom-peaks-", name, ".jpeg")
+  file_path = paste0(exportdir, "36-fall-fast-rise-slow/sp500-bottom-peaks-", name, ".jpeg")
   
   # Create the plot object
   plot <- ggplot(to_plot, aes(x = Date, y = price_plus_div)) +
@@ -216,7 +223,7 @@ for (j in 1:length(drops)){
                               xmax = as.Date(recovery_date), ymax = price_plus_div),
               fill = "green") +
     geom_line() +
-    ggtitle(paste0("The S&P 500 Drops Fast and Recovers Slowly\n", name, " Drawdowns")) +
+    ggtitle(paste0("The S&P 500 Drops Fast and Recovers Slow\n", name, " Drawdowns")) +
     scale_y_continuous(label = dollar, trans = log_trans(), breaks = c(0, 1, 10, 100, 1000, 10000, 100000, 1000000)) +
     of_dollars_and_data_theme +
     labs(x = "Year", y = "Real Price + Dividends (Log Scale)")
@@ -241,7 +248,10 @@ for (j in 1:length(drops)){
   ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm") 
 }
 
-
+# Export a CSV of the peak to bottom and bottom to peak results
+write.csv(results_df, 
+          paste0(exportdir, "36-fall-fast-rise-slow/result_df.csv"),
+          row.names = FALSE)
 
 
 
