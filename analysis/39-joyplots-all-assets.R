8129160ca3900b09998dc7e09d934b4f447a0a13
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-09-12 20:03:53 -0400

    Create joyplots because they are lit

---
+++ analysis/39-joyplots-all-assets.R
@@ -0,0 +1,90 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+source(file.path("C:/Users/nmaggiulli/git/of-dollars-and-data/header.R"))
+
+########################## Load in Libraries ########################## #
+
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(grid)
+library(gridExtra)
+library(gtable)
+library(RColorBrewer)
+library(stringr)
+library(ggrepel)
+library(lubridate)
+library(tidyr)
+library(ggjoy)
+library(dplyr)
+
+########################## Start Program Here ######################### #
+
+# Load in BV returns
+full_bv_returns <- readRDS(paste0(localdir, "06-bv-returns.Rds"))
+
+# Convert year to a date object
+full_bv_returns$year <- as.Date(full_bv_returns$year, "%d/%m/%y")
+
+min_year <- min(year(full_bv_returns$year))
+max_year <- max(year(full_bv_returns$year))
+
+to_plot <- gather(full_bv_returns, key=key, value=value, -year)
+
+for (yr in 1:20){
+  for (i in yr:nrow(to_plot)){
+    if (to_plot[(i-yr+1), "key"] == to_plot[i, "key"]){
+      start_index <- i - yr + 1
+      to_plot[i, "ret"] <- prod(1 + to_plot[(start_index:i), "value"])^(1/yr) - 1
+    }
+  }
+  
+  if (yr < 10){
+    yr_string <- paste0("0", yr)
+  } else {
+    yr_string <- yr
+  }
+  
+  file_path <- paste0(exportdir, "39-joyplot-all-assets/", yr_string, "-joyplot.jpeg")
+  
+  plot <- ggplot(data = to_plot, aes(x=ret, y=factor(key), fill = factor(key))) +
+    geom_joy_gradient(rel_min_height = 0.01, scale = 3) +
+    scale_fill_discrete(guide = FALSE) +
+    scale_x_continuous(label = percent, limit = c(-0.5, 0.75), breaks = seq(-0.5, 0.75, 0.25)) +
+    of_dollars_and_data_theme +
+    ggtitle(paste0(yr, "-Year Annualized Returns by Asset Class")) +
+    labs(x = paste0(yr, "-Year Annualized Real Return (%)" ), y = "Asset Class")
+  
+  
+  # Add a source and note string for the plots
+  source_string <- paste0("Source:  BullionVault U.S. Asset Class Performance Data, ", min_year, "-", max_year," (OfDollarsAndData.com)")
+  note_string   <- paste0("Note:  Returns are adjusted using the U.S. Consumer Price Index.") 
+  
+  # Turn plot into a gtable for adding text grobs
+  my_gtable   <- ggplot_gtable(ggplot_build(plot))
+  
+  # Make the source and note text grobs
+  source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
+                          gp =gpar(fontfamily = "my_font", fontsize = 8))
+  
+  # Add the text grobs to the bototm of the gtable
+  my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
+  my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
+  
+  # Save the gtable
+  ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
+}
+
+# Instead of creating these images as a GIF in R, do it in Bash
+# I use Git Bash + magick because this is way faster than creating the GIF in R
+# After navigating to the correct folder, use this command:
+#
+# magick convert -delay 35 loop -0 *.jpeg all_plots.gif
+
+# ############################  End  ################################## #
+
+  
