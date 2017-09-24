bd12c0efcd9e7300ca078e038db7da480893e607
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-09-24 12:20:04 -0400

    Update header call to be dynamic

--- analysis/20-positive-lottery.R
+++ analysis/20-positive-lottery.R
@@ -2,7 +2,8 @@ cat("\014") # Clear your console
 rm(list = ls()) #clear your environment
 
 ########################## Load in header file ######################## #
-source(file.path("C:/Users/Nick/git/of-dollars-and-data/header.R"))
+setwd("~/git/of-dollars-and-data")
+source(file.path(paste0(getwd(),"/header.R")))
 
 ########################## Load in Libraries ########################## #
 
