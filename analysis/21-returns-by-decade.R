e6f5f2852b6d9511dd362898fa7f722ed1791c66
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-09-24 12:26:53 -0400

    Fix double comment in call to header

--- analysis/21-returns-by-decade.R
+++ analysis/21-returns-by-decade.R
@@ -2,7 +2,6 @@ cat("\014") # Clear your console
 rm(list = ls()) #clear your environment
 
 ########################## Load in header file ######################## #
-########################## Load in header file ######################## #
 setwd("~/git/of-dollars-and-data")
 source(file.path(paste0(getwd(),"/header.R")))
 
