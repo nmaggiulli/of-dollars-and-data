dc552ebf7d142f28e2fe40d0c24147a62b08c3c7
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-11-05 08:23:33 -0500

    Remove unneeded chart

--- analysis/45-the-constant-reminder.R
+++ analysis/45-the-constant-reminder.R
@@ -27,25 +27,32 @@ n_years_working     <- 40
 income              <- 50000
 annual_return       <- 0.05
 
-savings_rates <- c(0.05, 0.1, 0.15)
-
-results_df <- data.frame(matrix(NA, nrow = n_years_working*length(savings_rates), ncol = 0))
-
-counter <- -n_years_working
-for (s in savings_rates){
-  counter <- counter + n_years_working
+create_value_path <- function(savings_rate, annual_return, name){
+  
+  results_df <- data.frame(matrix(NA, nrow = n_years_working, ncol = 0))
+  
   for (i in 1:n_years_working){
-    results_df[(i + counter), "rate_label"]  <- paste0(s*100, "%")
-    results_df[(i + counter), "rate"]  <- s
-    results_df[(i + counter), "year"]  <- i
+    results_df[i, "rate_label"]  <- paste0(savings_rate*100, "%")
+    results_df[i, "rate"]    <- savings_rate
+    results_df[i , "year"]    <- i
+    results_df[i, "return"]  <- annual_return
     if (i==1){
-      results_df[(i + counter), "value"] <- income * s
+      results_df[i, "value"] <- income * savings_rate
     } else {
-      results_df[(i + counter), "value"] <- (results_df[((i + counter)-1), "value"] * (1+annual_return)) + income*s 
+      results_df[i, "value"] <- (results_df[(i -1), "value"] * (1+annual_return)) + income*savings_rate 
     }
   }
+  
+  assign(name, results_df, envir = .GlobalEnv)
+  print(results_df[n_years_working, "value"])
 }
 
+create_value_path(0.05, annual_return, "sr_5_5")
+create_value_path(0.1, annual_return, "sr_10_5")
+create_value_path(0.15, annual_return, "sr_15_5")
+
+
+results_df <- bind_rows(sr_5_5, sr_10_5, sr_15_5)
 results_df$rate_label <- factor(results_df$rate_label, levels = results_df$rate_label[order(results_df$rate)])
 
 years_to_plot <- c(seq(2, n_years_working, 2), n_years_working, n_years_working)
@@ -96,48 +103,6 @@ for (i in 1:length(years_to_plot)){
 # After navigating to the correct folder, use this command:
 #
 # magick convert -delay 30 loop -0 saving-plot-*.jpeg all_plots.gif
-
-## Plot an overall time series as well
-to_plot <- results_df
-
-# Set the file_path for the next output
-file_path = paste0(exportdir, "45-the-constant-reminder/all-years.jpeg")
-
-plot <- ggplot(data = to_plot, aes(x = year, y = value, col = as.factor(rate))) +
-  geom_line() +
-  scale_color_discrete(guide = FALSE) +
-  ggtitle(paste0("Small Changes in Your Savings Rate\nCan Have A Big Impact")) +
-  geom_text_repel(data = filter(to_plot, year == n_years_working),
-                  aes(x = year, 
-                      y = value,
-                      col = as.factor(rate),
-                      label = paste0(as.factor(round(100*rate)), "%"),
-                      family = "my_font"),
-                  nudge_y = 10000,
-                  max.iter = 3000) +
-  scale_y_continuous(label = dollar) +
-  of_dollars_and_data_theme +
-  labs(x = "Year" , y = "Total Wealth")
-
-# Add a source and note string for the plots
-source_string <- paste0("Source: Simulated data (OfDollarsAndData.com)")
-note_string   <- paste0("Note:  Assumes ", n_years_working, " years of annual savings with a ", annual_return*100,"% annual return.") 
-
-# Turn plot into a gtable for adding text grobs
-my_gtable   <- ggplot_gtable(ggplot_build(plot))
-
-# Make the source and note text grobs
-source_grob <- textGrob(source_string, x = (unit(0.5, "strwidth", source_string) + unit(0.2, "inches")), y = unit(0.1, "inches"),
-                        gp =gpar(fontfamily = "my_font", fontsize = 8))
-note_grob   <- textGrob(note_string, x = (unit(0.5, "strwidth", note_string) + unit(0.2, "inches")), y = unit(0.15, "inches"),
-                        gp =gpar(fontfamily = "my_font", fontsize = 8))
-
-# Add the text grobs to the bototm of the gtable
-my_gtable   <- arrangeGrob(my_gtable, bottom = source_grob)
-my_gtable   <- arrangeGrob(my_gtable, bottom = note_grob)
-
-# Save the gtable
-ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
 
 
 # ############################  End  ################################## #
