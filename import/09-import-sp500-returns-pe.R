678a2e72bedfb2c9385b837aa46da839114771be
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-12-03 13:21:18 -0500

    Update Shiller data import to do cleaning in R

--- import/09-import-sp500-returns-pe.R
+++ import/09-import-sp500-returns-pe.R
@@ -16,9 +16,25 @@ library(dplyr)
 # Build the data in this step
 # Also drop the 2017 data as the dividends are missing
 sp500_ret_pe <- read_excel(paste0(importdir, "09-sp500-returns-pe/ie_data.xls"),
-                          sheet = "cleaned") %>%
+                          sheet = "Data") 
+
+colnames(sp500_ret_pe) <- c("date", "price", "div", "earnings", "cpi", "date_frac", 
+                            "long_irate", "real_price", "real_div", "real_earn", "cape")
+
+#Remove first 6 rows
+sp500_ret_pe <- sp500_ret_pe[7:nrow(sp500_ret_pe),]
+
+# Convert vars to numeric
+sp500_ret_pe$real_price <- as.numeric(sp500_ret_pe$real_price)
+sp500_ret_pe$real_div <- as.numeric(sp500_ret_pe$real_div)
+sp500_ret_pe$date <- as.numeric(sp500_ret_pe$date)
+sp500_ret_pe$cpi <- as.numeric(sp500_ret_pe$cpi)
+sp500_ret_pe$cape <- as.numeric(sp500_ret_pe$cape)
+
+# Filter out missing dividends
+sp500_ret_pe <- sp500_ret_pe %>%
                   filter(!is.na(real_div)) %>%
-                select(Date, real_price, real_div, cape, CPI)
+                  select(date, real_price, real_div, cape, cpi)
 
 # Save down the data
 saveRDS(sp500_ret_pe, paste0(localdir, "09-sp500-ret-pe.Rds"))
