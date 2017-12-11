d716977a1236805418df3fa0d55ae994f81d595f
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2017-12-10 20:10:51 -0500

    New program to install all used libraries

---
+++ install_libraries.R
@@ -0,0 +1,17 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of-dollars-and-data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Install All in Libraries ########################## #
+
+pkgs <- c("dplyr", "ggplot2", "tidyr", "stringr", "lubridate", 
+          "ggjoy", "ggrepel", "scales", "grid", "gridExtra",
+          "RColorBrewer", "quadprog", "fTrading", "quantmod",
+          "gtable", "reshape2", "MASS")
+
+install.packages(pkgs)
+
+# ############################  End  ################################## #
