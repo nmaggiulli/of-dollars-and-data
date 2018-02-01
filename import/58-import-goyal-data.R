3ae919ba27127ec6528a9178a8a0051583cd14f1
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2018-01-31 19:54:05 -0500

    Import Goyal monthly data

---
+++ import/58-import-goyal-data.R
@@ -0,0 +1,20 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of-dollars-and-data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(readxl)
+library(httr)
+
+########################## Start Program Here ######################### #
+
+GET('http://www.hec.unil.ch/agoyal/docs/PredictorData2016.xlsx', write_disk(tf <- tempfile(fileext = ".xlsx")))
+df <- read_excel(tf, sheet="Monthly")
+
+saveRDS(df, paste0(importdir, "58-goyal-data/58-goyal-stock-bond-data.Rds"))
+
+# ############################  End  ################################## #
