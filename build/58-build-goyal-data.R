22a29825f88df7912f156cd56396f930eaa9f10a
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2018-01-31 20:39:39 -0500

    Build Goyal data

---
+++ build/58-build-goyal-data.R
@@ -0,0 +1,30 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of-dollars-and-data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(dplyr)
+
+########################## Start Program Here ######################### #
+
+df <- readRDS(paste0(importdir, "58-goyal-data/58-goyal-stock-bond-data.Rds")) %>%
+        mutate(char_date = as.character(yyyymm),
+              date = as.Date(paste0(substring(char_date, 1, 4),
+                                    "-",
+                                    substring(char_date, 5, 6),
+                                    "-01"),
+                             format = "%Y-%m-%d"),
+              cpi = as.numeric(infl),
+              stock = (Index/lag(Index) - 1) - cpi,
+              corp_bond_aaa = as.numeric(AAA) - cpi,
+              lt_bond = as.numeric(lty) - cpi) %>%
+        filter(!is.nan(cpi)) %>%
+        select(date, stock, lt_bond, corp_bond_aaa, cpi)
+
+saveRDS(df, paste0(localdir, "58-goyal-stock-bond-data.Rds"))
+
+# ############################  End  ################################## #
