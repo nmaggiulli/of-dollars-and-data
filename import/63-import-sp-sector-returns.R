cc8b719c598148c80bfbb55f6e812085f6cf82e4
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2018-03-07 10:42:51 -0500

    Import sp500 sector returns data

---
+++ import/63-import-sp-sector-returns.R
@@ -0,0 +1,30 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of-dollars-and-data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(readxl)
+library(dplyr)
+
+########################## Start Program Here ######################### #
+
+df <- read_excel(paste0(importdir, "63-sector-returns/sp500_sector_data.xlsx"), skip = 1) %>%
+        filter(!(is.na(`Start Date`) | `Start Date` == 'Totals')) %>%
+        mutate(security = trimws(gsub("TR\\s?USD \\(USD\\)", 
+                               "", 
+                               gsub("S&P 500 Sec/", 
+                                    "", 
+                                    `Security Name`
+                               )
+                          )),
+               date = as.Date(as.numeric(`Start Date`), origin = "1899-12-31"),
+               ret = `Total Return %`/100) %>%
+        select(security, date, ret)
+
+saveRDS(df, paste0(localdir, "63_sp500_sector_returns.Rds"))
+
+# ############################  End  ################################## #
