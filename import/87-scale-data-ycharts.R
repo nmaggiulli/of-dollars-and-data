a6ea943bfd7653c8b98ecb7bbfcd8058cc41e179
Nick Maggiulli <nick@ritholtzwealth.com>
2018-08-24 14:05:30 -0400

    Make import more dynamic and straight from the raw ycharts download

--- import/87-scale-data-ycharts.R
+++ import/87-scale-data-ycharts.R
@@ -17,21 +17,23 @@ folder_name <- "87-scale-data-ycharts"
 
 ########################## Start Program Here ######################### #
 
-
 read_in_ycharts <- function(filename){
-df <- read_excel(paste0(importdir, folder_name, "/", filename)) %>%
-        gather(key=key, value=value, -year) %>%
+df <- read.csv(paste0(importdir, folder_name, "/", filename)) %>%
+      gather(key=key, value=value, -Period) %>%
+        mutate(year = substr(Period, 1, 4),
+               company = gsub("(.*?)(\\.Inc|\\.Corp).*", "\\1", key),
+               measure = gsub(".*?(Inc\\.|Corp\\.)(.*?)\\.\\.Annual\\.", "\\2", key)
+        ) %>%
         filter(!is.na(value)) %>%
-        group_by(year, key) %>%
+        group_by(year, company, measure) %>%
         summarize(value = mean(value, na.rm = TRUE)) %>%
-        separate(key, sep= "_", into = c("company", "measure")) %>%
         spread(key=measure, value=value) %>%
         ungroup()
   return(df)
 }
 
-er <- read_in_ycharts("employees_rev.xlsx")
-ai <- read_in_ycharts("assets_netinc.xlsx")
+er <- read_in_ycharts("employees_rev.csv")
+ai <- read_in_ycharts("assets_netinc.csv")
 
 df <- er %>% full_join(ai)
 
