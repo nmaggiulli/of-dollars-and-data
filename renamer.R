dfdd579ecafc404d56bdaf95b5e0c5a11ea3eed9
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2018-10-14 10:36:34 -0400

    Rename analysis programs

--- renamer.R
+++ renamer.R
@@ -13,23 +13,24 @@ library(tidyverse)
 
 renamer <- function(folder){
 
-root_folder <- paste0(programroot, folder)  
+  root_folder <- paste0(exportdir, folder)  
+    
+  list <- data.frame(file_name = list.files(path = root_folder)) %>%
+    mutate(lead_number = gsub("(\\d+[a-z]?)-.*", "\\1", file_name),
+      new_file_name = paste0(root_folder, "00", str_replace_all(file_name, "-", "_")),
+      file_name = paste0(root_folder, file_name)) 
   
-list <- data.frame(file_name = list.files(path = root_folder)) %>%
-  mutate(lead_number = gsub("(\\d+[a-z]?)-.*", "\\1", file_name),
-    new_file_name = paste0(root_folder, "/00", str_replace_all(file_name, "-", "_")),
-    file_name = paste0(root_folder, "/", file_name)) 
+  assign("list", list, envir = .GlobalEnv)
 
-for (i in 1:nrow(list)){
-  orig <- list[i, "file_name"]
-  new <- list[i, "new_file_name"]
-  
-  file.rename(orig, new)
+  for (i in 1:nrow(list)){
+    orig <- list[i, "file_name"]
+    new <- list[i, "new_file_name"]
+    
+    file.rename(orig, new)
+  }
 }
 
-}
-
-renamer("build")
+#renamer("")
 
 
 # ############################  End  ################################## #
