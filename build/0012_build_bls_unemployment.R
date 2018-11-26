7e63801d0f8f70ea6e3632b858a45db3a77255a3
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2018-11-26 12:01:40 -0800

    Fix folder name

--- build/0012_build_bls_unemployment.R
+++ build/0012_build_bls_unemployment.R
@@ -23,15 +23,15 @@ loop_list <- c("data.0.CurrentU90-94",
 # Load in raw BLS UE data and stack it
 for (i in loop_list){
   if (i == "data.0.CurrentU90-94"){
-    ue_stack <- readRDS(paste0(importdir, "12-bls-unemployment/bls_unemployment_", i, ".Rds"))
+    ue_stack <- readRDS(paste0(importdir, "0012_bls_unemployment/bls_unemployment_", i, ".Rds"))
   } else{
-    temp <- readRDS(paste0(importdir, "12-bls-unemployment/bls_unemployment_", i, ".Rds"))
+    temp <- readRDS(paste0(importdir, "0012_bls_unemployment/bls_unemployment_", i, ".Rds"))
     ue_stack <- rbind(ue_stack, temp)
   }
 }
 
 # Load in the area and area_type
-area <- readRDS(paste0(importdir, "12-bls-unemployment/bls_unemployment_area.Rds"))
+area <- readRDS(paste0(importdir, "0012_bls_unemployment/bls_unemployment_area.Rds"))
 
 area <- select(area, area_type_code, area_code, row.names) %>%
           mutate(area_text = area_code, 
@@ -39,7 +39,7 @@ area <- select(area, area_type_code, area_code, row.names) %>%
                  area_type_code = row.names) %>%
             select(area_code, area_text, area_type_code)
 
-area_type <- readRDS(paste0(importdir, "12-bls-unemployment/bls_unemployment_area_type.Rds"))
+area_type <- readRDS(paste0(importdir, "0012_bls_unemployment/bls_unemployment_area_type.Rds"))
 
 area_type <- mutate(area_type, area_type_text = area_type_code,
                     area_type_code = rownames(area_type)) %>%
@@ -50,7 +50,7 @@ areas <- area %>%
           select(area_code, area_text, area_type_text, area_type_code)
 
 # Load in measure data as well
-measure <- readRDS(paste0(importdir, "12-bls-unemployment/bls_unemployment_measure.Rds"))
+measure <- readRDS(paste0(importdir, "0012_bls_unemployment/bls_unemployment_measure.Rds"))
 measure <- mutate(measure, 
                   measure_text = measure_code,
                   measure_code = rownames(measure))
@@ -66,6 +66,6 @@ ue_stack <- ue_stack %>%
               left_join(measure)
 
 # Save down RDS
-saveRDS(ue_stack, paste0(localdir, "12-bls-ue.Rds"))
+saveRDS(ue_stack, paste0(localdir, "12_bls_ue.Rds"))
 
 # ############################  End  ################################## #
