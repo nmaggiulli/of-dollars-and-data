bc3b6a58c40a803cc2ff51af9127526db86a8216
Nick Maggiulli <nick@ritholtzwealth.com>
2018-11-28 09:53:25 -0500

    Build the data instead of doing it in each analysis program

--- import/0009_import_sp500_returns_pe.R
+++ import/0009_import_sp500_returns_pe.R
@@ -16,7 +16,7 @@ library(dplyr)
 # Bring in the data Shiller uses on his website
 # Build the data in this step
 # Also drop the 2017 data as the dividends are missing
-sp500_ret_pe <- read_excel(paste0(importdir, "09-sp500-returns-pe/ie_data.xls"),
+sp500_ret_pe <- read_excel(paste0(importdir, "0009_sp500_returns_pe/ie_data.xls"),
                           sheet = "Data") 
 
 colnames(sp500_ret_pe) <- c("date", "price", "div", "earnings", "cpi", "date_frac", 
@@ -40,6 +40,28 @@ sp500_ret_pe <- sp500_ret_pe %>%
                   filter(!is.na(date), date < end_date) %>%
                   mutate(real_div = ifelse(is.na(real_div), 0, real_div)) %>%
                   select(date, real_price, real_div, cape, cpi)
+
+# Calculate returns for the S&P data
+for (i in 1:nrow(sp500_ret_pe)){
+  if (i == 1){
+    sp500_ret_pe[i, "n_shares"]       <- 1
+    sp500_ret_pe[i, "new_div"]        <- sp500_ret_pe[i, "n_shares"] * sp500_ret_pe[i, "real_div"]
+    sp500_ret_pe[i, "price_plus_div"] <- sp500_ret_pe[i, "n_shares"] * sp500_ret_pe[i, "real_price"]
+  } else{
+    sp500_ret_pe[i, "n_shares"]       <- sp500_ret_pe[(i - 1), "n_shares"] + sp500_ret_pe[(i-1), "new_div"]/ 12 / sp500_ret_pe[i, "real_price"]
+    sp500_ret_pe[i, "new_div"]        <- sp500_ret_pe[i, "n_shares"] * sp500_ret_pe[i, "real_div"]
+    sp500_ret_pe[i, "price_plus_div"] <- sp500_ret_pe[i, "n_shares"] * sp500_ret_pe[i, "real_price"]
+  }
+}
+
+# Change the Date to a Date type for plotting the S&P data
+sp500_ret_pe <- select(sp500_ret_pe, date, price_plus_div) %>%
+  mutate(date = as.Date(paste0(
+    substring(as.character(date), 1, 4),
+    "-", 
+    ifelse(substring(as.character(date), 6, 7) == "1", "10", substring(as.character(date), 6, 7)),
+    "-01", 
+    "%Y-%m-%d")))
 
 # Save down the data
 saveRDS(sp500_ret_pe, paste0(localdir, "0009_sp500_ret_pe.Rds"))
