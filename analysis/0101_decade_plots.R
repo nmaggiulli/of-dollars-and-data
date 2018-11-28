efa660727afa1ae41d928d92195772def2d74231
Nick Maggiulli <nick@ritholtzwealth.com>
2018-11-28 09:53:43 -0500

    Initial draft of decade plots

---
+++ analysis/0101_decade_plots.R
@@ -0,0 +1,27 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of_dollars_and_data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(lubridate)
+library(scales)
+library(tidyverse)
+
+folder_name <- "0101_decade_plots"
+out_path <- paste0(exportdir, folder_name)
+dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
+
+########################## Start Program Here ######################### #
+
+# Read in data for individual stocks and sp500 Shiller data
+sp500_ret_pe    <- readRDS(paste0(localdir, "0009_sp500_ret_pe.Rds"))
+
+
+
+
+
+# ############################  End  ################################## #
