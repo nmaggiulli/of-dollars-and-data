aebdea86afafc944ac6a269a36bc77acd098d27f
Nick Maggiulli <nick@ritholtzwealth.com>
2018-12-03 10:44:25 -0500

    Fix date labels

--- analysis/xxxx_japan_dca.R
+++ analysis/xxxx_japan_dca.R
@@ -53,21 +53,42 @@ plot_jpy_value <- function(date_start, n_years){
   
   assign("to_plot", to_plot, envir = .GlobalEnv)
   
+  total_cont <- 100 * nrow(filtered)
+  
   # Set the file_path
   file_path <- paste0(out_path, "/value_dca_", date_start_string, ".jpeg")
-  
-  # Set note and source string
-  source_string <- str_wrap("Source: YCharts (OfDollarsAndData.com)",
-                            width = 85)
-  note_string   <- str_wrap(paste0("Note: Assumes a monthly contribution of $100."), 
-                            width = 85)
   
   first_year <- year(min(to_plot$date))
   last_year <- year(max(to_plot$date))
   
+  n_year_cont <- min(n_years, last_year - first_year + 1)
+  
+  # Set note and source string
+  source_string <- str_wrap("Source: YCharts (OfDollarsAndData.com)",
+                            width = 85)
+  note_string   <- str_wrap(paste0("Note: Assumes a monthly contribution of $100 for a total contribution of $", formatC(total_cont, format="f", big.mark = ",", digits = 0), " across ", n_year_cont, " years."), 
+                            width = 85)
+  
+  min_dt_decade <- as.Date(paste0(year(min(to_plot$date)) - year(min(to_plot$date)) %% 10, "-01-01", format = "%Y-%m-%d"))
+  max_dt_decade <- as.Date(paste0(year(max(to_plot$date)) - year(max(to_plot$date)) %% 10, "-01-01", format = "%Y-%m-%d"))
+  
+  last_point <- filter(to_plot, row_number() == nrow(to_plot))
+  
   plot <- ggplot(to_plot, aes(x=date, y=value)) +
     geom_line() +
     scale_y_continuous(label = dollar) +
+    scale_x_date(breaks = seq.Date(min_dt_decade, max_dt_decade, "10 years"), date_labels = "%Y") +
+    geom_point(data = last_point, 
+                    aes(x=date, y=value),
+                    col = "red") +
+    geom_text_repel(data = last_point, 
+                    aes(x=date, y=value),
+                    col = "red",
+                    label = paste0("$",formatC(last_point$value, format="f", big.mark = ",", digits = 0)),
+                    max.iter = 3000,
+                    nudge_y = 25000,
+                    nudge_x = -200,
+                    segment.color = "transparent") +
     of_dollars_and_data_theme +
     ggtitle(paste0("Dollar Cost Averaging into Japan\n", first_year, "-", last_year)) +
     labs(x = "Date", y="Portfolio Value",
@@ -86,8 +107,9 @@ plot_jpy_value <- function(date_start, n_years){
   file_path <- paste0(out_path, "/dd_dca_", date_start_string, ".jpeg")
   
   plot <- ggplot(dd, aes(x = date, y = pct)) +
-    geom_area(position = "identity", alpha = 0.4, fill = "red") +
+    geom_area(position = "identity", fill = "red") +
     scale_y_continuous(label = percent) +
+    scale_x_date(breaks = seq.Date(min_dt_decade, max_dt_decade, "10 years"), date_labels = "%Y") +
     of_dollars_and_data_theme +
     ggtitle(paste0("Drawdowns for Japanese DCA Strategy\n", first_year, "-", last_year)) +
     labs(x = "Date" , y = "Percentage of Value Lost",
@@ -100,7 +122,8 @@ plot_jpy_value <- function(date_start, n_years){
   ggsave(file_path, my_gtable, width = 15, height = 12, units = "cm")
 }
 
-plot_jpy_value("1970-01-01", 40)
-plot_jpy_value("1980-01-01", 40)
+plot_jpy_value("1970-01-01", 48)
+plot_jpy_value("1980-01-01", 38)
+plot_jpy_value("1985-01-01", 33)
 
 # ############################  End  ################################## #
