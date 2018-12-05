1932efac059fa8f2ae8d4495926844d751b34a1f
Nick Maggiulli <nick@ritholtzwealth.com>
2018-12-05 16:12:32 -0500

    Simulate hot hand stats

--- analysis/xxxx_simulate_hot_hand.R
+++ analysis/xxxx_simulate_hot_hand.R
@@ -27,28 +27,16 @@ dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
 ########################## Start Program Here ######################### #
 
-df <- read_excel(paste0(importdir, "0097_spx_daily/spx_daily.xlsx")) %>%
-        rename(date = Period,
-               index = `S&P 500 Level`) %>%
-        mutate(ret = index/lag(index) - 1,
-               pos = ifelse(ret > 0 , 1, 0)) %>%
-        filter(!is.na(ret)) %>%
-        select(date, ret, pos)
+set.seed(12345)
 
-df <- read_excel(paste0(importdir, "xxxx_dow_daily/dow_daily.xlsx")) %>%
-      rename(date = Date,
-             index = PX_LAST) %>%
-      mutate(ret = index/lag(index) - 1,
-             pos = ifelse(ret > 0 , 1, 0)) %>%
-      filter(!is.na(ret)) %>%
-      select(date, ret, pos)
+df <- data.frame(pos=sample(0:1, 10000, replace = TRUE))
 
 for (i in 1:nrow(df)){
   pos <- df[i, "pos"]
   
   if(i == 1){
-      df[i, "pos_streak"] <- pos
-      df[i, "neg_streak"] <- (1 - pos)
+    df[i, "pos_streak"] <- pos
+    df[i, "neg_streak"] <- (1 - pos)
   } else {
     prior_pos <- df[(i-1), "pos"]
     prior_pos_streak <- df[(i-1), "pos_streak"]
@@ -71,36 +59,36 @@ for (i in 1:nrow(df)){
 }
 
 df <- df %>%
-        mutate(pos_next = lead(pos),
-               next_ret = lead(ret)) %>%
-        filter(!is.na(pos_next))
+  mutate(pos_next = lead(pos)) %>%
+  filter(!is.na(pos_next))
+
+t <- filter(df, pos == 1) %>%
+        summarize(pos = mean(df$pos),
+                  pos_next = mean(pos_next))
 
 final_results <- data.frame(pos_neg = c(), streak_length = c(),
                             pct_next_day_same = c(), pct_next_day_change = c(),
-                            ret_next_day = c(), sd_next_day = c(), 
                             t_pval = c(), n_days = c())
 
 counter <- 1
 for (i in 1:max(df$pos_streak, df$neg_streak)){
-    summary <- df %>%
-                filter(pos_streak >= i)
+  summary <- df %>%
+    filter(pos_streak >= i)
   
-    final_results[counter, "pos_neg"]             <- "Positive"
-    final_results[counter, "streak_length"]       <- i
-    final_results[counter, "pct_next_day_same"]   <- mean(summary$pos_next)
-    final_results[counter, "pct_next_day_change"] <- 1 - mean(summary$pos_next)
-    final_results[counter, "ret_next_day"]        <- mean(summary$next_ret)
-    final_results[counter, "sd_next_day"]         <- sd(summary$next_ret)
-    
-    final_results[counter, "n_days"]              <- nrow(summary)
-    
+  final_results[counter, "pos_neg"]             <- "Positive"
+  final_results[counter, "streak_length"]       <- i
+  final_results[counter, "pct_next_day_same"]   <- mean(summary$pos_next)
+  final_results[counter, "pct_next_day_change"] <- 1 - mean(summary$pos_next)
+  
+  final_results[counter, "n_days"]              <- nrow(summary)
+  
   if (final_results[counter, "n_days"] >= 100){
     final_results[counter, "t_pval"]  <- t.test(df$pos, summary$pos_next)$p.value
   } else {
     final_results[counter, "t_pval"]  <- NA
   }
   counter <- counter + 1
-
+  
   summary <- df %>%
     filter(neg_streak >= i)
   
@@ -108,8 +96,6 @@ for (i in 1:max(df$pos_streak, df$neg_streak)){
   final_results[counter, "streak_length"]       <- i
   final_results[counter, "pct_next_day_same"]   <- 1-mean(summary$pos_next)
   final_results[counter, "pct_next_day_change"] <- mean(summary$pos_next)
-  final_results[counter, "ret_next_day"]        <- mean(summary$next_ret)
-  final_results[counter, "sd_next_day"]         <- sd(summary$next_ret)
   final_results[counter, "n_days"]              <- nrow(summary)
   
   if (final_results[counter, "n_days"] >= 100){
@@ -122,6 +108,6 @@ for (i in 1:max(df$pos_streak, df$neg_streak)){
 }
 
 final_results <- final_results %>%
-                  mutate(sig_diff = ifelse(t_pval < 0.05, 1, 0))
+  mutate(sig_diff = ifelse(t_pval < 0.05, 1, 0))
 
 # ############################  End  ################################## #
