1768f9595b5199de6cdd08078cf745e4d2a69341
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2019-02-18 19:43:08 -0500

    Loop through savings rates to make plots

--- analysis/0113_life_cycle_simulation.R
+++ analysis/0113_life_cycle_simulation.R
@@ -31,12 +31,11 @@ colnames(dfa_data) <- c("date", "cpi", "ret_tbill", "ret_sp500", "ret_treasury_5
 max_date <- max(dfa_data$date)
 
 run_life_cycle <- function(working_years, spending_years, 
-                           spending_rate, 
+                           savings_rate, 
                            starting_date, wt_sp500){
   
   #Starting salary is arbitrary
   starting_salary <- 40000
-  savings_rate <- 0.15
   starting_date <- as.Date(starting_date)
   
   total_years <- working_years + spending_years
@@ -73,7 +72,7 @@ run_life_cycle <- function(working_years, spending_years,
       if (i != (working_years*12 + 1)){
         df[i, 4] <- df[(i-1), 4] * (1 + cpi)
       } else {
-        df[i, 4] <- (df[(i-1), 5] +  df[(i-1), 6]) * (spending_rate/12) * -1
+        df[i, 4] <- (df[(i-1), 4]/savings_rate)*(1 - savings_rate) * -1
       } 
       
       # Initiate a rebalance every 12 months
@@ -97,7 +96,7 @@ run_life_cycle <- function(working_years, spending_years,
 
 
 run_all_life_cycles <- function(working_years, spending_years, 
-                                spending_rate_low, spending_rate_high,
+                                savings_rate_low, savings_rate_high,
                                 wt_sp500_low, wt_sp500_high,
                                 first_date){
   
@@ -107,19 +106,20 @@ run_all_life_cycles <- function(working_years, spending_years,
   assign("all_dates", all_dates, envir = .GlobalEnv)
   
   counter <- 1
-  for(sp in seq(spending_rate_low, spending_rate_high, 0.01)){
-    for(wt in seq(wt_sp500_low, wt_sp500_high, 0.1)){
+  for(sr in seq(savings_rate_low, savings_rate_high, 0.01)){
+    for(wt in seq(wt_sp500_low, wt_sp500_high, 0.2)){
       for(i in 1:length(all_dates)){
         dt <- all_dates[i]
         print(dt)
         
-        m  <- run_life_cycle(working_years, spending_years, sp, dt, wt)
+        m  <- run_life_cycle(working_years, spending_years, sr, dt, wt)
+        assign(paste0("m_", counter), m, envir = .GlobalEnv)
         mf <- na.omit(m[m[,4] < 0, 4])
 
         df <- data.frame(starting_date = dt + years(working_years),
                working_years = working_years,
-               spending_rate = paste0(100*sp, "%"),
-               weight_sp500 = paste0(100*wt, "% S&P"),
+               savings_rate = sr,
+               weight_sp500 = paste0(100*wt, "/", 100*(1-wt), " Stock/Bond"),
                n_years_survival = length(mf)/12
         )
         if(counter == 1){
@@ -136,20 +136,58 @@ run_all_life_cycles <- function(working_years, spending_years,
   assign(paste0("all_results"), final_df, envir=.GlobalEnv)
 }
 
-run_all_life_cycles(40, 
-                    25, 
-                    0.03, 0.08,
-                    0.2, 0.8,
+# Set savings rates, working years, and spending years
+min_sr       <- 0.05
+max_sr       <- 0.3
+working_yrs  <- 40
+spending_yrs <- 25
+
+
+run_all_life_cycles(working_yrs, 
+                    spending_yrs, 
+                    min_sr, max_sr,
+                    0.4, 0.8,
                     "1926-01-01")
 
-to_plot <- all_results 
+for (sr in seq(min_sr, max_sr, 0.01)){
+  
+  if(sr < 0.1){
+    sr_string <- paste0("0", 100*sr)
+  } else{
+    sr_string <- paste0(100*sr)
+  }
+  
+  file_path <- paste0(out_path, "/survival_years_", sr_string, ".jpeg")
 
-ggplot(to_plot, aes(x=starting_date, y = n_years_survival, col = as.factor(weight_sp500))) +
-  facet_wrap(~spending_rate) +
-  geom_line() +
-  of_dollars_and_data_theme +
-  theme(legend.position = "bottom",
-        legend.title = element_blank())
+  source_string <- paste0("Source:  DFA, 1926-2018 (OfDollarsAndData.com)")
+  note_string <- str_wrap(paste0("Note: Assumes income grows with inflation while saved money grows at the portfolio return level.   ",
+                                 "Spending in the first retirement year is indexed to inflation.  ",
+                                 "'Stocks' are represented by the S&P 500 and 'Bonds' are represented by 5-Year U.S. Treasuries.  ",
+                                 "The portfolio is rebalanced annually."), 
+                          width = 80)
+    
+  to_plot <- all_results %>%
+              filter(savings_rate == sr)
+  
+  plot <- ggplot(to_plot, aes(x=starting_date, y = n_years_survival, col = as.factor(weight_sp500), linetype = as.factor(weight_sp500))) +
+            geom_line() +
+            scale_y_continuous(limits = c(0, spending_yrs), breaks = seq(0, spending_yrs, 5)) +
+            of_dollars_and_data_theme +
+            theme(legend.position = "bottom",
+                  legend.title = element_blank()) +
+            ggtitle(paste0("Number of Years Until You Run Out of Money\nWith a ", 100*sr, "% Savings Rate")) +
+            labs(x="Retirement Start Year", y="Number of Years",
+                 caption = paste0(source_string, "\n", note_string))
+  
+  # Save the plot
+  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+}
+
+create_gif(out_path,
+           paste0("survival_years_*.jpeg"),
+           40,
+           0,
+           paste0("_gif_all_savings_rates.gif"))
 
 
 # ############################  End  ################################## #
