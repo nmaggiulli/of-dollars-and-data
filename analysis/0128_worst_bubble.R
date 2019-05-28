b802c170e776238824e709b057b2873f138a0b8f
Nick Maggiulli <nick@ritholtzwealth.com>
2019-05-28 13:01:17 -0400

    Plot Japan and Bitcoin plots

--- analysis/0128_worst_bubble.R
+++ analysis/0128_worst_bubble.R
@@ -31,15 +31,99 @@ colnames(fred_jpy_real_gdp) <- c("date", "real_gdp")
 
 jpy <- fred_jpy_mcap_to_gdp %>%
           left_join(fred_jpy_real_gdp) %>%
-          mutate(mcap_billions = mcap_to_gdp/100*real_gdp/1000) %>%
+          mutate(mcap_billions = mcap_to_gdp/100*real_gdp/1000,
+                 date = as.Date(date)) %>%
           filter(!is.na(mcap_billions)) %>%
           select(date, mcap_billions)
 
 bcoin_mcap <- read_excel(paste0(importdir, "0128_bubble_data/bitcoin_coin_market_cap.xlsx")) %>%
-                mutate(mcap_billions = mcap/(10^9)) %>%
-                select(date, mcap_billions) 
+                mutate(mcap_billions = mcap/(10^9),
+                       date = as.Date(date)) %>%
+                select(date, mcap_billions) %>%
+                arrange(date)
 
+# Reset file path
+file_path <- paste0(out_path, "/jpy_mcap.jpeg")
 
+# Set source/note
+source_string <- paste0("Source:  FRED (OfDollarsAndData.com)")
+note_string   <- str_wrap(paste0("Note:  Japan's market capitalization is listed in 2011 dollars."), 
+                          width = 85)
 
+to_plot <- jpy
+
+# Dates of interest for Japan
+bubble_high <- as.Date("1989-01-01")
+bubble_low <- as.Date("1998-01-01")
+
+text_labels <- data.frame(date = c(bubble_high, bubble_low))
+
+text_labels[1, "mcap_billions"] <- filter(to_plot, date == bubble_high) %>% pull(mcap_billions)
+text_labels[1, "label"] <- paste0("$", formatC(round(text_labels[1, "mcap_billions"],0), 
+                                               big.mark = ",", format = "f", digits = 0))
+text_labels[2, "mcap_billions"] <- filter(to_plot, date == bubble_low) %>% pull(mcap_billions)
+text_labels[2, "label"] <- paste0("$",  formatC(round(text_labels[2, "mcap_billions"],0), 
+                                                big.mark = ",", format = "f", digits = 0))
+
+plot <- ggplot(to_plot, aes(x=date, y=mcap_billions)) +
+  geom_line() +
+  geom_point(data=text_labels, aes(x=date, y=mcap_billions), col = ifelse(text_labels$date == bubble_high, "green","red")) +
+  scale_y_continuous(label = dollar) +
+  geom_text_repel(data=text_labels, aes(x=date, y=mcap_billions),
+                  color = ifelse(text_labels$date == bubble_high, "green","red"),
+                  label = text_labels$label,
+                  size = 3.5,
+                  family = "my_font",
+                  max.iter = 1,
+                  segment.colour = "transparent",
+                  nudge_y = ifelse(text_labels$date == bubble_high, 0, -200),
+                  nudge_x = ifelse(text_labels$date == bubble_high, -800, 0)) +
+  of_dollars_and_data_theme +
+  ggtitle(paste0("Japan's Stock Market Lost Over $2 Trillion\nAfter the Bubble Burst")) +
+  labs(x="Date", y="Market Capitalization\n(in billions)",
+       caption = paste0("\n", source_string, "\n", note_string))
+
+# Save the plot
+ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+
+# Reset file path
+file_path <- paste0(out_path, "/bcoin_mcap.jpeg")
+
+# Set source/note
+source_string <- paste0("Source:  CoinMarketCap.com (OfDollarsAndData.com)")
+
+# Dates of interest for Bitcoin
+bubble_high <- as.Date("2017-12-16")
+bubble_low <- as.Date("2018-12-15")
+
+text_labels <- data.frame(date = c(bubble_high, bubble_low))
+
+text_labels[1, "mcap_billions"] <- filter(bcoin_mcap, date == bubble_high) %>% pull(mcap_billions)
+text_labels[1, "label"] <- paste0("$", formatC(round(text_labels[1, "mcap_billions"],0), 
+                                               big.mark = ",", format = "f", digits = 0))
+text_labels[2, "mcap_billions"] <- filter(bcoin_mcap, date == bubble_low) %>% pull(mcap_billions)
+text_labels[2, "label"] <- paste0("$",  formatC(round(text_labels[2, "mcap_billions"],0), 
+                                                big.mark = ",", format = "f", digits = 0))
+
+plot <- ggplot(bcoin_mcap, aes(x=date, y=mcap_billions)) +
+  geom_line() +
+  geom_point(data=text_labels, aes(x=date, y=mcap_billions), col = ifelse(text_labels$date == bubble_high, "green","red")) +
+  scale_y_continuous(label = dollar) +
+  geom_text_repel(data=text_labels, aes(x=date, y=mcap_billions),
+                  color = ifelse(text_labels$date == bubble_high, "green","red"),
+                  label = text_labels$label,
+                  size = 3.5,
+                  family = "my_font",
+                  max.iter = 1,
+                  segment.colour = "transparent",
+                  nudge_y = ifelse(text_labels$date == bubble_high, 0, -15),
+                  nudge_x = ifelse(text_labels$date == bubble_high, -100, 0)) +
+  of_dollars_and_data_theme +
+  ggtitle(paste0("Bitcoin Lost Over $200 Billion\nFollowing Its 2017 Peak")) +
+  labs(x="Date", y="Market Capitalization\n(in billions)",
+       caption = paste0("\n", source_string))
+
+# Save the plot
+ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 
 # ############################  End  ################################## #
