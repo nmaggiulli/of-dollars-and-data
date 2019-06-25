865a5c0fdc68709b750b3661205ef114267fd795
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2019-06-24 22:39:03 -0700

    More analysis of Lump Sum vs Annuity

--- analysis/0131_annuity_or_lump_sum.R
+++ analysis/0131_annuity_or_lump_sum.R
@@ -24,37 +24,156 @@ dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 hist_bond_stock <- readRDS(paste0(localdir, "0021_historical_returns_sp500_bond_damodaran.Rds")) %>%
                     mutate(ret_60_40 = 0.6*ret_sp500 + 0.4*ret_10yr_bond)
 
+# Bring in SSA life table
+ssa_life <- read_excel(paste0(importdir, "/0131_lump_sum_annuity/2016_ssa_life_table.xlsx"))
+
+# Calculate her survival probability
+death_p_female_45_61 <- ssa_life %>%
+                            filter(age >= 45, age <= 61) %>%
+                            mutate(female_death_p = female_death_p + 1) %>%
+                            summarize(cumulative_female_death_p = prod(female_death_p) - 1) %>%
+                            pull()
+
+death_p_female_85_86 <- ssa_life %>%
+                        filter(age >= 84, age <= 86) %>%
+                        mutate(female_death_p = female_death_p + 1) %>%
+                        summarize(cumulative_female_death_p = prod(female_death_p) - 1) %>%
+                        pull()
+
 # Find average 60/40 return
 mean(hist_bond_stock$ret_60_40)
 
+# Write a function
+lump_sum_v_annuity <- function(client_staring_age,
+                               n_years_work, 
+                               n_years_retire, 
+                               pmt_annuty, 
+                               lump_sum,
+                               ret_work,
+                               ret_retire){
+  
+  # Calculate value of annuity at time of retirement
+  pv_annuity <- pv(r = ret_retire,
+                   n = n_years_retire,
+                   fv = 0,
+                   pmt = -pmt_annuity,
+                   type = 1)
+  
+  fv_lump_sum <- fv(r = ret_work, n = n_years_work, pv = lump_sum, pmt = 0, type = 1)
+  
+  ret_eq <- discount.rate(n = n_years_work, 
+                          pv = lump_sum,
+                          fv = pv_annuity,
+                          pmt = 0,
+                          type=0
+                          )
+  
+  df <- data.frame(age = seq(client_starting_age, client_starting_age+n_years_work+n_years_retire, 1))
+  
+  for(i in 1:nrow(df)){
+    age <- df[i, "age"]
+    
+    if(age == client_starting_age){
+      df[i, "value_lump_sum"] <- -lump_sum
+      df[i, "value_annuity"] <- 0
+    } else if (age == client_starting_age + n_years_work){
+      df[i, "value_lump_sum"] <- df[(i-1), "value_lump_sum"] * (1 + ret_work) 
+      df[i, "value_annuity"]  <- pv_annuity
+    } else if (age > client_starting_age + n_years_work){
+      df[i, "value_lump_sum"] <- (df[(i-1), "value_lump_sum"] - pmt_annuity) * (1 + ret_retire) 
+      df[i, "value_annuity"] <- (df[(i-1), "value_annuity"] - pmt_annuity) * (1 + ret_retire) 
+    } else{
+      df[i, "value_lump_sum"] <- df[(i-1), "value_lump_sum"] * (1 + ret_work)
+      df[i, "value_annuity"] <- 0
+    }
+    
+    if(df[i, "value_annuity"] < 0){
+      df[i, "value_annuity"] <- 0
+    }
+    if(df[i, "value_lump_sum"] < 0){
+      df[i, "value_lump_sum"] <- 0
+    }
+  }
+  
+  to_plot <- df %>%
+              gather(-age, key=key, value=value) 
+  
+  
+  # Reset file path
+  file_path <- paste0(out_path, "/ls_vs_annuity_retire_", n_years_retire,"_years.jpeg")
+  
+  # Set source/note
+  source_string <- paste0("Source:  SSA, Math (OfDollarsAndData.com)")
+  note_string   <- str_wrap(paste0("Note:  Assumes a", 100*ret_retire,"% real return during working years and retirement."), 
+                            width = 85)
+  
+  text_labels <- data.frame(age = c(55, 65))
+  
+  text_labels[1, "key"] <- "value_lump_sum"
+  text_labels[1, "value"] <- filter(to_plot, age == text_labels[1, "age"],
+                                    key == text_labels[1, "key"]) %>% pull(value)
+  text_labels[1, "label"] <- "Lump Sum"
+  text_labels[2, "key"] <- "value_annuity"
+  text_labels[2, "value"] <- filter(to_plot, age == text_labels[2, "age"],
+                                    key == text_labels[2, "key"]) %>% pull(value)
+  text_labels[2, "label"] <- "Annuity"
+  
+  if(df[nrow(df), "value_lump_sum"] > df[nrow(df), "value_annuity"]){
+    title <- "The Lump Sum is Worth More than\nthe Annuity at Retirement"
+  } else{
+    title <- "The Annuity is Worth More than\nthe Lump Sum at Retirement"
+  }
+  
+  plot <- ggplot(to_plot, aes(x=age, y=value, fill = key)) +
+    geom_bar(stat="identity", position = "dodge") +
+    geom_vline(xintercept = client_starting_age + n_years_work+0.5, linetype = "dashed") +
+    scale_fill_discrete(guide = FALSE) +
+    scale_color_discrete(guide = FALSE) +
+    scale_y_continuous(label = dollar) +
+    geom_text_repel(data=text_labels, aes(x=age, y=value, col = key),
+                    label = text_labels$label,
+                    size = 3.5,
+                    family = "my_font",
+                    max.iter = 1,
+                    segment.colour = "transparent",
+                    nudge_x = ifelse(text_labels$label == "Lump Sum", -5, 7)) +
+    of_dollars_and_data_theme +
+    ggtitle(paste0(title)) +
+    labs(x="Age", y="Value",
+         caption = paste0("\n", source_string, "\n", note_string))
+  
+  # Save the plot
+  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+  
+  
+  print(paste0("Future Value of Lump Sum is: $", formatC(fv_lump_sum, digits = 0, big.mark = ",", format = "f")))
+  print(paste0("Present Value of Annuity is: $", formatC(-pv_annuity, digits = 0, big.mark = ",", format = "f")))
+}
+
 # Define working and retirement parameters
 n_years_work <- 17
-n_years_retire <- 25
-
+client_starting_age <- 45
+lump_sum <- -138000
 pmt_annuity <- 1800*12
-
 ret_work <- 0.05
 ret_retire <- 0.05
 
-# Calculate value of annuity at time of retirement
-pv_annuity <- pv(r = ret_retire,
-                 n = n_years_retire,
-                 fv = 0,
-                 pmt = -pmt_annuity)
+# Run the simulations but play with the retiremnt years
+lump_sum_v_annuity(client_staring_age,
+                   n_years_work, 
+                   23, 
+                   pmt_annuty, 
+                   lump_sum,
+                   ret_work,
+                   ret_retire)
 
-# Include lump sum v
-lump_sum <- -138000
+lump_sum_v_annuity(client_staring_age,
+                   n_years_work, 
+                   25, 
+                   pmt_annuty, 
+                   lump_sum,
+                   ret_work,
+                   ret_retire)
 
-fv_lump_sum <- fv(r = ret_work, n = n_years_work, pv = lump_sum, pmt = 0, type = 0)
-
-ret_eq <- discount.rate(n = n_years_work, 
-                        pv = lump_sum,
-                        fv = pv_annuity,
-                        pmt = 0,
-                        type=0
-                        )
-
-print(paste0("Future Value of Lump Sum is: $", formatC(fv_lump_sum, digits = 0, big.mark = ",", format = "f")))
-print(paste0("Present Value of Annuity is: $", formatC(-pv_annuity, digits = 0, big.mark = ",", format = "f")))
 
 # ############################  End  ################################## #
