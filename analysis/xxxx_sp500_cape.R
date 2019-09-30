e4057f8b6d6e47e1187edda26aff7e02d2226d55
Nick Maggiulli <nick@ritholtzwealth.com>
2019-09-30 16:09:21 -0400

    Fix cape charts

--- analysis/xxxx_sp500_cape.R
+++ analysis/xxxx_sp500_cape.R
@@ -19,34 +19,67 @@ dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
 ########################## Start Program Here ######################### #
 
-n_month_ret <- 120
+plot_cape_time <- function(start_date, end_date, n_month_ret, file_out){
+  
+  n_year_ret <- n_month_ret/12
+  
+  #Bring in all data
+  ret_yr <- readRDS(paste0(localdir, "0009_sp500_ret_pe.Rds")) %>%
+                    rename(index = price_plus_div) %>%
+                    mutate(fwd_ret = (lead(index, n_month_ret)/index)^(12/n_month_ret) - 1) %>%
+                    select(date, fwd_ret, cape) %>%
+                    filter(!is.na(fwd_ret), !is.na(cape), 
+                           date >= start_date,
+                           date < end_date) 
+  
+  to_plot <- ret_yr
+  
+  start_year <- year(start_date)
+  end_year <- year(max(to_plot$date))
+  
+  # Do stock beats by year
+  file_path <- paste0(out_path, "/", file_out)
+  
+  source_string <- paste0("Source:  http://www.econ.yale.edu/~shiller/data.htm (OfDollarsAndData.com)")
+  note_string <- paste0("Note:  Real return includes reinvested dividends.")
+  
+  plot <- ggplot(data = to_plot, aes(x=cape, y = fwd_ret)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    scale_y_continuous(label = percent, limits = c(-0.15, 0.3)) +
+    scale_x_continuous(limits = c(0, 45)) +
+    of_dollars_and_data_theme +
+    ggtitle(paste0("Starting CAPE vs. ", n_year_ret, "-Year Forward Return\n", start_year, "-", end_year)) +
+    labs(x = paste0("Starting CAPE"), y = paste0(n_year_ret, "-Year Annualized Forward Return"),
+         caption = paste0(source_string, "\n", note_string))
+  
+  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+  
+}
 
-#Bring in all data
-ret_yr <- readRDS(paste0(localdir, "0009_sp500_ret_pe.Rds")) %>%
-                  rename(index = price_plus_div) %>%
-                  mutate(fwd_ret = (lead(index, n_month_ret)/index)^(12/n_month_ret) - 1) %>%
-                  select(date, fwd_ret, cape) %>%
-                  filter(!is.na(fwd_ret), !is.na(cape), 
-                         date >= "1980-01-01",
-                         date < "1995-01-01") 
+date_seq <- seq.Date(as.Date("1940-01-01"), as.Date("2008-01-01"), "year")
 
-to_plot <- ret_yr
+for(i in 1:length(date_seq)){
+  n_month_fwd_ret <- 60
+  
+  start_dt <- date_seq[i]
+  start_date_string <- date_to_string(start_dt)
+  
+  end_dt <- start_dt + years(20)
+  
+  file_name_out <- paste0("cape_forward_ret_", n_month_fwd_ret, "m_", start_date_string, ".jpeg")
+  
+  plot_cape_time(start_dt, end_dt, n_month_fwd_ret, file_name_out)
+}
 
-# Do stock beats by year
-file_path <- paste0(out_path, "/cape_forward_ret.jpeg")
+create_gif(out_path,
+           paste0("cape_forward_ret_", n_month_fwd_ret, "m_*.jpeg"),
+           40,
+           0,
+           paste0("_gif_cape_fwd_ret.gif"))
 
-source_string <- paste0("Source:  http://www.econ.yale.edu/~shiller/data.htm (OfDollarsAndData.com)")
-note_string <- paste0("Note:  Real return includes reinvested dividends.")
+plot_cape_time(as.Date("1940-01-01"), as.Date("2014-01-01"), n_month_fwd_ret, "cape_all_years.jpeg")
 
-plot <- ggplot(data = to_plot, aes(x=cape, y = fwd_ret)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE) +
-  scale_y_continuous(label = percent) +
-  of_dollars_and_data_theme +
-  ggtitle(paste0("CAPE vs. ", n_month_ret, "-Month Annualized Forward Return")) +
-  labs(x = paste0("CAPE"), y = paste0(n_month_ret, "-Month Annualized Forward Return (%)"),
-       caption = paste0(source_string, "\n", note_string))
-
-ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 
 # ############################  End  ################################## #
