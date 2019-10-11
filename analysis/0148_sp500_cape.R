a25e27c7fef43483367ab9264208d64f9df8f7d9
Nick Maggiulli <nick@ritholtzwealth.com>
2019-10-11 10:57:09 -0400

    Rename analyses

--- analysis/0148_sp500_cape.R
+++ analysis/0148_sp500_cape.R
@@ -13,7 +13,7 @@ library(lubridate)
 library(ggrepel)
 library(tidyverse)
 
-folder_name <- "0146_sp500_cape"
+folder_name <- "0148_sp500_cape"
 out_path <- paste0(exportdir, folder_name)
 dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
