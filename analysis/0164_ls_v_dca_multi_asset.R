3e0dfd068b0d052502ef5ab9976d914f3c6c35ec
Nick Maggiulli <nick@ritholtzwealth.com>
2020-02-13 14:02:31 -0500

    Create visual of ls vs dca

--- analysis/0164_ls_v_dca_multi_asset.R
+++ analysis/0164_ls_v_dca_multi_asset.R
@@ -12,7 +12,6 @@ library(readxl)
 library(lubridate)
 library(ggrepel)
 library(zoo)
-library(PerformanceAnalytics)
 library(tidyverse)
 
 folder_name <- "0164_ls_v_dca_multi_asset"
@@ -125,7 +124,7 @@ plot_ls_v_dca <- function(asset, f_out, in_df, var, var_note){
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 }
 
-raw <- read.csv(paste0(importdir, "0162_ycharts_multi_asset/timeseries_1-22-2020.csv"), skip = 6) %>%
+raw <- read.csv(paste0(importdir, "0164_ycharts_multi_asset/timeseries_1-22-2020.csv"), skip = 6) %>%
   rename(symbol = Symbol,
          name = Name) %>%
   select(-Metric) %>%
@@ -206,14 +205,8 @@ for(a in all_assets){
     ls_sd <- paste0("ls_sd_", n_month_dca, "m")
     dca_sd <- paste0("dca_sd_", n_month_dca, "m")
     
-    ls_downside <- paste0("ls_downside_", n_month_dca, "m")
-    dca_downside <- paste0("dca_downside_", n_month_dca, "m")
-    
     ls_sharpe <- paste0("ls_sharpe_", n_month_dca, "m")
     dca_sharpe <- paste0("dca_sharpe_", n_month_dca, "m")
-    
-    ls_sortino <- paste0("ls_sortino_", n_month_dca, "m")
-    dca_sortino <- paste0("dca_sortino_", n_month_dca, "m")
     
     asset_result[i, "asset"] <- a
     asset_result[i, ls_col] <- end_ls - 1
@@ -227,14 +220,8 @@ for(a in all_assets){
     asset_result[i, ls_sd] <- sd(ls_ret)
     asset_result[i, dca_sd] <- sd(dca_ret)
     
-    asset_result[i, ls_downside] <- DownsideDeviation(ls_ret)
-    asset_result[i, dca_downside] <- DownsideDeviation(dca_ret)
-    
     asset_result[i, ls_sharpe] <- asset_result[i, ls_r]/sd(ls_ret)
     asset_result[i, dca_sharpe] <- asset_result[i, dca_r]/sd(dca_ret)
-    
-    asset_result[i, ls_sortino] <- asset_result[i, ls_r]/DownsideDeviation(ls_ret)
-    asset_result[i, dca_sortino] <- asset_result[i, dca_r]/DownsideDeviation(dca_ret)
   }
   
   if(a == all_assets[1]){
@@ -250,20 +237,18 @@ for(a in all_assets){
   
   plot_ls_v_dca(a, out_folder, asset_result, "ret", "monthly return")
   plot_ls_v_dca(a, out_folder, asset_result, "sd", "standard deviation")
-  plot_ls_v_dca(a, out_folder, asset_result, "downside", "downside deviation")
   plot_ls_v_dca(a, out_folder, asset_result, "sharpe", "Sharpe Ratio")
-  plot_ls_v_dca(a, out_folder, asset_result, "sortino", "Sortino Ratio")
   plot_ls_v_dca(a, out_folder, asset_result, "outperformance", "DCA outperformance")
 }
 
 #Plot S&P 500 downside vs. 60/40 Downside
 to_plot <- final_results %>%
             filter(asset %in% c("S&P 500 Total Return", "Portfolio 60-40")) %>%
-            select(date, asset, contains("downside")) %>%
-            mutate(value = ifelse(asset == "Portfolio 60-40", ls_downside_24m, dca_downside_24m)) %>%
+            select(date, asset, contains("_sd_")) %>%
+            mutate(value = ifelse(asset == "Portfolio 60-40", ls_sd_24m, dca_sd_24m)) %>%
             select(date, asset, value)
 
-file_path <- paste0(out_path,"/sp500_dca_vs_6040_ls_downside_", n_month_dca, "m.jpeg")
+file_path <- paste0(out_path,"/sp500_dca_vs_6040_ls_sd_", n_month_dca, "m.jpeg")
 
 plot <- ggplot(to_plot, aes(x=date, y=value, col = asset)) +
   geom_line() +
@@ -272,8 +257,8 @@ plot <- ggplot(to_plot, aes(x=date, y=value, col = asset)) +
   of_dollars_and_data_theme +
   theme(legend.position = "bottom",
         legend.title = element_blank()) +
-  ggtitle(paste0("Downside Deviation For\n", n_month_dca, "-Month DCA into S&P 500 vs.\nLump Sum into 60/40")) +
-  labs(x = "Date", y=("Downside Deviation"),
+  ggtitle(paste0("Standard Deviation For\n", n_month_dca, "-Month DCA into S&P 500 vs.\nLump Sum into 60/40")) +
+  labs(x = "Date", y=("Standard Deviation"),
        caption = paste0("Source:  YCharts (OfDollarsAndData.com)"))
 
 ggsave(file_path, plot, width = 15, height = 12, units = "cm")
@@ -311,5 +296,29 @@ export_to_excel(final_summary,
                 sheetname = "final_summary",
                 1,
                 0)
+
+# Plot sample LS vs. DCA
+amount <- 24000
+
+to_plot <- data.frame(period = rep(seq(1, n_month_dca), 2),
+                      value = c(amount, rep(0, n_month_dca-1),
+                                     rep(amount/n_month_dca, n_month_dca)),
+                      key = c(rep("Lump Sum", n_month_dca),
+                              rep("Dollar Cost Averaging", n_month_dca))
+)
+
+file_path <- paste0(out_path,"/_ls_vs_dca_example.jpeg")
+
+plot <- ggplot(to_plot, aes(x=period, y=value)) +
+  geom_bar(stat="identity", fill = "blue") +
+  facet_grid(key ~ .) +
+  scale_y_continuous(label = dollar) +
+  scale_x_continuous(limits = c(0, 25), breaks = seq(0, 24, 3)) +
+  of_dollars_and_data_theme +
+  ggtitle(paste0("The Difference Between LS and DCA")) +
+  labs(x = "Period", y=("Amount Invested per Period"),
+       caption = paste0("Source:  Simulated data (OfDollarsAndData.com)"))
+
+ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 
 # ############################  End  ################################## #
