4fee15b9dfdd94a3437de1e51a281225f46d5cf6
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2020-02-20 07:19:24 -0500

    New analysis on LNW

---
+++ analysis/0165_liquid_net_worth.R
@@ -0,0 +1,24 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of_dollars_and_data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(scales)
+library(readxl)
+library(lubridate)
+library(ggrepel)
+library(tidyverse)
+
+folder_name <- "0165_liquid_net_worth"
+out_path <- paste0(exportdir, folder_name)
+dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
+
+########################## Start Program Here ######################### #
+
+
+
+# ############################  End  ################################## #
