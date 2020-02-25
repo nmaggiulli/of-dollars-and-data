1d91bc8c9eb12fab557f771c037e0770a5ec2683
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2020-02-24 22:20:12 -0500

    Update percentages

--- analysis/0165_liquid_net_worth.R
+++ analysis/0165_liquid_net_worth.R
@@ -28,54 +28,101 @@ data_year <- 2016
 scf_stack <- readRDS(paste0(localdir, "0003_scf_stack.Rds")) %>%
               filter(year == data_year)
 
-liquid_net_worth <- scf_stack %>%
-                      mutate(liquid_net_worth = fin - reteq - ccbal) %>%
-                      select(hh_id, imp_id, 
-                             networth, debt, fin, reteq, homeeq, ccbal, liquid_net_worth, wgt, 
-                             agecl, edcl) %>%
-                      arrange(hh_id, imp_id)
+df <- scf_stack %>%
+      mutate(liquid_net_worth = fin - reteq - ccbal,
+             homeeq_pct = homeeq/networth) %>%
+      select(hh_id, imp_id, 
+             networth, debt, fin, reteq, homeeq, homeeq_pct, ccbal, liquid_net_worth, wgt, 
+             agecl, edcl) %>%
+      arrange(hh_id, imp_id)
 
-n_hh <- length(unique(liquid_net_worth$hh_id))
+n_hh <- length(unique(df$hh_id))
 
-create_median_chart <- function(var, var_title){
+median_nw <- df %>% 
+  summarize(median_nw  = wtd.quantile(networth, weights = wgt, probs=0.5)) %>%
+  pull(median_nw)
 
-  to_plot <- liquid_net_worth %>%
+by_age <- df %>% 
+          group_by(agecl) %>%
+          summarize(median_nw  = wtd.quantile(networth, weights = wgt, probs=0.5)) %>%
+          ungroup()
+
+file_path <- paste0(out_path, "/age_median_nw_text.jpeg")
+source_string <- paste0("Source:  Survey of Consumer Finances, ", data_year, " (OfDollarsAndData.com)")
+note_string <-  str_wrap(paste0("Note:  Percentiles are calculated using data based on ", 
+                                formatC(n_hh, digits = 0, format = "f", big.mark = ","), 
+                                " U.S. households.")
+                         , width = 85)
+
+text_labels <- by_age %>%
+                mutate(label = paste0("$", round(median_nw/1000, 0), "k"))
+
+plot <- ggplot(by_age, aes(x=agecl, y=median_nw)) +
+  geom_bar(stat = "identity", fill = chart_standard_color) +
+  geom_text_repel(data=text_labels, aes(x=agecl, y=median_nw, label = label),
+                  col = chart_standard_color,
+                  max.iter = 1,
+                  nudge_y = 6000) +
+  scale_color_discrete(guide = FALSE) +
+  scale_y_continuous(label = dollar) +
+  of_dollars_and_data_theme +
+  ggtitle(paste0("Median Net Worth by Age")) +
+  labs(x="Age", y=paste0("Median Net Worth"),
+       caption = paste0(source_string, "\n", note_string))
+
+ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+
+
+create_percentile_chart <- function(var, var_title){
+
+  to_plot <- df %>%
                     rename_(.dots = setNames(paste0(var), "var_for_qtile")) %>%
                     group_by(agecl, edcl) %>%
                     summarize(
-                      `25th Percentile` = wtd.quantile(var_for_qtile, weights = wgt, probs=0.25),
-                      `50th Percentile` = wtd.quantile(var_for_qtile, weights = wgt, probs=0.5),
-                      `75th Percentile` = wtd.quantile(var_for_qtile, weights = wgt, probs=0.75)) %>%
+                      `50th Percentile` = wtd.quantile(var_for_qtile, weights = wgt, probs=0.5)
+                      ) %>%
                     ungroup() %>%
-                  gather(-agecl, -edcl, key=key, value=value) %>%
-                  filter(key == "50th Percentile")
+                  gather(-agecl, -edcl, key=key, value=value)
   
   file_path <- paste0(out_path, "/", var, "_age_educ.jpeg")
   source_string <- paste0("Source:  Survey of Consumer Finances, ", data_year, " (OfDollarsAndData.com)")
-  note_string <-  str_wrap(paste0("Note:  Percentiles are calculated using data based on ", 
+  note_string <-  str_wrap(paste0("Note:  Percentiles are calculated using data from ", 
                                   formatC(n_hh, digits = 0, format = "f", big.mark = ","), 
                                   " U.S. households.")
                                   , width = 85)
   
-  plot <- ggplot(to_plot, aes(x=agecl, y=value, fill = key)) +
-    geom_bar(stat = "identity", position = "dodge") +
-    facet_rep_wrap(edcl ~ ., scales = "free_y", repeat.tick.labels = c("left", "bottom")) +
-    scale_fill_discrete(guide = FALSE) +
+  if(var != "homeeq_pct"){
+  plot <- ggplot(to_plot, aes(x=edcl, y=value)) +
+    geom_bar(stat = "identity", position = "dodge", fill = chart_standard_color) +
+    facet_rep_wrap(agecl ~ ., scales = "free_y", repeat.tick.labels = c("left", "bottom")) +
     scale_y_continuous(label = dollar) +
     of_dollars_and_data_theme +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     ggtitle(paste0(var_title, "\nby Age & Education Level")) +
-    labs(x="Age", y=paste0(var_title),
+    labs(x="Education", y=paste0(var_title),
          caption = paste0(source_string, "\n", note_string))
+  } else{
+    plot <- ggplot(to_plot, aes(x=edcl, y=value)) +
+      geom_bar(stat = "identity", position = "dodge", fill = chart_standard_color) +
+      facet_rep_wrap(agecl ~ ., scales = "free_y", repeat.tick.labels = c("left", "bottom")) +
+      scale_y_continuous(label = percent_format(accuracy = 1)) +
+      of_dollars_and_data_theme +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      ggtitle(paste0(var_title, "\nby Age & Education Level")) +
+      labs(x="Education", y=paste0(var_title),
+           caption = paste0(source_string, "\n", note_string))
+  }
   
   # Save the plot
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 }
 
-create_median_chart("networth", "Net Worth")
-create_median_chart("liquid_net_worth", "Liquid Net Worth")
-create_median_chart("ccbal", "Credit Card Balance")
-create_median_chart("reteq", "Retirement Account Balance")
-create_median_chart("homeeq", "Home Equity")
-create_median_chart("fin", "Total Financial Assets")
+create_percentile_chart("networth", "Net Worth")
+create_percentile_chart("liquid_net_worth", "Liquid Net Worth")
+create_percentile_chart("ccbal", "Credit Card Balance")
+create_percentile_chart("reteq", "Retirement Account Balance")
+create_percentile_chart("homeeq", "Home Equity")
+create_percentile_chart("homeeq_pct", "Home Equity Over Net Worth")
+create_percentile_chart("fin", "Total Financial Assets")
 
 # ############################  End  ################################## #
