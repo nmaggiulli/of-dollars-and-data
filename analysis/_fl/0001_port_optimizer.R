13dee38432a75e82f41bd5c06c161f320553b6f7
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2020-03-07 18:39:42 -0500

    Update optimizer

--- analysis/_fl/0001_port_optimizer.R
+++ analysis/_fl/0001_port_optimizer.R
@@ -7,10 +7,6 @@ source(file.path(paste0(getwd(),"/header.R")))
 
 ########################## Load in Libraries ########################## #
 
-
-########################## Start Program Here ######################### #
-
-
 library(scales)
 library(RColorBrewer)
 library(stringr)
@@ -24,12 +20,12 @@ folder_name <- "/_fl/0001_port_optimizer"
 out_path <- paste0(exportdir, folder_name)
 dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
-# ############################  End  ################################## #
+########################## Start Program Here ######################### #
 
 # Load in BV returns
 full_bv_returns <- readRDS(paste0(localdir, "0006_bv_returns.Rds")) %>%
                       mutate(year = as.Date(year, "%d/%m/%y")) %>%
-                      filter(year >= as.Date("1984-01-01"))
+                      rename(`TBill 3m` = `Tbill 3m`)
 
 min_year <- min(year(full_bv_returns$year))
 max_year <- max(year(full_bv_returns$year))
@@ -37,7 +33,8 @@ max_year <- max(year(full_bv_returns$year))
 # Define the number of years
 n_years <- nrow(full_bv_returns)
 
-melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'asset')
+melted_returns <- full_bv_returns %>%
+                    gather(-year, key=asset, value=value)
 
 ############################### First Returns Plot ###############################  
   # Set the file_path for the output
@@ -56,8 +53,7 @@ melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'ass
     facet_wrap(~asset) +
     ggtitle(paste0("Returns Vary by Asset Class\n", min_year, "-", max_year)) +
     scale_y_continuous(label = percent, limits = c(-0.5, 1.25)) +
-    scale_x_date(date_breaks = "10 years", date_labels = "%y",
-                 limits = c(min(full_bv_returns$year), max(full_bv_returns$year))) +
+    scale_x_date(date_breaks = "10 years", date_labels = "%y") +
     scale_color_discrete(guide = FALSE) +
     scale_fill_discrete(guide = FALSE) +
     of_dollars_and_data_theme +
@@ -66,35 +62,46 @@ melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'ass
   
   # Save the gtable
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
-
   
-############################### Second Returns Plot ###############################  
-  # Set the file_path for the next output
-  file_path = paste0(out_path, "/bv_asset_returns_all.jpeg")
+############################### Plot Risk v. Return ###############################   
   
-  plot <- ggplot(data = melted_returns, aes(x = year, y = value, col = asset)) +
-    geom_line() +
-    ggtitle(paste0("Asset Returns Are Hard to Predict\n", min_year, "-", max_year)) +
-      scale_y_continuous(label = percent, limits = c(-0.5, 1.25), breaks = seq(-0.5, 1.25, .25)) +
-    scale_x_date(date_breaks = "10 years", date_labels = "%Y",
-                 limits = c(min(full_bv_returns$year), max(full_bv_returns$year))) +
-    scale_color_discrete(guide = FALSE) +
-    scale_fill_discrete(guide = FALSE) +
-      of_dollars_and_data_theme +
-    labs(x = "Year" , y = "Annual Real Return (%)",
-         caption = paste0(source_string, "\n", note_string))
+  # Set the file_path based on the function input 
+  file_path <- paste0(out_path, "/bv_risk_return.jpeg")
+  # Add a source and note string for the plots
+  source_string <- str_wrap(paste0("Source:  BullionVault, ", min_year, "-", max_year," (OfDollarsAndData.com)"),
+                            width = 85)
+  note_string   <- str_wrap(paste0("Note:  Returns are adjusted using the U.S. Consumer Price Index."),
+                            width = 85)
+  
+  to_plot <- melted_returns %>%
+              group_by(asset) %>%
+              summarize(ret = mean(value),
+                        sd = sd(value)) %>%
+              ungroup()
+  
+  plot <- ggplot(data=to_plot, aes(x = sd, y = ret, col = asset)) +
+          geom_point() +
+          geom_hline(yintercept = 0, linetype = "dashed") +
+          geom_text(data=to_plot, aes(x=sd, y=ret, col=asset, label=asset), vjust = -1) +
+          scale_color_discrete(guide = FALSE) +
+          scale_x_continuous(label = percent, limits = c(0, 0.25), breaks = seq(0, 0.25, 0.05)) +
+          scale_y_continuous(label = percent, limits = c(-0.05, .10), breaks = seq(-0.05, 0.10, 0.05)) +
+          of_dollars_and_data_theme +
+          ggtitle(paste0("Risk vs. Return")) + 
+          labs(x = "Risk (standard deviation)", y ="Annual Real Return",
+               caption = paste0(source_string, "\n", note_string))
   
   # Save the gtable
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
-
+  
 ############################### Optimal Portfolio Solver ###############################  
   
   # Drop the year and the risk free rate from the data so we can optimize on the remaining assets
-  returns <- full_bv_returns[, -which(names(full_bv_returns) %in% c("year", "Tbill 3m"))]
+  returns <- full_bv_returns[, -which(names(full_bv_returns) %in% c("year", "TBill 3m"))]
   
   n_assets <- ncol(returns)
   
-  avg_rf  <- colMeans(full_bv_returns[, "Tbill 3m"])
+  avg_rf  <- colMeans(full_bv_returns[, "TBill 3m"])
   
   eff_frontier <- function (returns, short = "no", max_allocation = NULL, risk_premium_upper_limit = .5, risk_increment = .005){
     # return argument should be a m x n matrix with one column per security
@@ -165,7 +172,7 @@ melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'ass
   max_alloc <- 1
   
   eff <- eff_frontier(returns=returns, short = "no", max_allocation = max_alloc, risk_premium_upper_limit = 0.5, risk_increment = .001)
-
+  
 ############################### Create Additional Portfolios ###############################   
 
   # Initialize all weights as empty data frames
@@ -173,6 +180,8 @@ melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'ass
   equal_weighted   <- data.frame(matrix(nrow = 1, ncol = 0))
   all_stock        <- data.frame(matrix(nrow = 1, ncol = 0))
   all_gold         <- data.frame(matrix(nrow = 1, ncol = 0))
+  all_reit         <- data.frame(matrix(nrow = 1, ncol = 0))
+  all_10yr         <- data.frame(matrix(nrow = 1, ncol = 0))
     
   # Stock + Bond 50-50  
   for (j in colnames(eff[1:n_assets])){
@@ -206,6 +215,24 @@ melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'ass
     }
   }  
   
+  # All REIT  
+  for (j in colnames(eff[1:n_assets])){
+    if (j == "Treasury 10yr"){
+      all_10yr[j] <- 1
+    } else{
+      all_10yr[j] <- 0
+    }
+  }  
+  
+  # All 10yr  
+  for (j in colnames(eff[1:n_assets])){
+    if (j == "REIT"){
+      all_reit[j] <- 1
+    } else{
+      all_reit[j] <- 0
+    }
+  }  
+  
   find_ret_sd_sharpe <- function(df){
     cov_matrix <- cov(returns)
     utc <- upper.tri(cov_matrix)
@@ -223,6 +250,8 @@ melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'ass
   equal_weighted   <- find_ret_sd_sharpe(equal_weighted)
   all_stock        <- find_ret_sd_sharpe(all_stock)
   all_gold         <- find_ret_sd_sharpe(all_gold)
+  all_reit         <- find_ret_sd_sharpe(all_reit)
+  all_10yr         <- find_ret_sd_sharpe(all_10yr)
   
 ############################### Efficient Frontier Plot ###############################    
   # Plot the efficient frontier
@@ -238,33 +267,47 @@ melted_returns <- melt(full_bv_returns ,  id.vars = 'year', variable.name = 'ass
                             width = 85)
   
   # Color Scheme
-  ealred  <- "#7D110C"
-  ealtan  <- "#CDC4B6"
-  eallighttan <- "#F7F6F0"
-  ealdark  <- "#423C30"
+  color_optimal  <- "#7D110C"
+  color_sp500  <- "green"
+  color_reit <- "cyan"
+  color_10yr <- chart_standard_color
+  color_5050 <- "blue"
+  color_gold  <- "#FFD700"
+  color_ew <- "purple"
+  color_dark <- "#423C30"
   
-  plot <- ggplot(eff, aes(x = sd, y = exp_return)) + geom_point(alpha = .1, color = ealdark) +
+  plot <- ggplot(eff, aes(x = sd, y = exp_return)) + geom_point(alpha = .1, color = color_dark) +
     # Add optimal point
-    geom_point(data = optimal, aes(x = sd, y = exp_return), color = ealred, size = 5) +
-    geom_text_repel(data = optimal, label = "Optimal Portfolio", family = "my_font", size = 3.5, nudge_x = -0.02, nudge_y = 0.009, max.iter = 5000) +
+    geom_point(data = optimal, aes(x = sd, y = exp_return), color = color_optimal, size = 5) +
+    geom_text_repel(data = optimal, label = "Optimal Portfolio", family = "my_font", size = 3.5, nudge_x = -0.02, nudge_y = 0.009, max.iter = 5000, segment.color = "transparent") +
     # Add S&P 500 only
-    geom_point(data = all_stock, aes(x = sd, y = exp_return), color = "green", size = 2) +
-    geom_text_repel(data = all_stock, label = "S&P 500 Only", family = "my_font", size = 3, nudge_y = -0.004, max.iter = 5000) +
+    geom_point(data = all_stock, aes(x = sd, y = exp_return), color = color_sp500, size = 2) +
+    geom_text_repel(data = all_stock, label = "S&P 500 Only", family = "my_font", size = 3, nudge_y = -0.004, max.iter = 5000, segment.color = "transparent") +
     # Add 50-50 portfolio
-    geom_point(data = stock_bond_50_50, aes(x = sd, y = exp_return), color = "blue", size = 2) +
-    geom_text_repel(data = stock_bond_50_50, label = "50-50 Stock/Bond", family = "my_font", size = 3, nudge_x = 0.03, max.iter = 5000) +
+    geom_point(data = stock_bond_50_50, aes(x = sd, y = exp_return), color = color_5050, size = 2) +
+    geom_text_repel(data = stock_bond_50_50, label = "50-50 Stock/Bond", family = "my_font", size = 3, nudge_x = 0.03, max.iter = 5000, segment.color = "transparent") +
     # Add all Gold
-    geom_point(data = all_gold, aes(x = sd, y = exp_return), color = "#FFD700", size = 2) +
-    geom_text_repel(data = all_gold, label = "Gold Only", family = "my_font", size = 3, nudge_x = -0.015, max.iter = 5000) +
+    geom_point(data = all_gold, aes(x = sd, y = exp_return), color = color_gold, size = 2) +
+    geom_text_repel(data = all_gold, label = "Gold Only", family = "my_font", size = 3, nudge_x = -0.015, max.iter = 5000, segment.color = "transparent") +
+    # Add all REIT
+    geom_point(data = all_reit, aes(x = sd, y = exp_return), color = color_reit, size = 2) +
+    geom_text_repel(data = all_reit, label = "REITs Only", family = "my_font", size = 3, nudge_x = -0.015, max.iter = 5000, segment.color = "transparent") +
     # Add Equal weighted portfolio
-    geom_point(data = equal_weighted, aes(x = sd, y = exp_return), color = "purple", size = 2) +
-    geom_text_repel(data = equal_weighted, label = "Equal Weighted", family = "my_font", size = 3, nudge_y = -0.004, nudge_x = 0.002, max.iter = 5000) +
+    geom_point(data = equal_weighted, aes(x = sd, y = exp_return), color = color_ew, size = 2) +
+    geom_text_repel(data = equal_weighted, label = "Equal Weighted", family = "my_font", size = 3, nudge_y = -0.004, nudge_x = 0.002, max.iter = 5000, segment.color = "transparent") +
+    # Add all 10yr
+    geom_point(data = all_10yr, aes(x = sd, y = exp_return), color = color_10yr, size = 2) +
+    geom_text_repel(data = all_10yr, label = "Treasury 10yr\nOnly", family = "my_font", size = 3, nudge_y = -0.004, nudge_x = 0.002, max.iter = 5000, segment.color = "transparent") +
     ggtitle(paste0("Efficient Frontier and Optimal Portfolio\n")) + 
-    labs(x = "Risk (standard deviation of portfolio variance)", y ="Real Return",
+    labs(x = "Risk (standard deviation)", y ="Real Return",
          caption = paste0(source_string, "\n", note_string)) +
     of_dollars_and_data_theme +
-    scale_x_continuous(label = percent) +
-    scale_y_continuous(label = percent, limits = c(0.02, .10), breaks = seq(0.02, 0.10, 0.02))
+    scale_x_continuous(label = percent, limits = c(0, 0.25), breaks = seq(0, 0.25, 0.05)) +
+    scale_y_continuous(label = percent, limits = c(0, .10), breaks = seq(0, 0.10, 0.02))
   
   # Save the gtable
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+  
+# ############################  End  ################################## #
+
+  
