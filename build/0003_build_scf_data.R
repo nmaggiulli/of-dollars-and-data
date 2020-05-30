c1813461f3448d0b5f32f68c3337398c04b31089
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2020-05-30 16:37:06 -0400

    Add race as a factor

--- build/0003_build_scf_data.R
+++ build/0003_build_scf_data.R
@@ -46,6 +46,7 @@ for (x in year_list){
   # agecl = age class, 1:<35, 2:35-44, 3:45-54, 4:55-64, 5:65-74, 6:>=75
   # hhsex = gender, 1 = male , 2 = female
   # race = race, 1 = white non-Hispanic, 2 = nonwhite or Hispanic
+  # racelcl4 = 1=white non-Hispanic, 2=black, 3=Hispanic or Latino, 4=Other or Multiple race;
   # edcl = education class, 1 = no high school diploma/GED, 2 = high school diploma or GED,
   #   3 = some college, 4 = college degree
   # married = marital status, 1 = married/living with partner, 2 = neither married nor living with partner
@@ -54,7 +55,7 @@ for (x in year_list){
   vars_to_keep <- c('y1', 'yy1', 'networth', 'debt', 'asset', 'liq', 'reteq',
                     'homeeq', 'hdebt', 'fin', 'nfin', 'vehic', 'bus', 'oresre', 'othnfin', 'mrthel', 'resdbt', 'ccbal', 
                     'income', 'wageinc', 'intdivinc', 'bussefarminc', 'equitinc', 'ssretinc',
-                    'agecl', 'hhsex', 'race', 'edcl', 'married', 'kids', 'wgt')
+                    'agecl', 'hhsex', 'race', 'racecl4', 'edcl', 'married', 'kids', 'wgt')
   
   # Write a function to subset our data
   subset_data <- function(name){
@@ -80,40 +81,43 @@ for (x in year_list){
 }
 
 # Clean some additional variables
-scf_stack <- mutate(scf_stack, married = married %% 2,
+scf_stack_final <- mutate(scf_stack, married = married %% 2,
                     white = race %% 2,
                     male = hhsex %% 2) %>%
-              select(-hhsex, - race)
-
-# Define strings for each age class based on the definitions above
-scf_stack$agecl <- ifelse(scf_stack[,"agecl"] == 1, "<35", 
-                          ifelse(scf_stack[,"agecl"] == 2, "35-44",
-                                 ifelse(scf_stack[,"agecl"] == 3, "45-54",   
-                                        ifelse(scf_stack[,"agecl"] == 4, "55-64",
-                                               ifelse(scf_stack[,"agecl"] == 5, "65-74",
-                                                      ifelse(scf_stack[,"agecl"] == 6, "75+", "99"))))))
-
-# Define strings for education class
-scf_stack$edcl <- ifelse(scf_stack[,"edcl"]  == 1, "No High School", 
-                         ifelse(scf_stack[,"edcl"] == 2, "High School",
-                                ifelse(scf_stack[,"edcl"] == 3, "Some College",   
-                                       ifelse(scf_stack[,"edcl"] == 4, "College Degree", "99"))))
+              select(-hhsex, - race) %>%
+              mutate(race = case_when(racecl4 == 1 ~ "White",
+                                      racecl4 == 2 ~ "Black",
+                                      racecl4 == 3 ~ "Hispanic",
+                                      racecl4 == 4 ~ "Other",
+                                     TRUE ~ "Missing"),
+                     agecl = case_when(agecl == 1 ~ "<35",
+                                       agecl == 2 ~ "35-44",
+                                       agecl == 3 ~ "45-54",
+                                       agecl == 4 ~ "55-64",
+                                       agecl == 5 ~ "65-74",
+                                       agecl == 6 ~ "75+",
+                                       TRUE ~ "99"),
+                     edcl = case_when(edcl == 1 ~ "No High School",
+                                      edcl == 2 ~ "High School",
+                                      edcl == 3 ~ "Some College",
+                                      edcl == 4 ~ "College Degree",
+                                      TRUE ~ "99"))
 
 # Make edcl into a factor
-scf_stack$edcl <- factor(scf_stack$edcl,levels = c("No High School", 
+scf_stack_final$edcl <- factor(scf_stack_final$edcl,levels = c("No High School", 
                                                    "High School", 
                                                    "Some College", 
                                                    "College Degree"))
 
 # Make agecl into a factor
-scf_stack$agecl <- factor(scf_stack$agecl,levels = c("<35", "35-44", "45-54", "55-64",
+scf_stack_final$agecl <- factor(scf_stack_final$agecl,levels = c("<35", "35-44", "45-54", "55-64",
                                                    "65-74", "75+"))
 
-scf_stack <- scf_stack %>%
+scf_stack_final <- scf_stack_final %>%
               rename(hh_id = yy1,
                      imp_id = y1)
 
 # Save down data to permanent file
-saveRDS(scf_stack, paste0(localdir, "0003_scf_stack.Rds"))
+saveRDS(scf_stack_final, paste0(localdir, "0003_scf_stack.Rds"))
 
 # ############################  End  ################################## #
