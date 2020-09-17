952e2bd8279162e041e2f4de75ea20be3bcab6d4
Nick Maggiulli <nick@ritholtzwealth.com>
2020-09-17 13:47:56 -0400

    lc updates

--- analysis/0205_lifestyle_creep.R
+++ analysis/0205_lifestyle_creep.R
@@ -22,10 +22,10 @@ dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
 inc <- 100000
 withdrawal_pct <- 0.04
-inc_growth <- 0.03
+inc_growth <- 0.05
 annual_ret <- 0.04
 
-savings_rates <- seq(0.1, 0.6, 0.1)
+savings_rates <- seq(0.1, 0.6, 0.05)
 
 for(initial_savings_rate in savings_rates){
       annual_savings <- inc*initial_savings_rate
@@ -33,16 +33,9 @@ for(initial_savings_rate in savings_rates){
       retirement_target <- annual_expenditure/withdrawal_pct
       
       n_periods_baseline <- log(1 + (retirement_target/annual_savings)*annual_ret)/log(1 + annual_ret)
-    
+      print(n_periods_baseline)
       
-      df <- data.frame(year = c(),
-                       saving_amount = c(),
-                       total_saved = c(),
-                       retirement_target = c(),
-                       income = c(),
-                       pct_total_retirement = c())
-      
-      raise_saved_pcts <- seq((initial_savings_rate + 0.25), 0.95, 0.01)
+      raise_saved_pcts <- seq(initial_savings_rate , 0.95, 0.01)
       
       tmp <- data.frame(
         annual_ret = rep(annual_ret, length(raise_saved_pcts)),
@@ -52,7 +45,13 @@ for(initial_savings_rate in savings_rates){
       
       counter_f <- 1
       for(r in raise_saved_pcts){
-        print(r)
+        df <- data.frame(year = c(),
+                         saving_amount = c(),
+                         total_saved = c(),
+                         retirement_target = c(),
+                         income = c(),
+                         pct_total_retirement = c())
+        
         counter <- 1
         retire_pct <- 0
         while(retire_pct < 1){
@@ -74,22 +73,26 @@ for(initial_savings_rate in savings_rates){
           retire_pct <- df[counter, "pct_total_retirement"]
           counter <- counter + 1
         }
-        tmp[counter_f, "n_periods"] <- counter
+        tmp[counter_f, "n_periods"] <- counter - 1
         tmp[counter_f, "better_than_baseline"] <- ifelse(tmp[counter_f, "n_periods"] < n_periods_baseline, 1, 0)
+        
+        if(r == initial_savings_rate & initial_savings_rate == 0.6){
+          assign("investigate", df, envir = .GlobalEnv)
+        }
         
         counter_f <- counter_f + 1
       }
       
-      tmp <- tmp %>%
+      tmp2 <- tmp %>%
                 filter(better_than_baseline == 1) %>%
                 head(1) %>%
                 select(-better_than_baseline) %>% 
         mutate(savings_rate = initial_savings_rate)
   
   if(initial_savings_rate == savings_rates[1]){
-    final_results <- tmp
+    final_results <- tmp2
   } else{
-    final_results <- final_results %>% bind_rows(tmp) %>%
+    final_results <- final_results %>% bind_rows(tmp2) %>%
                       select(savings_rate, raise_saved_pct, n_periods)
   }
 }
