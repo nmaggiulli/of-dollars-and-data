e67759e437e39e3fdebc9ecbcbb00f41d33e188b
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2020-10-09 15:23:07 -0400

    Update yield vs. return

--- analysis/0209_hist_bond_rates.R
+++ analysis/0209_hist_bond_rates.R
@@ -35,20 +35,19 @@ df <- bond_ret %>%
         left_join(shiller) %>%
         mutate(ret = (lead(index_bond, n_months_ret)/index_bond)^(1/(n_months_ret/12)) - 1)
 
-file_path <- paste0(out_path, "/yield.jpeg")
-source_string <- paste0("Source:  YCharts (OfDollarsAndData.com)")
+file_path <- paste0(out_path, "/yield_v_return.jpeg")
+source_string <- paste0("Source:  Returns 2.0, Shiller data (OfDollarsAndData.com)")
 
 to_plot <- df 
 
 # Plot the results
-plot <- ggplot(to_plot, aes(x = date, y = final_index, col = as.factor(group))) +
-  geom_line(aes(group = 1)) +
-  geom_text(data = text_labels, aes(x=date, y=final_index, col = as.factor(group), label = label)) +
-  scale_color_manual(values = c("red", "green"), guide = FALSE) +
-  scale_y_continuous(label = dollar, limits = c(6000, 11000)) +
+plot <- ggplot(to_plot, aes(x = long_irate, y = ret)) +
+  geom_point() +
+  scale_x_continuous(label = percent_format(accuracy = 1)) +
+  scale_y_continuous(label = percent_format(accuracy = 1)) +
   of_dollars_and_data_theme +
-  ggtitle(paste0("Tax Loss Harvesting\nRealized Loss vs. Unrealized Gains")) +
-  labs(x = "Date" , y = "Portfolio Value",
+  ggtitle(paste0("Starting Yield vs. Future 10-Year Return")) +
+  labs(x = "Starting Yield" , y = "Forward 10-Year Return",
        caption = paste0("\n", source_string))
 
 # Save the plot
