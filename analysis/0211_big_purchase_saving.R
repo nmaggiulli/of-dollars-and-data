7a24ea07b775f1119f58fd90f7702ce8e850e193
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2020-10-25 11:40:52 -0400

    Add big purchase analysis

--- analysis/0211_big_purchase_saving.R
+++ analysis/0211_big_purchase_saving.R
@@ -20,13 +20,7 @@ dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
 ########################## Start Program Here ######################### #
 
-# Sim paramaters
-monthly_payment <- 1000
-
-# Expected months must be >= 24
-expected_months <- 60
-required_savings <- monthly_payment*expected_months
-
+#Bring in raw data
 bond_ret <- read.csv(paste0(importdir, "/0209_bond_rets/treasury_5yr.csv"), skip = 7,
                      col.names = c("date", "index_cpi", "index_bond")) %>%
   drop_na() %>%
@@ -46,168 +40,248 @@ stock_bond <- readRDS(paste0(localdir, "0009_sp500_ret_pe.RDS")) %>%
 min_year <- year(min(stock_bond$date))
 max_year <- year(max(stock_bond$date))
 
+# Global Sim paramaters
+monthly_payment <- 1000
+cap_gains <- 0.15
+
+source_string <- paste0("Source: Shiller data, Returns 2.0, ", min_year, "-", max_year, " (OfDollarsAndData.com)")
+
+# Select dates
 dates_to_run <- stock_bond %>%
                   select(date) %>%
                   distinct() %>%
-                  filter(month(date) %in% c(1), year(date) < 2016) %>%
+                  filter(month(date) %in% c(1, 4, 7, 10), year(date) < 2016) %>%
                   pull(date)
 
-run_saving_sim <- function(s_weight, b_weight, c_weight, name, i_name){
-  
-  final_results <- data.frame()
-  
-  counter <- 1
-  for(d in dates_to_run){
-    print(as.Date(d, format = "%Y-%m-%d"))
-    tmp<- stock_bond %>% filter(date >= d)
-    
-    current_savings <- 0
-    liquid_savings <- 0
-    cost_basis <- 0
-    month_counter <- 1
-    cap_gains <- 0.15
-    
-    while(liquid_savings < required_savings & month_counter < nrow(tmp)){
-      s_ret <- tmp[month_counter, "ret_sp500"]
-      b_ret <- tmp[month_counter, "ret_bond_real"]
-      c_ret <- tmp[month_counter, "ret_cash_real"]
-      ret <- s_weight*s_ret + b_weight*b_ret + c_weight*c_ret
-      
-      cost_basis <- cost_basis + monthly_payment 
-      
-      if(current_savings == 0){
-        current_savings <- monthly_payment * (1 + ret)
-      } else{
-        current_savings <- (current_savings + monthly_payment) * (1 + ret)
-      }
-      
-      if(cost_basis > current_savings){
-        liquid_savings <- current_savings
-      } else{
-        liquid_savings <- ((current_savings - cost_basis) * (1 - cap_gains)) + cost_basis
-      }
-      
-      month_counter <- month_counter + 1
-    }
-    
-    if(month_counter >= nrow(tmp)){
-      month_counter <- NA
-    }
-    
-    final_results[counter, "start_date"] <- as.Date(d, format = "%Y-%m-%d")
-    final_results[counter, "n_months_to_goal"] <- month_counter
-    print(month_counter)
-    
-    counter <- counter + 1
-  }
-  
-  assign(paste0("final_results_", i_name), final_results, envir = .GlobalEnv)
-  
-  time_buckets <- final_results %>%
-    mutate(time_bucket = case_when(
-      n_months_to_goal < (expected_months - 12) ~ "12m+ early",
-      n_months_to_goal < (expected_months - 6) ~ "6-12m early",
-      n_months_to_goal < expected_months ~ "1-6m early",
-      n_months_to_goal < (expected_months + 5) ~ "1-6m late",
-      n_months_to_goal < (expected_months + 13) ~ "6-12m late",
-      TRUE ~ "12m+ late"
-    )) %>%
-    group_by(time_bucket) %>%
-    summarize(n_quarters = n(),
-          pct = n()/ nrow(final_results)) %>%
-    ungroup()
-  
-  tb_levels <- c("12m+ early",
-                 "6-12m early",
-                 "1-6m early",
-                 "1-6m late",
-                 "6-12m late",
-                 "12m+ late"
-                 )
+# Find months to test
+months_to_test <- seq(24, 60, 6)
 
-  to_plot <- data.frame(time_bucket = tb_levels) %>%
-    full_join(time_buckets) %>% 
-    mutate(pct = ifelse(is.na(pct), 0, pct))
+for(expected_months in months_to_test){
+  # Sim paramaters
+  print(expected_months)
+  required_savings <- monthly_payment*expected_months
   
-  to_plot$time_bucket <- factor(to_plot$time_bucket, levels = tb_levels)
-  
-  to_plot <- to_plot %>% arrange(time_bucket)
-  
-  for(i in 1:nrow(to_plot)){
-    if(i == 1){
-      to_plot[i, "pct_cumulative"] <- to_plot[i, "pct"]
-    } else{
-      to_plot[i, "pct_cumulative"] <- to_plot[(i-1), "pct_cumulative"] + to_plot[i, "pct"]
+  run_saving_sim <- function(s_weight, b_weight, c_weight, name, i_name){
+    
+    tmp_results <- matrix(nrow = length(dates_to_run), ncol = 2)
+    
+    counter <- 1
+    for(d in dates_to_run){
+      tmp<- stock_bond %>% 
+        filter(date >= d) %>%
+        select(ret_sp500, ret_bond_real, ret_cash_real) %>%
+        as.matrix()
+      
+      current_savings <- 0
+      liquid_savings <- 0
+      cost_basis <- 0
+      month_counter <- 1
+      
+      while(liquid_savings < required_savings & month_counter < nrow(tmp)){
+        s_ret <- tmp[month_counter, 1]
+        b_ret <- tmp[month_counter, 2]
+        c_ret <- tmp[month_counter, 3]
+        ret <- s_weight*s_ret + b_weight*b_ret + c_weight*c_ret
+        
+        cost_basis <- cost_basis + monthly_payment 
+        
+        if(current_savings == 0){
+          current_savings <- monthly_payment * (1 + ret)
+        } else{
+          current_savings <- (current_savings + monthly_payment) * (1 + ret)
+        }
+        
+        if(cost_basis > current_savings){
+          liquid_savings <- current_savings
+        } else{
+          liquid_savings <- ((current_savings - cost_basis) * (1 - cap_gains)) + cost_basis
+        }
+        
+        month_counter <- month_counter + 1
+      }
+      
+      if(month_counter >= nrow(tmp)){
+        month_counter <- NA
+      }
+      
+      tmp_results[counter, 1] <- as.Date(d, format = "%Y-%m-%d")
+      tmp_results[counter, 2] <- month_counter
+      
+      counter <- counter + 1
     }
+    
+    final_results <- data.frame(start_date = as.Date(tmp_results[, 1]),
+                                n_months_to_goal = tmp_results[, 2])
+    
+    assign(paste0("final_results_", i_name), final_results, envir = .GlobalEnv)
+    
+    time_buckets <- final_results %>%
+      mutate(time_bucket = case_when(
+        n_months_to_goal <= (expected_months - 12) ~ "12m+\nearly",
+        n_months_to_goal <= (expected_months - 6) ~ "6m-12m\nearly",
+        n_months_to_goal <= expected_months ~ "0m-6m\nearly",
+        n_months_to_goal <= (expected_months + 6) ~ "1m-6m\nlate",
+        n_months_to_goal <= (expected_months + 12) ~ "6m-12m\nlate",
+        TRUE ~ "12m+\nlate"
+      )) %>%
+      group_by(time_bucket) %>%
+      summarize(n_quarters = n(),
+                pct = n()/ nrow(final_results)) %>%
+      ungroup()
+    
+    tb_levels <- c("12m+\nearly",
+                   "6m-12m\nearly",
+                   "0m-6m\nearly",
+                   "1m-6m\nlate",
+                   "6m-12m\nlate",
+                   "12m+\nlate"
+    )
+    
+    to_plot <- data.frame(time_bucket = tb_levels) %>%
+      full_join(time_buckets) %>% 
+      mutate(pct = ifelse(is.na(pct), 0, pct))
+    
+    to_plot$time_bucket <- factor(to_plot$time_bucket, levels = tb_levels)
+    
+    to_plot <- to_plot %>% arrange(time_bucket)
+    
+    for(i in 1:nrow(to_plot)){
+      if(i == 1){
+        to_plot[i, "pct_cumulative"] <- to_plot[i, "pct"]
+      } else{
+        to_plot[i, "pct_cumulative"] <- to_plot[(i-1), "pct_cumulative"] + to_plot[i, "pct"]
+      }
+    }
+    
+    file_path <- paste0(out_path, "/_", expected_months, "m_", name, "_distribution_of_saving_times.jpeg")
+    #Create note for charts
+    note_string <- str_wrap(
+      paste0("Note: Assumes you save ", format_as_dollar(monthly_payment), 
+             " per month for ", 
+             expected_months,
+             " months, that capital gains taxes are paid at the long term rate of ", 
+             100*cap_gains, 
+             "%, and that all returns are adjusted for inflation."),
+      width = 85)
+    
+    plot <- ggplot(to_plot, aes(x = time_bucket, y = pct)) +
+      geom_bar(stat = "identity", fill = chart_standard_color) +
+      scale_y_continuous(label = percent_format(accuracy = 1)) +
+      of_dollars_and_data_theme +
+      ggtitle(paste0("How Long it Takes to Save ", format_as_dollar(required_savings, 0), "\nWhen Fully Invested in ", i_name)) +
+      labs(x = "Number of Months" , y = paste0("Percentage of Time"),
+           caption = paste0(source_string, "\n", note_string))
+    
+    # Save the plot
+    ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+    
+    # Do cumulative plot
+    file_path <- paste0(out_path, "/_", expected_months, "m_", name, "_cumulative_saving_times.jpeg")
+    
+    plot <- ggplot(to_plot, aes(x = time_bucket, y = pct_cumulative)) +
+      geom_bar(stat = "identity", fill = chart_standard_color) +
+      scale_y_continuous(label = percent_format(accuracy = 1), breaks = seq(0, 1, 0.1)) +
+      of_dollars_and_data_theme +
+      ggtitle(paste0("Cumulative Distribution of Savings Times\nWhen Fully Invested in ", i_name)) +
+      labs(x = "Number of Months" , y = paste0("Cumulative Percentage"),
+           caption = paste0(source_string, "\n", note_string))
+    
+    # Save the plot
+    ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+    
+    # Do date plot
+    file_path <- paste0(out_path, "/_", expected_months, "m_", name, "_by_qtr.jpeg")
+    
+    plot <- ggplot(final_results, aes(x = start_date, y = n_months_to_goal)) +
+      geom_bar(stat = "identity", fill = chart_standard_color, width = 92) +
+      scale_x_date(date_labels = "%Y",
+                   limits = c(as.Date("1926-01-01"), as.Date("2016-10-01")),
+                   breaks = seq.Date(from = as.Date("1920-01-01"),
+                                     to = as.Date("2010-01-01"),
+                                     by = "10 years")) +
+      of_dollars_and_data_theme +
+      ggtitle(paste0("Number of Months to Reach Goal\nWhen Fully Invested in ", i_name)) +
+      labs(x = "Start Date" , y = paste0("Number of Months"),
+           caption = paste0(source_string, "\n", note_string))
+    
+    # Save the plot
+    ggsave(file_path, plot, width = 15, height = 12, units = "cm")
   }
   
-  assign(paste0("to_plot_", i_name), to_plot, envir = .GlobalEnv)
+  run_saving_sim(1, 0, 0, "all_stock", "Stocks")
+  run_saving_sim(0, 1, 0, "all_bond", "Bonds")
+  run_saving_sim(0, 0, 1, "all_cash", "Cash")
   
-  file_path <- paste0(out_path, "/", name, "_distribution_of_saving_times.jpeg")
-  source_string <- paste0("Source: Shiller data, Returns 2.0, ", min_year, "-", max_year, " (OfDollarsAndData.com)")
+  final_results_all <- final_results_Bonds %>% rename(n_months_bonds = n_months_to_goal) %>%
+    left_join(final_results_Cash %>% rename(n_months_cash = n_months_to_goal)) %>%
+    left_join(final_results_Stocks %>% rename(n_months_stocks = n_months_to_goal)) %>%
+    mutate(bonds_win = ifelse(n_months_bonds < n_months_cash, 1, 0),
+           stocks_win = ifelse(n_months_stocks < n_months_cash, 1, 0),
+           net_bonds =  case_when(n_months_cash - n_months_bonds > 12 ~ 12,
+                                  n_months_cash - n_months_bonds < -12 ~ -12,
+                                  TRUE ~ n_months_cash - n_months_bonds),
+           net_stocks = case_when(n_months_cash - n_months_stocks >12 ~ 12,
+                                  n_months_cash - n_months_stocks < -12 ~ -12,
+                                  TRUE ~ n_months_cash - n_months_stocks))
+  
+  
+  file_path <- paste0(out_path, "/net_bonds_", expected_months, "m_.jpeg")
+  #Create note for charts
   note_string <- str_wrap(
     paste0("Note: Assumes you save ", format_as_dollar(monthly_payment), 
-           " per month, that capital gains taxes are paid at the long term rate of ", 
+           " per month for ", 
+           expected_months,
+           " months, that capital gains taxes are paid at the long term rate of ", 
            100*cap_gains, 
-           "%, and that all returns are adjusted for inflation."),
+           "%, and that all returns are adjusted for inflation.  ",
+           "Differences greater than 12 months have been capped at 12 months for simplicity."),
     width = 85)
   
-  plot <- ggplot(to_plot, aes(x = time_bucket, y = pct)) +
-    geom_bar(stat = "identity", fill = chart_standard_color) +
-    scale_y_continuous(label = percent_format(accuracy = 1)) +
-    of_dollars_and_data_theme +
-    ggtitle(paste0("How Long it Takes to Save ", format_as_dollar(required_savings, 0), "\nWhen Fully Invested in ", i_name)) +
-    labs(x = "Number of Months" , y = paste0("Percentage of Time"),
-         caption = paste0(source_string, "\n", note_string))
-  
-  # Save the plot
-  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
-  
-  # Do cumulative plot
-  file_path <- paste0(out_path, "/", name, "_cumulative_saving_times.jpeg")
-  
-  plot <- ggplot(to_plot, aes(x = time_bucket, y = pct_cumulative)) +
-    geom_bar(stat = "identity", fill = chart_standard_color) +
-    scale_y_continuous(label = percent_format(accuracy = 1), breaks = seq(0, 1, 0.1)) +
-    of_dollars_and_data_theme +
-    ggtitle(paste0("Cumulative Distribution of Savings Times\nWhen Fully Invested in ", i_name)) +
-    labs(x = "Number of Months" , y = paste0("Cumulative Percentage"),
-         caption = paste0(source_string, "\n", note_string))
-  
-  # Save the plot
-  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
-  
-  # Do date plot
-  file_path <- paste0(out_path, "/", name, "_by_qtr.jpeg")
-  
-  plot <- ggplot(final_results, aes(x = start_date, y = n_months_to_goal)) +
+  plot <- ggplot(final_results_all, aes(x = start_date, y = net_bonds)) +
     geom_bar(stat = "identity", fill = chart_standard_color, width = 92) +
     scale_x_date(date_labels = "%Y",
                  limits = c(as.Date("1926-01-01"), as.Date("2016-10-01")),
                  breaks = seq.Date(from = as.Date("1920-01-01"),
                                    to = as.Date("2010-01-01"),
                                    by = "10 years")) +
+    scale_y_continuous(limits = c(-12, 12), breaks = seq(-12, 12, 6)) +
     of_dollars_and_data_theme +
-    ggtitle(paste0("Number of Months to Reach Goal\nWhen Fully Invested in ", i_name)) +
-    labs(x = "Start Date" , y = paste0("Number of Months"),
+    ggtitle(paste0("Additional Months for Cash to Reach Savings Goal\nCompared to Investing in Bonds\nOver ", expected_months, " Months")) +
+    labs(x = "Start Date" , y = paste0("Additional Months"),
+         caption = paste0(source_string, "\n", note_string))
+  
+  # Save the plot
+  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+  
+  # Do stocks
+  file_path <- paste0(out_path, "/net_stocks_", expected_months, "m_.jpeg")
+  
+  plot <- ggplot(final_results_all, aes(x = start_date, y = net_stocks)) +
+    geom_bar(stat = "identity", fill = chart_standard_color, width = 92) +
+    scale_x_date(date_labels = "%Y",
+                 limits = c(as.Date("1926-01-01"), as.Date("2016-10-01")),
+                 breaks = seq.Date(from = as.Date("1920-01-01"),
+                                   to = as.Date("2010-01-01"),
+                                   by = "10 years")) +
+    scale_y_continuous(limits = c(-12, 12), breaks = seq(-12, 12, 6)) +
+    of_dollars_and_data_theme +
+    ggtitle(paste0("Additional Months for Cash to Reach Savings Goal\nCompared to Investing in Stocks\nOver ", expected_months, " Months")) +
+    labs(x = "Start Date" , y = paste0("Additional Months"),
          caption = paste0(source_string, "\n", note_string))
   
   # Save the plot
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 }
 
-run_saving_sim(1, 0, 0, "all_stock", "Stocks")
-run_saving_sim(0, 1, 0, "all_bond", "Bonds")
-run_saving_sim(0, 0, 1, "all_cash", "Cash")
+create_gif(path = paste0(out_path), 
+           file_stub = "net_bonds_*.jpeg",
+           speed_milliseconds = 100,
+           out_name = paste0("/_net_bonds_all_months.gif"))
 
-final_results_all <- final_results_Bonds %>% rename(n_months_bonds = n_months_to_goal) %>%
-                      left_join(final_results_Cash %>% rename(n_months_cash = n_months_to_goal)) %>%
-                      left_join(final_results_Stocks %>% rename(n_months_stocks = n_months_to_goal)) %>%
-                      mutate(bonds_win = ifelse(n_months_bonds < n_months_cash, 1, 0),
-                             stocks_win = ifelse(n_months_stocks < n_months_cash, 1, 0))
+create_gif(path = paste0(out_path), 
+           file_stub = "net_stocks_*.jpeg",
+           speed_milliseconds = 100,
+           out_name = paste0("/_net_stocks_all_months.gif"))
 
-win_summary <- final_results_all %>%
-                summarize(bonds_win_pct = mean(bonds_win, na.rm = TRUE),
-                          stocks_win_pct = mean(stocks_win, na.rm = TRUE))
 
 # ############################  End  ################################## #
