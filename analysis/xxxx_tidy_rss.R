40f127b8c3500e24dbc536e46a2541df24fa70f6
Nicholas Maggiulli <nmaggiulli@users.noreply.github.com>
2020-11-20 12:53:00 -0500

    Get blog post URLs

---
+++ analysis/xxxx_tidy_rss.R
@@ -0,0 +1,44 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of_dollars_and_data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(scales)
+library(readxl)
+library(lubridate)
+library(stringr)
+library(tidyRSS)
+library(tidyverse)
+
+folder_name <- "xxxx_tidy_rss"
+out_path <- paste0(exportdir, folder_name)
+dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
+
+########################## Start Program Here ######################### #
+
+df <- tidyfeed(
+  "https://ofdollarsanddata.com/feed"
+) 
+
+to_export <- df %>%
+              select(item_title, item_link, item_description, item_pub_date, item_category) %>%
+              mutate(pub_date = as.Date(item_pub_date)) %>%
+              arrange(pub_date) %>%
+              mutate(post_num = row_number()) %>%
+              rename(title = item_title,
+                     link = item_link,
+                     description = item_description,
+                     category = item_category) %>%
+              select(post_num, pub_date, title, link, description, category)
+
+export_to_excel(to_export,
+                outfile = paste0(out_path, "/all_posts.xlsx"),
+                sheetname = "raw",
+                new_file = 1,
+                fancy_formatting = 0)
+
+# ############################  End  ################################## #
