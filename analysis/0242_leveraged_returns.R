3705ec3cd0e661c7d2d594cc26c7694d1396bd8a
Nick Maggiulli <nick@ritholtzwealth.com>
2021-05-14 12:56:28 -0400

    Rename and update leveraged return plot

--- analysis/0242_leveraged_returns.R
+++ analysis/0242_leveraged_returns.R
@@ -16,23 +16,23 @@ library(tidylog)
 library(zoo)
 library(tidyverse)
 
-folder_name <- "0242_levered_returns"
+folder_name <- "0242_leveraged_returns"
 out_path <- paste0(exportdir, folder_name)
 dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
 ########################## Start Program Here ######################### #
 
-dow_pre_2020 <- read_excel(paste0(importdir, "/0242_levered_returns/Dow daily 2020.xlsx"),
+dow_pre_2020 <- read_excel(paste0(importdir, "/0242_leveraged_returns/Dow daily 2020.xlsx"),
                            col_names = c("date", "index")) %>%
                   mutate(date = as.Date(date)) %>%
                   filter(year(date) < 2020)
 
-dow_2020 <- read.csv(paste0(importdir, "/0242_levered_returns/DJI_data.csv"),
+dow_2020 <- read.csv(paste0(importdir, "/0242_leveraged_returns/DJI_data.csv"),
                        col.names = c("date", "index")) %>%
               mutate(date = as.Date(date)) %>%
               filter(year(date) == 2020)
 
-n_years <- 20
+n_years <- 10
 n_trading_days <- n_years*250
 
 df <- dow_pre_2020 %>%
@@ -94,7 +94,7 @@ avg_3x <- to_plot %>%
   summarize(avg = mean(value)) %>%
   pull(avg)
 
-file_path <- paste0(out_path, "/levered_outperformance_", n_years, "yrs.jpeg")
+file_path <- paste0(out_path, "/leveraged_outperformance_", n_years, "yrs.jpeg")
 source_string <- "Source:  Bloomberg (OfDollarsAndData.com)"
 note_string <- str_wrap(paste0("Note: Assumes an annual fee of ", 10000*bps_fee, " basis points for each leveraged strategy. ",
                                "The average annualized outperformance of the 2x Leverage strategy is ", round(100*avg_2x, 2), "%. ",
@@ -105,32 +105,39 @@ text_labels <- data.frame()
 
 if(n_years == 10){
   label_date <- as.Date("1965-01-01")  
+  date_breaks <- seq.Date(as.Date("1920-01-01"), as.Date("2010-01-01"), "10 years")
+  y_max <- 0.4
 } else if (n_years == 20){
   label_date <- as.Date("1960-01-01")  
+  date_breaks <- seq.Date(as.Date("1920-01-01"), as.Date("2000-01-01"), "10 years")
+  y_max <- 0.3
 } else if (n_years == 30){
   label_date <- as.Date("1955-01-01")  
+  date_breaks <- seq.Date(as.Date("1920-01-01"), as.Date("1990-01-01"), "10 years")
+  y_max <- 0.2
 }
 
-
+y_min <- -1 * y_max
 text_labels[1, "start_date"] <- label_date
-text_labels[1, "value"] <- 0.4
-text_labels[1, "label"] <- "Levered Portfolio Outperforms"
+text_labels[1, "label"] <- "Leveraged Portfolio Outperforms"
+text_labels[1, "value"] <- y_max
 
 text_labels[2, "start_date"] <- label_date
-text_labels[2, "value"] <- -0.35
-text_labels[2, "label"] <- "Levered Portfolio Underperforms"
+text_labels[2, "label"] <- "Leveraged Portfolio Underperforms"
+text_labels[2, "value"] <- y_min + 0.05
 
 plot <- ggplot(to_plot, aes(x=start_date, y= value, col = key)) +
   geom_line() +
   geom_hline(yintercept = 0, linetype = "dashed") +
   geom_text(data = text_labels, aes(x=start_date, y=value, label = label), col = "black", family = "my_font") +
   scale_color_manual(values = c("#67a9cf", "#ef8a62")) +
-  scale_y_continuous(label = percent_format(accuracy = 1), limits = c(-0.4, 0.4), breaks = seq(-0.4, 0.4, 0.1)) +
+  scale_y_continuous(label = percent_format(accuracy = 1), limits = c(y_min, y_max), breaks = seq(y_min, y_max, 0.1)) +
+  scale_x_date(breaks = date_breaks, date_labels = "%Y") +
   of_dollars_and_data_theme +
   theme(legend.position = "bottom",
         legend.title = element_blank()) +
-  ggtitle(paste0("Dow Jones\nLevered Annualized Outperformance\nOver ", n_years, " Years")) +
-  labs(x="Start Date", y="Annualized Outperformance",
+  ggtitle(paste0("Dow Jones\nLeveraged Annualized Outperformance\nOver ", n_years, " Years")) +
+  labs(x="Start Year", y="Annualized Outperformance",
        caption = paste0(source_string, "\n", note_string))
 
 # Save the plot
