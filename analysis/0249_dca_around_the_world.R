e226ad6dda01f10b764eca24fbead312ea427943
Nick Maggiulli <nick@ritholtzwealth.com>
2021-06-30 13:17:55 -0400

    Update summary stats

--- analysis/0249_dca_around_the_world.R
+++ analysis/0249_dca_around_the_world.R
@@ -26,11 +26,11 @@ dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 period_length <- 120
 
 raw_1970 <- read.csv(paste0(importdir, "/0249_world_returns/dfa_msci_1970.csv"), skip = 6,
-                col.names = c("date", "Australia", "Sweden", "US", "Spain",
-                              "UK", "Canada", "Japan"),
+                col.names = c("date", "US", "Spain",
+                              "UK", "Canada", "Japan", "Italy"),
                 ) %>%
-        filter(row_number() != 1, `Australia` != "") %>%
-        mutate(date = as.Date(date, "%m/%d/%Y"))
+        filter(row_number() != 1, `US` != "") %>%
+        mutate(date = as.Date(date, "%m/%d/%y"))
 
 df_1970 <- sapply(raw_1970[2:ncol(raw_1970)], as.numeric) %>%
                 as.data.frame() %>%
@@ -38,10 +38,10 @@ df_1970 <- sapply(raw_1970[2:ncol(raw_1970)], as.numeric) %>%
             select(date, everything())
 
 raw_1999 <- read.csv(paste0(importdir, "/0249_world_returns/dfa_msci_1999.csv"), skip = 6,
-                     col.names = c("date", "China", "Mexico", "SouthAfrica", "Greece"),
+                     col.names = c("date", "China", "Greece", "Russia"),
 ) %>%
   filter(row_number() != 1, `China` != "") %>%
-  mutate(date = as.Date(date, "%m/%d/%Y"))
+  mutate(date = as.Date(date, "%m/%d/%y"))
 
 df_1999 <- sapply(raw_1999[2:ncol(raw_1999)], as.numeric) %>%
   as.data.frame() %>%
@@ -102,13 +102,24 @@ for(x in index_cols){
   }
 }
 
-file_path <- paste0(out_path, "/dca_all_countries.jpeg")
-source_string <- paste0("Source: Returns2.0")
+final_results <- final_results %>%
+              mutate(stock_premium = value/cost_basis - 1,
+                stock_beats_cash = ifelse(stock_premium > 0, 1, 0),
+                stock_premium_bins = case_when(
+                  stock_premium < 0 ~ "<0%",
+                  stock_premium < 0.5~ "0%-50%",
+                  TRUE ~ ">50%"
+                )
+              )
+                
+final_results$stock_premium_bins <- factor(final_results$stock_premium_bins, levels = c("<0%", "0%-50%",">50%"))
+
+source_string <- paste0("Source: Returns 2.0")
 note_string <- str_wrap(paste0("Note: Assumes that DCA invests $100 a month for 10 years. Returns are shown net of dividends."),
                         width = 80)
 
-to_plot <- final_results %>%
-              mutate(stock_beats_cash = ifelse(value > cost_basis, 1, 0))
+file_path <- paste0(out_path, "/dca_all_countries.jpeg")
+to_plot <- final_results 
 
 plot <- ggplot(to_plot, aes(x= start_date, y=value, col = country)) +
   geom_line() +
@@ -124,6 +135,41 @@ plot <- ggplot(to_plot, aes(x= start_date, y=value, col = country)) +
 # Save the plot
 ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 
-print(paste0("Stock beats cash: ", 100*round(mean(to_plot$stock_beats_cash), 2), "%"))
+# Do binning
+file_path <- paste0(out_path, "/dca_bins_all_countries.jpeg")
+
+country_bin <- final_results %>%
+              group_by(stock_premium_bins, country) %>%
+              summarise(count = n()) %>%
+              ungroup()
+
+country <- final_results %>%
+  group_by(country) %>%
+  summarise(country_total = n()) %>%
+  ungroup()
+
+to_plot <- country_bin %>%
+            left_join(country) %>%
+            mutate(pct = count/country_total,
+                   label = paste0(100*round(pct, 2), "%")) %>%
+            select(country, stock_premium_bins, pct, label)
+  
+plot <- ggplot(to_plot, aes(x=stock_premium_bins, y=pct, fill = country)) +
+  geom_bar(stat = "identity") +
+  geom_text(data=to_plot, aes(x=stock_premium_bins, y=pct + 0.05, label = label), col = "black", size = 2) + 
+  facet_wrap(~country) +
+  scale_fill_discrete(guide = FALSE) +
+  scale_y_continuous(label = percent_format(accuracy = 1)) +
+  of_dollars_and_data_theme +
+  ggtitle(paste0("DCA Return Above Cash\nOver 10 Years\nBy Country and Return Bin")) +
+  labs(x="Return Bin", y="Percentage",
+       caption = paste0(source_string, "\n", note_string))
+
+# Save the plot
+ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+
+print(paste0("Stock beats cash: ", 100*round(mean(final_results$stock_beats_cash), 2), "% of the time (avg)"))
+print(paste0("Stock beats cash by: ", 100*round(mean(final_results$stock_premium), 2), "% (avg)"))
+print(paste0("Stock beats cash by: ", 100*round(quantile(final_results$stock_premium, probs = 0.5), 2), "% (median)"))
 
 # ############################  End  ################################## #
