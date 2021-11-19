a5e0c6f48dff2980b845e5b6cac09e5d87d9443f
Nick Maggiulli <nick@ritholtzwealth.com>
2021-11-19 16:00:40 -0500

    PS analysis

---
+++ analysis/0269_ps_analysis.R
@@ -0,0 +1,101 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of_dollars_and_data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(scales)
+library(readxl)
+library(lubridate)
+library(ggrepel)
+library(tidylog)
+library(zoo)
+library(ggjoy)
+library(tidyverse)
+
+folder_name <- "0269_ps_analysis"
+out_path <- paste0(exportdir, folder_name)
+dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
+
+########################## Start Program Here ######################### #
+
+ps <- read.csv(paste0(importdir, "/0269_ycharts_market_cap_ps/timeseries_11-19-2021_ps.csv"),
+                   skip = 6) %>%
+  select(-Metric, -Name) %>%
+  rename(symbol = Symbol) %>%
+  gather(-symbol, key=key, value=ps) %>%
+  mutate(year = gsub("X(\\d+)\\.(\\d+)\\.(\\d+)", "\\1", key, perl = TRUE),
+         month =  gsub("X(\\d+)\\.(\\d+)\\.(\\d+)", "\\2", key, perl = TRUE),
+         day =  gsub("X(\\d+)\\.(\\d+)\\.(\\d+)", "\\3", key, perl = TRUE),
+         date = as.Date(paste0(year, "-", month, "-", day), format = "%Y-%m-%d")) %>%
+  select(date, symbol, ps) %>%
+  drop_na()
+
+mcap <- read.csv(paste0(importdir, "/0269_ycharts_market_cap_ps/timeseries_11-19-2021_mcap.csv"),
+               skip = 6) %>%
+  select(-Metric, -Name) %>%
+  rename(symbol = Symbol) %>%
+  gather(-symbol, key=key, value=mcap) %>%
+  mutate(year = gsub("X(\\d+)\\.(\\d+)\\.(\\d+)", "\\1", key, perl = TRUE),
+         month =  gsub("X(\\d+)\\.(\\d+)\\.(\\d+)", "\\2", key, perl = TRUE),
+         day =  gsub("X(\\d+)\\.(\\d+)\\.(\\d+)", "\\3", key, perl = TRUE),
+         date = as.Date(paste0(year, "-", month, "-", day), format = "%Y-%m-%d"), 
+         mcap = mcap/1000) %>%
+  select(date, symbol, mcap) %>%
+  drop_na()
+
+df <- ps %>%
+        full_join(mcap) %>%
+        drop_na()
+
+dates_to_run <- c("2016-11-30", "2017-11-30", "2018-11-30", "2019-11-30", "2020-11-30")
+
+for(dt in dates_to_run){
+    
+  ps_stocks <- df %>%
+                filter(date == as.Date(dt)) %>%
+                mutate(ps_above_20 = ifelse(ps > 20, 1, 0)) %>%
+                select(symbol, ps_above_20, mcap)
+  
+  starting_mcap <- ps_stocks %>%
+                    group_by(ps_above_20) %>%
+                    summarise(starting_mcap = sum(mcap)) %>%
+                    ungroup()
+  
+  to_plot <- df %>%
+              filter(date >= as.Date(dt)) %>%
+              inner_join(select(ps_stocks, -mcap)) %>%
+              group_by(date, ps_above_20) %>%
+              summarise(mcap = sum(mcap)) %>%
+              ungroup() %>%
+              left_join(starting_mcap) %>%
+              mutate(mcap_final = mcap/starting_mcap,
+                     ps_above_20 = ifelse(ps_above_20 == 1, "P/S > 20", "P/S <= 20")) 
+  
+  print(nrow(to_plot))
+  
+  date_string <- date_to_string(dt)
+  
+  file_path <- paste0(out_path, "/ps_gt_20_mcap_", date_string, ".jpeg")
+  source_string <- paste0("Source: YCharts (OfDollarsAndData.com)")
+  
+  # Plot the returns to show how much they change over time
+  plot <- ggplot(data = to_plot, aes(x = date, y = mcap_final, col = ps_above_20)) +
+    geom_line() +
+    scale_y_continuous(label = dollar) +
+    ggtitle(paste0("Growth of $1 for Stocks by P/S Ratio")) +
+    of_dollars_and_data_theme +
+    theme(legend.position = "bottom",
+          legend.title = element_blank()) +
+    labs(x = "Date" , y = "Growth of $1",
+         caption = paste0(source_string))
+  
+  # Save the plot
+  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+
+}
+
+# ############################  End  ################################## #
