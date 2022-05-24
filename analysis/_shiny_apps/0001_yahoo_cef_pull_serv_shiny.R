4efcf6140d70ac8df7b0b41e6774e88a2927713b
Nicholas Maggiulli <nmaggiulli22@gmail.com>
2022-05-24 12:29:14 -0400

    Serv shiny

---
+++ analysis/_shiny_apps/0001_yahoo_cef_pull_serv_shiny.R
@@ -0,0 +1,35 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/rwm_analytics")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(rsconnect)
+
+########################## Start Program Here ######################### #
+
+# Connect to Shiny Apps
+rsconnect::setAccountInfo(name='nmaggiulli',
+                          token='see account for token',
+                          secret='see account for secret')
+
+# Set paths
+shiny_path <- paste0("/Users/nmaggiulli/git/of_dollars_and_data/analysis/_shiny_apps/0001_shiny_yahoo_cef_pull/")
+rsconnect_path <- paste0(shiny_path, "/rsconnect")
+
+# Terminate app and delete rsconnect library
+terminateApp("0001_shiny_yahoo_cef_pull", account = 'nmaggiulli')
+
+if(dir.exists(rsconnect_path)){
+  unlink(rsconnect_path, recursive = TRUE)
+}
+
+# Redeploy app and then delete rsconnect folder
+rsconnect::deployApp(shiny_path, account = 'nmaggiulli')
+Sys.sleep(5)
+unlink(rsconnect_path, recursive = TRUE)
+
+# ############################  End  ################################## #
