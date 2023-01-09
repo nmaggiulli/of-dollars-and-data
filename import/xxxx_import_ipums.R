3a39b5b36f2c824b8827ad074228f7995b96edc0
Nicholas Maggiulli <nmaggiulli22@gmail.com>
2023-01-09 13:01:20 -0500

    Add ipums import

---
+++ import/xxxx_import_ipums.R
@@ -0,0 +1,29 @@
+cat("\014") # Clear your console
+rm(list = ls()) #clear your environment
+
+########################## Load in header file ######################## #
+setwd("~/git/of_dollars_and_data")
+source(file.path(paste0(getwd(),"/header.R")))
+
+########################## Load in Libraries ########################## #
+
+library(scales)
+library(readxl)
+library(lubridate)
+library(stringr)
+library(ggrepel)
+library(survey)
+library(lemon)
+library(mitools)
+library(Hmisc)
+library(ipumsr)
+library(tidyverse)
+
+folder_name <- "0331_income_by_age_edc"
+out_path <- paste0(exportdir, folder_name)
+dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
+
+########################## Start Program Here ######################### #
+
+ddi <- read_ipums_ddi(paste0(importdir, "xxxx_ipums_data/cps_00001.xml"))
+data <- read_ipums_micro(ddi)
