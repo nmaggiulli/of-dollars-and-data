b97b5fbae49353a3a05756dc8dcf088e278169ed
Nicholas Maggiulli <nmaggiulli22@gmail.com>
2023-04-11 17:28:30 -0400

    Rich vs wealthy update

--- analysis/0345_rich_vs_wealthy.R
+++ analysis/0345_rich_vs_wealthy.R
@@ -30,22 +30,56 @@ data_year <- 2019
 scf_stack <- readRDS(paste0(localdir, "0003_scf_stack.Rds")) %>%
               filter(year == data_year)
 
-rich_income_limit <- 200000
-wealthy_networth_limit <- 1.2 * 10^6
-
 df <- scf_stack %>%
       select(hh_id, imp_id, age, agecl,
              networth,
-             income, wgt) %>%
-      mutate(high_income = ifelse(income > rich_income_limit, 1, 0),
-             high_wealth = ifelse(networth > wealthy_networth_limit, 1, 0)) %>%
-      arrange(hh_id, imp_id)
+             income, wgt)
 
-high_inc <- df %>%
-            filter(high_income == 1)
+n_hh <- length(unique(df$hh_id))
 
-n_hhs <- high_inc %>%
-            select(hh_id) %>%
-            distinct()
+plot_var <- function(var, title_var){
+  
+  to_plot <- df %>%
+                rename_(.dots = setNames(paste0(var), "var_for_qtile")) %>%
+                summarise(`Top 10%` = wtd.quantile(var_for_qtile, weights = wgt, probs = 0.9),
+                          `Top 5%` = wtd.quantile(var_for_qtile, weights = wgt, probs = 0.95),
+                          `Top 1%` = wtd.quantile(var_for_qtile, weights = wgt, probs = 0.99)) %>%
+                gather(key=key, value = value)
+  
+  to_plot$key <- factor(to_plot$key, levels = c("Top 10%", "Top 5%", "Top 1%"))
+  
+  file_path <- paste0(out_path, "/rich_v_wealthy_", var, "_", data_year, "_scf.jpeg")
+  source_string <- paste0("Source:  Survey of Consumer Finances, ", data_year, " (OfDollarsAndData.com)")
+  note_string <-  str_wrap(paste0("Note:  Percentiles are calculated using data based on ", 
+                                  formatC(n_hh, digits = 0, format = "f", big.mark = ","), 
+                                  " U.S. households.")
+                           , width = 85)
+  
+  text_labels <- to_plot %>%
+    mutate(label = case_when(
+      value > 10^6 ~  paste0("$", formatC(round(value/1000000, 2), big.mark=",", format="f", digits=1), "M"),
+      value > 1000 ~  paste0("$", formatC(round(value/1000, 0), big.mark=",", format="f", digits=0), "k"),
+      TRUE ~ paste0("$", value)
+    ))
+  
+  
+  plot <- ggplot(to_plot, aes(x=key, y=value)) +
+    geom_bar(stat = "identity", fill = chart_standard_color) +
+    geom_text(data = text_labels, aes(x=key, y=value, label = label),
+              col = chart_standard_color,
+              size = 3,
+              vjust= -0.5) +
+    scale_y_continuous(label = dollar) +
+    of_dollars_and_data_theme +
+    ggtitle(paste0("U.S. Household ", title_var, " for the\nTop 10%, Top 5%, and Top 1%")) +
+    labs(x = "Percentile" , y = title_var,
+         caption = paste0(source_string, "\n", note_string))
+  
+  # Save the plot
+  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+}
+
+plot_var("networth", "Net Worth")
+plot_var("income", "Income")
 
 # ############################  End  ################################## #
