01c7c89f971c9af907548c76029f776a391dfaba
Nick Maggiulli <nick@ofdollarsanddata.com>
2024-02-02 13:50:35 -0500

    Rename stock dividends analysis

--- analysis/0385_stock_dividends.R
+++ analysis/0385_stock_dividends.R
@@ -14,7 +14,7 @@ library(stringr)
 library(ggrepel)
 library(tidyverse)
 
-folder_name <- "0384_stock_dividends"
+folder_name <- "0385_stock_dividends"
 out_path <- paste0(exportdir, folder_name)
 dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
