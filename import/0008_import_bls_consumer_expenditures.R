2a20a6f516cd2ad4c68e396de22826aa3acd4a26
Nick Maggiulli <nick@ofdollarsanddata.com>
2024-04-21 18:00:29 -0400

    Fix BLS import for updated analysis

--- import/0008_import_bls_consumer_expenditures.R
+++ import/0008_import_bls_consumer_expenditures.R
@@ -15,13 +15,18 @@ library(dplyr)
 # Use function to make this dynamic
 read_in_bls <- function(string){
   name      <- deparse(substitute(string))
-  temp_name <- read.csv(paste0("https://download.bls.gov/pub/time.series/cx/cx.", name), 
+  temp_name <- read.csv(paste0(importdir,"/0008_bls_consumer_expenditures/cx.", name), 
                              header = TRUE, 
                              sep = "\t", row.names = NULL)
   
+  if(name == "data.1.AllData"){
+    temp_name <- temp_name %>%
+                    mutate(value = as.numeric(value))
+  }
+  
   saveRDS(temp_name, paste0(importdir, "0008_bls_consumer_expenditures/bls_cx_", name, ".Rds"))
+  assign(paste0("t_", name), temp_name, envir = .GlobalEnv)
 }
-
 
 read_in_bls(data.1.AllData)
 read_in_bls(category)
