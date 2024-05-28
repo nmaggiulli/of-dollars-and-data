04ee552b0df13a9f2451b36195185e753909f7b9
Nick Maggiulli <nick@ofdollarsanddata.com>
2024-05-28 18:44:24 -0400

    More PSID fixes

--- analysis/_twl/xxxx_psid_analysis.R
+++ analysis/_twl/xxxx_psid_analysis.R
@@ -27,8 +27,8 @@ psid_path <- paste0(importdir, "_twl/0001_psid/")
 r <- system.file(package="psidR")
 cwf <- openxlsx::read.xlsx(file.path(r,"psid-lists","psid.xlsx"))
 
-wealth_years <- c(1984, 1989, 1994, 1999, 2001, 2003, 2005, 2007, 
-                  2009, 2011, 2013, 2015, 2017, 2019, 2021)
+wealth_years <- c(1984, 1989, 1994, 1999, 2001, 2003, 2005, 2007,
+                  2009, 2011, 2013, 2015, 2017)
 
 # Select vars we care about and the years we care about
 f <- fread(file.path(r,"psid-lists","famvars.txt")) %>%
@@ -105,6 +105,7 @@ cpi_deflator <- cpi_all_years %>%
                   mutate(cpi_deflator = cpi/cpi_all_years[1, "cpi"]) %>%
                   select(year, cpi_deflator)
                         
+# Join all data sources and deflate by CPI
 full_psid_supp <- full_psid %>%
                     left_join(supp_stack) %>%
                     left_join(cpi_deflator) %>%
@@ -113,42 +114,97 @@ full_psid_supp <- full_psid %>%
                       TRUE ~ wealth/cpi_deflator
                     ),
                     wealth_level = case_when(
-                      networth < 10000 ~ "L1 (<$10k)",
-                      floor(log10(networth)) == 4 ~ "L2 ($10k)",
-                      floor(log10(networth)) == 5 ~ "L3 ($100k)",
-                      floor(log10(networth)) == 6 ~ "L4 ($1M)",  
-                      floor(log10(networth)) == 7 ~ "L5 ($10M)",  
-                      floor(log10(networth)) > 7 ~ "L6 ($100M+)", 
-                      TRUE ~ "ERROR"
+                      networth < 10000 ~ 1,
+                      floor(log10(networth)) == 4 ~ 2,
+                      floor(log10(networth)) == 5 ~ 3,
+                      floor(log10(networth)) == 6 ~ 4,  
+                      floor(log10(networth)) == 7 ~ 5,  
+                      floor(log10(networth)) > 7 ~ 6, 
+                      TRUE ~ NA
                     )) %>%
-                    select(year, interview, weight, networth, wealth_level, faminc, hvalue,
+                    select(year, interview, weight, pernum, networth, wealth_level, faminc, hvalue,
                            ID1968, age, educ)
-
-full_psid_supp$wealth_level <- factor(full_psid_supp$wealth_level, levels = c("L1 (<$10k)", "L2 ($10k)",
-                                                                    "L3 ($100k)", "L4 ($1M)",
-                                                                    "L5 ($10M)", "L6 ($100M+)"))
 
 missing_nw_id_to_remove <- full_psid_supp %>%
                           filter(is.na(networth)) %>%
                           pull(ID1968)
 
+# Now just keep one observation per HH
 full_data <- full_psid_supp %>%
-            filter(ID1968 != missing_nw_id_to_remove) 
-            
-test_1984_1994 <- full_data %>%
-  filter(year %in% c(1984, 1994), weight > 0) %>%
-  group_by(year, wealth_level, ID1968) %>%
-  summarise(total_weight = sum(weight)) %>%
-  ungroup() %>%
-  mutate(percentage = total_weight / sum(total_weight)) %>%
-  arrange(ID1968, year) %>%
-  mutate(level_change = case_when(
-    ID1968 != lead(ID1968) ~ "",
-    wealth_level == lead(wealth_level) ~ "0 (No Change)",
-    wealth_level < lead(wealth_level) ~ "1 (Up Level)",
-    TRUE ~ "-1 (Down Level)"
-  ))
+            filter(ID1968 != missing_nw_id_to_remove) %>%
+            arrange(ID1968, year, pernum) %>%
+            group_by(year, ID1968) %>%
+            slice(1) %>%
+            ungroup()
 
+#Loop through start and end years
+years_df <- data.frame(
+   start_year = c(1984, 1989, 1994, 1999, 2001, 2003, 2005, 2007),
+   end_year = c(1994, 1999, 2003, 2009, 2011, 2013, 2015, 2017)
+)
 
+for(i in 1:nrow(years_df)){
+  start_yr <- years_df[i, "start_year"]
+  end_yr <- years_df[i, "end_year"]
+  
+  start_data <- full_data %>%
+    filter(year == start_yr, weight > 0) %>%
+    select(year, weight, ID1968, wealth_level) %>%
+    rename(start_level = wealth_level)
+  
+  end_data <- full_data %>%
+    filter(year == end_yr, weight > 0) %>%
+    select(ID1968, wealth_level) %>%
+    rename(end_level = wealth_level)
+  
+  merged_level_comparison <- start_data %>%
+                              left_join(end_data)
+  
+  tmp_level_weight <- merged_level_comparison %>%
+                      group_by(start_level) %>%
+                      summarise(level_weight = sum(weight)) %>%
+                      ungroup()
+  
+  tmp_levels_summary <- merged_level_comparison %>%
+                      group_by(start_level, end_level) %>%
+                      summarise(n_hhs = n(),
+                                total_weight = sum(weight)) %>%
+                      ungroup() %>%
+                      left_join(tmp_level_weight) %>%
+                      mutate(level_pct = total_weight / level_weight,
+                             all_pct = total_weight / sum(total_weight),
+                             start_year = start_yr,
+                             end_year = end_yr)
+  
+  tmp_change_summary <- merged_level_comparison %>%
+    mutate(level_change = end_level - start_level) %>%
+    group_by(level_change) %>%
+    summarise(n_hhs = n(),
+              total_weight = sum(weight)) %>%
+    ungroup() %>%
+    mutate(all_pct = total_weight / sum(total_weight),
+           start_year = start_yr,
+           end_year = end_yr)
+  
+  if(start_yr == min(years_df$start_year)){
+    levels_stack <- tmp_levels_summary
+    change_stack <- tmp_change_summary
+  } else{
+    levels_stack <- levels_stack %>% bind_rows(tmp_levels_summary)
+    change_stack <- change_stack %>% bind_rows(tmp_change_summary)
+  }
+}
+
+#Summarize the summaries
+all_levels_summary <- levels_stack %>%
+                      group_by(start_level, end_level) %>%
+                      summarise(level_pct = mean(level_pct),
+                                all_pct = mean(all_pct)) %>%
+                      ungroup()
+
+all_change_summary <- change_stack %>%
+                        group_by(level_change) %>%
+                        summarise(all_pct = mean(all_pct)) %>%
+                        ungroup()
 
 # ############################  End  ################################## #
