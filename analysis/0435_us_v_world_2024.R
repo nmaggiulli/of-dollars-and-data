4088840145971dc9d1e30beef84f545cb74332c4
Nick Maggiulli <nick@ofdollarsanddata.com>
2025-01-19 12:49:17 -0500

    Rename analysis

--- analysis/0435_us_v_world_2024.R
+++ analysis/0435_us_v_world_2024.R
@@ -14,7 +14,7 @@ library(stringr)
 library(ggrepel)
 library(tidyverse)
 
-folder_name <- "0434_us_v_world"
+folder_name <- "0435_us_v_world"
 out_path <- paste0(exportdir, folder_name)
 dir.create(file.path(paste0(out_path)), showWarnings = FALSE)
 
