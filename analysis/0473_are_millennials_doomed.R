8b8cf9cfb11b7ce54232dc73246159b7fad32456
Nick Maggiulli <nmaggiulli22@gmail.com>
2025-10-18 10:04:14 -0400

    Add more vars

--- analysis/0473_are_millennials_doomed.R
+++ analysis/0473_are_millennials_doomed.R
@@ -33,24 +33,31 @@ scf_stack <- readRDS(paste0(localdir, "0003_scf_stack.Rds")) %>%
               mutate(age_26_41 = case_when(
                 age >= 26 & age <= 41 ~ 1,
                 TRUE ~ 0
-              ))
+              ),
+              rent_over_income = case_when(
+                rent > 0 & (rent*12 > income) ~ 1,
+                rent > 0 ~ (rent*12)/income,
+                TRUE ~ NaN),
+              nhd = debt - mrthel
+              )
               
 cohort_26_41 <- scf_stack %>%
                   filter(age_26_41 == 1)
 
 max_year <- max(scf_stack$year)
 
-nw_stats <- cohort_26_41 %>%
-            group_by(year) %>%
-            summarise(`90th Percentile` = wtd.quantile(networth, probs = 0.9, weights = wgt),
-                      `75th Percentile` = wtd.quantile(networth, probs = 0.75, weights = wgt),
-                      `50th Percentile` = wtd.quantile(networth, probs = 0.5, weights = wgt),
-                      `25th Percentile` = wtd.quantile(networth, probs = 0.25, weights = wgt),
-                      `15th Percentile` = wtd.quantile(networth, probs = 0.15, weights = wgt)) %>%
-            ungroup() %>%
-            gather(-year, key=key, value=value)
-
-plot_percentiles <- function(grepl_string, pct_file_string, percentile_text){
+plot_percentiles <- function(grepl_string, pct_file_string, percentile_text, var_name, var_slug){
+  
+  tmp <- cohort_26_41 %>%
+    group_by(year) %>%
+    rename_(.dots = setNames(var_name, "var_name")) %>%
+    summarise(`90th Percentile` = wtd.quantile(var_name, probs = 0.9, weights = wgt),
+              `75th Percentile` = wtd.quantile(var_name, probs = 0.75, weights = wgt),
+              `50th Percentile` = wtd.quantile(var_name, probs = 0.5, weights = wgt),
+              `25th Percentile` = wtd.quantile(var_name, probs = 0.25, weights = wgt),
+              `15th Percentile` = wtd.quantile(var_name, probs = 0.15, weights = wgt)) %>%
+    ungroup() %>%
+    gather(-year, key=key, value=value)
   
   if(grepl_string == "50th|75th|90th"){
     tmp_colors <- rev(my_colors[1:3])
@@ -58,28 +65,45 @@ plot_percentiles <- function(grepl_string, pct_file_string, percentile_text){
     tmp_colors <- rev(my_colors[3:5])
   }
   
-  to_plot <- nw_stats %>%
+  if(var_slug== "nw"){
+    title <- "Net Worth"
+  } else if (var_slug == "roi"){
+    title <- "Rent Over Income"
+  } else if (var_slug == "nhd"){
+    title <- "Non-Home Debt"
+  }
+  
+  to_plot <- tmp %>%
                 filter(grepl(grepl_string, key))
   
-  file_path <- paste0(out_path, "/millennial_cohort_net_worth_by_year_", pct_file_string, "_", max_year, ".jpeg")
+  file_path <- paste0(out_path, "/millennial_cohort_", var_slug, "_by_year_", pct_file_string, "_", max_year, ".jpeg")
   source_string <- "Source:  Survey of Consumer Finances (OfDollarsAndData.com)"
   
   plot <- ggplot(to_plot, aes(x=year, y = value, col = key)) +
     geom_line() +
     scale_color_manual(values = tmp_colors) +
-    scale_y_continuous(label = dollar) +
     of_dollars_and_data_theme +
     theme(legend.position = "bottom",
           legend.title = element_blank()) +
-    ggtitle(paste0("Inflation-Adjusted Net Worth By Year\nHouseholds Age 26-41\n", percentile_text)) +
-    labs(x="Year", y="Inflation-Adjusted Net Worth",
+    ggtitle(paste0("Inflation-Adjusted ", title, " By Year\nHouseholds Age 26-41\n", percentile_text)) +
+    labs(x="Year", y=paste0("Inflation-Adjusted ", title),
          caption = paste0(source_string))
+  
+  if(var_name == "nw" | var_name == "nhd"){
+    plot <- plot +
+      scale_y_continuous(label = dollar) 
+  } else{
+    plot <- plot +
+      scale_y_continuous(label = percent_format(accuracy = 1)) 
+  }
   
   # Save the plot
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 }
 
-plot_percentiles("50th|75th|90th", "50_90", "50th-90th Percentile")
-plot_percentiles("15th|25th|50th", "15_50", "15th-50th Percentile")
+plot_percentiles("50th|75th|90th", "50_90", "50th-90th Percentile", "networth", "nw")
+plot_percentiles("15th|25th|50th", "15_50", "15th-50th Percentile", "networth", "nw")
+plot_percentiles("50th|75th|90th", "50_90", "50th-90th Percentile", "rent_over_income", "roi")
+plot_percentiles("50th|75th|90th", "50_90", "50th-90th Percentile", "nhd", "nhd")
 
 # ############################  End  ################################## #
