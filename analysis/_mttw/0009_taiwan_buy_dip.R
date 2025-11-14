b82ad562722b5748dc8b7375a40fe650b95b2f25
Nick Maggiulli <nmaggiulli22@gmail.com>
2025-11-14 12:01:59 -0500

    Update btd strategy

--- analysis/_mttw/0009_taiwan_buy_dip.R
+++ analysis/_mttw/0009_taiwan_buy_dip.R
@@ -85,7 +85,10 @@ ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 
 create_full_dd <- function(dataset, start_date, end_date){
   
-  dd <- drawdown_path(dataset)
+  tmp_dataset <- dataset %>%
+                  filter(date >= start_date, date <= end_date)
+  
+  dd <- drawdown_path(tmp_dataset)
   
   # Find bottoms
   for (i in 1:nrow(dd)){
@@ -180,7 +183,9 @@ calculate_dca_dip_diff <- function(dataset, n_month_delay, start_date, end_date)
     mutate(lag_price = lag(index)) %>%
     filter(!is.na(lag_price))
   
-  purchase_dates <- full_dd %>%
+  tmp_dd <- create_full_dd(dataset, start_date, end_date)
+  
+  purchase_dates <- tmp_dd %>%
     filter(pct == min_dd, min_dd < 0) %>%
     mutate(date = date + months(n_month_delay + 1)) %>%
     filter(date >= as.Date(start_date), 
@@ -188,11 +193,11 @@ calculate_dca_dip_diff <- function(dataset, n_month_delay, start_date, end_date)
     mutate(dip_amount = (interval(lag(date), date) %/% months(1)) * monthly_buy) %>%
     select(date, dip_amount)
   
-  # Find first purchase amount
-  purchase_dates[1, "dip_amount"] <- (interval(start_date, purchase_dates[1, "date"]) %/% months(1) + 1) * monthly_buy
-  
-  # Find the last purchase date
-  last_purchase_date <- purchase_dates[nrow(purchase_dates), "date"]
+    # Find first purchase amount
+    purchase_dates[1, "dip_amount"] <- (interval(start_date, purchase_dates[1, "date"]) %/% months(1) + 1) * monthly_buy
+    
+    # Find the last purchase date
+    last_purchase_date <- purchase_dates[nrow(purchase_dates), "date"]
   
   end <- tmp_dataset[nrow(tmp_dataset), "index"]
   
@@ -221,7 +226,7 @@ calculate_dca_dip_diff <- function(dataset, n_month_delay, start_date, end_date)
 }
 
 # Plot ATHs and Dips
-plot_ath_dips <- function(dataset, lag_length, start_date, end_date){
+plot_ath_dips <- function(dataset, start_date, end_date){
   
   # Calculate data
   full_df <- full_dca_dip(dataset, 0, start_date, end_date)
@@ -271,30 +276,31 @@ plot_ath_dips <- function(dataset, lag_length, start_date, end_date){
     scale_y_continuous(label = dollar) +
     scale_x_date(date_labels = "%Y") +
     of_dollars_and_data_theme +
-    ggtitle("All-Time Highs & 'Dips'") +
-    labs(x = "Date", y = "Growth of $1")
+    ggtitle("歷史新高與回落") +
+    labs(x = "日期", y = "新台幣1元的成長")
   
   # Save the plot
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
 }
 
 # Plot DCA v Cash
-plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date){
+plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date, cash_nudge, invest_nudge,
+                            dca_nudge, btd_nudge){
   
-  if(lag_length > 0){
-    end_title <- paste0(" (With ", lag_length, "-Month Lag)")
+  if(lag_length == 2){
+    end_title <- paste0(" (延遲兩個月)")
   } else{
     end_title <- ""
   }
   
   to_plot <- full_dca_dip(dataset, lag_length, start_date, end_date) %>%
     select(date, dip_vested) %>%
-    rename(`Invested\nAmount` = dip_vested) %>%
+    rename(`投資金額` = dip_vested) %>%
     gather(-date, key=key, value=value)
   
   cash <- full_dca_dip(dataset, lag_length, start_date, end_date) %>%
     select(date, dip_cash) %>%
-    rename(`Cash` = dip_cash) %>%
+    rename(`現金` = dip_cash) %>%
     gather(-date, key=key, value=value)
   
   bottom <- full_dca_dip(dataset, lag_length, start_date, end_date) %>%
@@ -316,20 +322,20 @@ plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date
   file_path <- paste0(out_path, "/", start_date_string, "/dip_cash_lag_", lag_length, "_", start_date_string, ".jpeg")
   
   plot <- ggplot(to_plot, aes(x=date, y=value)) +
-    geom_bar(data=cash, aes(x=date, y=value), col = "green", stat="identity") +
+    geom_bar(data=cash, aes(x=date, y=value), fill = "green", col = "green", stat="identity") +
     geom_line(col = "black") +
     geom_point(data=bottom, aes(x=date, y=value), col = "red", size = dot_size, alpha = 0.7) +
     scale_y_continuous(label = dollar) +
-    scale_color_manual(values = c("green", "black"), guide = FALSE) +
+    scale_color_manual(values = c("black", "green"), guide = FALSE) +
     geom_text_repel(data=text_labels, aes(x=date, y=value, col = key),
                     label = text_labels$key,
                     family = "my_font",
                     segment.color = "transparent", 
-                    nudge_y = ifelse(text_labels$key == "Cash", 10000, 15000),
+                    nudge_y = ifelse(text_labels$key == "現金", cash_nudge, invest_nudge),
                     max.iter = 3000) +
     of_dollars_and_data_theme +
-    ggtitle(paste0("Buy the Dip", end_title)) +
-    labs(x = "Date", y = "Amount")
+    ggtitle(paste0("逢低買進", end_title)) +
+    labs(x = "日期", y = "金額")
   
   # Save the plot
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
@@ -337,9 +343,11 @@ plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date
   # Second plot
   to_plot <- full_dca_dip(dataset, lag_length, start_date, end_date) %>%
     select(date, dip_value, dca_value) %>%
-    rename(`DCA` = dca_value,
-           `Buy the Dip` = dip_value) %>%
+    rename(`持續買進` = dca_value,
+           `逢低買進` = dip_value) %>%
     gather(-date, key=key, value=value)
+  
+  assign("to_plot_tmp", to_plot, envir = .GlobalEnv)
   
   text_labels <- to_plot %>%
     filter(date == text_date)
@@ -357,50 +365,22 @@ plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date
     geom_line() +
     geom_point(data=bottom, aes(x=date, y=value), col = "red", size = dot_size, alpha = 0.7) +
     scale_y_continuous(label = dollar) +
-    scale_color_manual(values = c("#3182bd", "black"), guide = FALSE) +
+    scale_color_manual(values = c("black", "#3182bd"), guide = FALSE) +
     geom_text_repel(data=text_labels, aes(x=date, y=value, col = key),
                     label = text_labels$key,
                     family = "my_font",
-                    nudge_y = ifelse(text_labels$key == "DCA", -15000, 15000),
+                    nudge_y = ifelse(text_labels$key == "持續買進", dca_nudge, btd_nudge),
                     segment.color = "transparent") +
     of_dollars_and_data_theme +
-    ggtitle(paste0("Buy the Dip", end_title, "\nvs. DCA")) +
-    labs(x = "Date", y = "Amount")
+    ggtitle(paste0("逢低買進 vs. 持續買進\n", end_title)) +
+    labs(x = "日期", y = "金額")
   
   # Save the plot
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
   
   # Pull cumulative dollar growth
   cumulative_totals <- calculate_dca_dip_diff(dataset, lag_length, start_date, end_date)
-  
-  to_plot <- cumulative_totals %>%
-    select(date, cumulative_dca, cumulative_dip) %>%
-    rename(`DCA` = cumulative_dca,
-           `Buy the Dip` = cumulative_dip) %>%
-    gather(-date, key=key, value=value)
-  
-  text_labels <- to_plot %>%
-    filter(date == text_date)
-  
-  # Plot Cumululative growth
-  file_path <- paste0(out_path, "/", start_date_string, "/cumulative_growth_lag_", lag_length, "_", start_date_string, ".jpeg")
-  
-  plot <- ggplot(to_plot, aes(x=date, y=value, col = key)) +
-    geom_line() +
-    scale_y_continuous(label = dollar) +
-    scale_color_manual(values = c("#3182bd", "black"), guide = FALSE) +
-    geom_text_repel(data=text_labels, aes(x=date, y=value, col = key),
-                    label = text_labels$key,
-                    family = "my_font",
-                    nudge_y = ifelse(text_labels$key == "DCA", -15000, 1000),
-                    segment.color = "transparent") +
-    of_dollars_and_data_theme +
-    ggtitle(paste0("Cumulative Growth of DCA and\nBuy the Dip", end_title)) +
-    labs(x = "Date", y = "Amount")
-  
-  # Save the plot
-  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
-  
+
   # DCA growth only
   to_plot <- cumulative_totals %>%
     select(date, dca_growth) %>%
@@ -411,10 +391,10 @@ plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date
     select(date, dca_growth) %>%
     gather(-date, key=key, value=value)
   
-  file_path <- paste0(out_path, "/", start_date_string, "/dca_final_growth_lag_", lag_length, "_", start_date_string, ".jpeg")
+  file_path <- paste0(out_path, "/", start_date_string, "/dca_final_growth_btd_", lag_length, "_", start_date_string, ".jpeg")
   
   plot <- ggplot(to_plot, aes(x=date, y=value)) +
-    geom_bar(stat="identity", position="dodge", col = "black") +
+    geom_bar(stat="identity", position="dodge", col = "black", fill = "black") +
     geom_point(data=bottom, aes(x=date, y=value), col = "red", alpha = 0.7)+
     geom_hline(yintercept = monthly_buy, linetype = "dashed", col="red") +
     scale_y_continuous(label = dollar) +
@@ -427,10 +407,10 @@ plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date
   ggsave(file_path, plot, width = 15, height = 12, units = "cm")
   
   # Do a separate one without the dips
-  file_path <- paste0(out_path, "/", start_date_string, "/dca_final_growth_", lag_length, "_", start_date_string, ".jpeg")
+  file_path <- paste0(out_path, "/", start_date_string, "/dca_final_growth_", start_date_string, ".jpeg")
   
   plot <- ggplot(to_plot, aes(x=date, y=value)) +
-    geom_bar(stat="identity", position="dodge", col = "black") +
+    geom_bar(stat="identity", position="dodge", col = "black", fill = "black") +
     scale_y_continuous(label = dollar) +
     scale_color_manual(values = c("#3182bd", "black"), guide = FALSE) +
     of_dollars_and_data_theme +
@@ -445,9 +425,8 @@ plot_dca_v_cash <- function(dataset, lag_length, start_date, end_date, text_date
 
 # Define simulation parameters
 monthly_buy <- 10000
-n_years <- 30
 analysis_start <- as.Date("1988-01-01")
-analysis_end <- as.Date("2025-10-01")
+analysis_end <- as.Date("2024-12-01")
 
 # Parameters for all plots
 dot_size <- 1.2
@@ -456,7 +435,91 @@ dot_size <- 1.2
 full_dd <- create_full_dd(raw_msci, analysis_start, analysis_end)
 
 # Do all plotting
-plot_ath_dips(raw_msci, 0, "1988-01-01", "2025-10-01")
-plot_dca_v_cash(raw_msci, 0, "1988-01-01", "2025-10-01", "2014-12-01")
+plot_ath_dips(raw_msci, "2015-01-01", "2024-12-01")
+plot_dca_v_cash(raw_msci, 0, "2015-01-01", "2024-12-01", "2022-01-01", 400000, 500000, -900000, 500000)
+plot_dca_v_cash(raw_msci, 2, "2015-01-01", "2024-12-01", "2022-01-01", 400000, 500000, 500000, -900000)
+
+
+run_all_years <- 1
+n_years <- 10
+
+if (run_all_years == 1){
+  # Define final results data frame
+  final_results <- data.frame()
+  
+  # Define list of dates
+  all_dates <- seq.Date(analysis_start, analysis_end - years(n_years) + months(1), "year")
+  
+  # Loop through all dates to run DCA vs. Buy the Dip Strategy comparisons
+  counter <- 1
+  for (d in 1:length(all_dates)){
+    
+    st <- all_dates[d]
+    print(st)
+    
+    final_results[counter, "start_date"] <- format.Date(st)
+    final_results[counter, "end_date"] <- format.Date(st + years(n_years) - months(1))
+    final_results[counter, "n_years"] <- n_years
+    
+    for(i in c(0, 2)){
+      tmp <- calculate_dca_dip_diff(raw_msci, i, st, st + years(n_years) - months(1))
+      
+      dip_diff_name <- paste0("dip_pct_gt_lag_", i)
+      dip_win_name <- paste0("dip_win_lag_", i)
+      dca_name <- paste0("dca_final_amount_lag_", i)
+      
+      final_results[counter, dip_diff_name] <- sum(tmp$dip_growth)/sum(tmp$dca_growth) - 1
+      final_results[counter, dip_win_name] <- ifelse(final_results[counter, dip_diff_name] > 0, 1, 0)
+      final_results[counter, dca_name] <- sum(tmp$dca_growth)
+    }
+    
+    counter <- counter + 1
+  }
+  
+  # Plot the DCA outperformance by year
+  to_plot <- final_results %>%
+    select(start_date, dip_pct_gt_lag_2) %>%
+    mutate(date = as.Date(start_date)) %>%
+    select(-start_date) %>%
+    rename(`Lag2` = dip_pct_gt_lag_2) %>%
+    gather(-date, key=key, value=value) %>%
+    mutate(key = factor(key, levels = c("Lag2")))
+  
+  file_path <- paste0(out_path, "/dip_buying_outperformance_lag2.jpeg")
+  
+  text_labels <- data.frame(date = c(as.Date("2008-01-01"), as.Date("2008-01-01")),
+                            value = c(0.15, -0.15),
+                            label = c("逢低買進表現較佳", "逢低買進表現較差"))
+  
+  plot <- ggplot(to_plot, aes(x=date, y=value, col = key)) +
+    geom_line() +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    geom_text_repel(data=text_labels, aes(x=date, y=value), 
+                    label = text_labels$label, 
+                    col="black", 
+                    family = "my_font",
+                    max.iter = 1) +
+    scale_y_continuous(label = percent) +
+    scale_color_manual(values = c("#3182bd"), guide = FALSE) +
+    scale_x_date(date_labels = "%Y",
+                 breaks = c(
+                   as.Date("1990-01-01"),
+                   as.Date("2000-01-01"),
+                   as.Date("2010-01-01")
+                 )) +
+    of_dollars_and_data_theme +
+    ggtitle(paste0("逢低買進 vs. 持續買進 (延遲兩個月)\n所有", n_years, "年期間")) +
+    labs(x = "日期", y = "逢低買進超額報酬率（%）")
+  
+  # Save the plot
+  ggsave(file_path, plot, width = 15, height = 12, units = "cm")
+  
+  # Print summary stats
+  print(mean(final_results$dip_win_lag_0))
+  print(mean(final_results$dip_win_lag_2))
+  
+  print(mean(final_results$dip_pct_gt_lag_0))
+  print(mean(final_results$dip_pct_gt_lag_2))
+}
 
 # ############################  End  ################################## #
